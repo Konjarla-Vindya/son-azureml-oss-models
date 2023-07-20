a2fb72fd3bce980c4824d8522e73f85d7189a360
PoojaRam12 <139552501+PoojaRam12@users.noreply.github.com>
2023-07-20 19:33:21 +0530

    Update register_bert_model.py

--- tests/src/register_bert_model.py
+++ tests/src/register_bert_model.py
@@ -158,7 +158,7 @@ def download_and_register_model():
             transformers_model = {"model" : model, "tokenizer":tokenizer},
             task="fill-mask",
             artifact_path="Bert_artifact",
-            registered_model_name=models
+            registered_model_name=test_model_name
     )
     
 # def get_latest_model_version(registry_ml_client, test_model_name):
