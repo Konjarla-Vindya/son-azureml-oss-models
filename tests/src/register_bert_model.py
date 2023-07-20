76e0710aa9ae439d616ac0eca9946ee875be19cf
PoojaRam12 <139552501+PoojaRam12@users.noreply.github.com>
2023-07-20 17:18:02 +0530

    Update register_bert_model.py

--- tests/src/register_bert_model.py
+++ tests/src/register_bert_model.py
@@ -211,7 +211,7 @@ def main():
         resource_group_name=queue['resource_group'],
         workspace_name=queue['workspace']
     )
-mlflow.set_tracking_uri(queue['workspace'].get_mlflow_tracking_uri())
+mlflow.set_tracking_uri(test_workspace_name.get_mlflow_tracking_uri())
 
 # checkpoint = "bert-base-uncased"
 # registered_model_name = "bert_registered"
