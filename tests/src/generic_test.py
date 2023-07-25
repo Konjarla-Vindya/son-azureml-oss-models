c58a998b7e8c30fed7b4acd30531c8df2a1a0fa4
SameepPanigrahi <sameepkumar.1997@gmail.com>
2023-07-25 11:05:30 +0530

    Run Job

--- tests/src/generic_test.py
+++ tests/src/generic_test.py
@@ -1,5 +1,12 @@
+from azureml.core import Experiment, ScriptRunConfig, Workspace
 import os
 import json
+
+workspace = "sonata-test-ws"
+subscription = "80c77c76-74ba-4c8c-8229-4c3b2957990c"
+resource_group = "sonata-test-rg"
+registry = "HuggingFace"
+path = "tests/src/register_xlnet_models.py"
 
 test_model_name = os.environ.get('test_model_name')
 
@@ -29,41 +36,56 @@ def get_test_queue():
         content = json.load(f)
         return content
 
-def set_next_trigger_model(queue):
-    print ("In set_next_trigger_model...")
-    # file the index of test_model_name in models list queue dictionary
-    index = queue['models'].index(test_model_name)
-    print (f"index of {test_model_name} in queue: {index}")
-# if index is not the last element in the list, get the next element in the list
-    if index < len(queue['models']) - 1:
-        next_model = queue['models'][index + 1]
-    else:
-        next_model = ""
-        # if (test_keep_looping == "true"):
-        #     next_model = queue[0]
-        # else:
-        #     print ("::warning:: finishing the queue")
-        #     next_model = ""
-        test_trigger_next_model = False
-        #os.environ["test_trigger_next_model"] = False
-        env_file = os.getenv('GITHUB_ENV')
-        with open(env_file, "a") as myfile:
-            myfile.write(f"test_trigger_next_model={test_trigger_next_model}")
-    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-        print(f'NEXT_MODEL={next_model}')
-        print(f'NEXT_MODEL={next_model}', file=fh)
-    return next_model
+def submit_azuremljob():
+    pass
+# def set_next_trigger_model(queue):
+#     print ("In set_next_trigger_model...")
+#     # file the index of test_model_name in models list queue dictionary
+#     index = queue['models'].index(test_model_name)
+#     print (f"index of {test_model_name} in queue: {index}")
+# # if index is not the last element in the list, get the next element in the list
+#     if index < len(queue['models']) - 1:
+#         next_model = queue['models'][index + 1]
+#     else:
+#         next_model = ""
+#         # if (test_keep_looping == "true"):
+#         #     next_model = queue[0]
+#         # else:
+#         #     print ("::warning:: finishing the queue")
+#         #     next_model = ""
+#         test_trigger_next_model = False
+#         #os.environ["test_trigger_next_model"] = False
+#         env_file = os.getenv('GITHUB_ENV')
+#         with open(env_file, "a") as myfile:
+#             myfile.write(f"test_trigger_next_model={test_trigger_next_model}")
+#     with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+#         print(f'NEXT_MODEL={next_model}')
+#         print(f'NEXT_MODEL={next_model}', file=fh)
+#     return next_model
 
 if __name__ == "__main__":
     queue = get_test_queue()
-    next_model = set_next_trigger_model(queue)
-    if test_trigger_next_model:
-        if next_model is not None:
-            env_file = os.getenv('GITHUB_ENV')
-            with open(env_file, "a") as myfile:
-                myfile.write(f"test_model_name={next_model}")
-                #os.environ["test_model_name"] = 
-    res = os.environ.get("test_model_name")
-    trigger_next_model = os.environ.get("test_trigger_next_model")
-    print(f"Here is the next model to proceed with : {res} and the trigger_next_model value is {trigger_next_model}")
+    azureml_workspace = Workspace(subscription, resource_group, workspace)
+    script_config = ScriptRunConfig(
+                            source_directory='./src',
+                            script='hello.py',
+                            compute_target='cpu-cluster',
+                            
+                            )
+    # Create an Experiment
+    experiment = Experiment(azureml_workspace, 'my_test_experiment_for_run')
+    # Submit the script for execution
+    run = experiment.submit(script_config)
+    print(run)
+    run.wait_for_completion(show_output=True)
+    # next_model = set_next_trigger_model(queue)
+    # if test_trigger_next_model:
+    #     if next_model is not None:
+    #         env_file = os.getenv('GITHUB_ENV')
+    #         with open(env_file, "a") as myfile:
+    #             myfile.write(f"test_model_name={next_model}")
+    #             #os.environ["test_model_name"] = 
+    # res = os.environ.get("test_model_name")
+    # trigger_next_model = os.environ.get("test_trigger_next_model")
+    # print(f"Here is the next model to proceed with : {res} and the trigger_next_model value is {trigger_next_model}")
     
