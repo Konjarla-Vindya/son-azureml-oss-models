3ec7ca9dccbacaab48de78d59b6f37254ebf45c8
VindyaKonjarla <137049777+Konjarla-Vindya@users.noreply.github.com>
2023-07-27 16:36:39 +0530

    Update BertJob.py

--- tests/src/AML_Jobs/BertJob.py
+++ tests/src/AML_Jobs/BertJob.py
@@ -44,7 +44,7 @@ def define_command():
 
 if __name__ == "__main__":
     try:
-        credential = AzureMLOnBehalfOfCredential()()
+        credential = AzureMLOnBehalfOfCredential()
         credential.get_token("https://management.azure.com/.default")
     except Exception as e:
         print (f"::warning:: Getting Exception in the default azure credential and here is the exception log : \n{e}")
