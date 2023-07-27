0343784cf35e2f740c5b214dc9ae45a36b11f8e0
SameepPanigrahi <sameepkumar.1997@gmail.com>
2023-07-27 10:02:37 +0530

    Refactoring

--- tests/src/generic_test.py
+++ tests/src/generic_test.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
     azureml_workspace = Workspace(subscription, resource_group, workspace)
     # or create a new Pip environment from the requirements.txt file
     myenv = Environment.get(workspace=azureml_workspace, name="bert_environment")
-    env = myenv.from_pip_requirements(file_path='requirements/bert_requirements.txt')
+    env = myenv.from_pip_requirements(name="bert_environment", file_path='requirements/bert_requirements.txt')
 
     # Register the environment in your workspace
     env.register(workspace=azureml_workspace)
