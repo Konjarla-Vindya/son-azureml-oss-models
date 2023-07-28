d05debc64aceadc524935161fbbd7b888feec0de
SameepPanigrahi <sameepkumar.1997@gmail.com>
2023-07-28 12:00:01 +0530

    Fixing File hierarchy

--- tests/src/AIMLjob/automated_bert_job.py
+++ tests/src/AIMLjob/automated_bert_job.py
@@ -21,15 +21,15 @@ test_keep_looping = os.environ.get('test_keep_looping')
 
 def get_test_queue():
     config_name = test_queue+'-test'
-    queue_file1 = f"../config/queue/{test_set}/{config_name}.json"
-    queue_file = f"../config/queue/{test_set}/{test_queue}.json"
+    queue_file1 = f"../../config/queue/{test_set}/{config_name}.json"
+    queue_file = f"../../config/queue/{test_set}/{test_queue}.json"
     with open(queue_file) as f:
         return json.load(f)
     
 
 def get_sku_override():
     try:
-        with open(f'../config/sku-override/{test_set}.json') as json_file:
+        with open(f'../../config/sku-override/{test_set}.json') as json_file:
             return json.load(json_file)
     except Exception as e:
         print (f"::warning:: Could not find sku-override file: \n{e}")
