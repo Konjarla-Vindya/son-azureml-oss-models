7772b19dac410d48a529645ae0294a1aa2b6861d
SameepPanigrahi <sameepkumar.1997@gmail.com>
2023-08-01 16:29:19 +0530

    Chnaging compute to avoid queue issue

--- tests/src/automation_for_constant_library/generic_initial_automation.py
+++ tests/src/automation_for_constant_library/generic_initial_automation.py
@@ -87,7 +87,7 @@ def create_or_get_compute_target(ml_client):
     except Exception:
         print("Creating a new cpu compute target...")
         compute = AmlCompute(
-            name=cpu_compute_target, size="Standard_D12_v2", min_instances=0, max_instances=4
+            name=cpu_compute_target, size="STANDARD_D12", min_instances=0, max_instances=4
         )
         ml_client.compute.begin_create_or_update(compute).result()
     
