83a72159e7f1ec024e746c3cbe9891f3ce5ce53d
PoojaRam12 <139552501+PoojaRam12@users.noreply.github.com>
2023-08-01 22:11:08 +0530

    Update 1p-BertJob.py

--- tests/src/AML_Jobs/1p-BertJob.py
+++ tests/src/AML_Jobs/1p-BertJob.py
@@ -38,6 +38,9 @@ def get_sku_override():
 def set_next_trigger_model(queue):
     print ("In set_next_trigger_model...")
     model_list = list(queue['models'])
+    print(model_list)
+    print("****************")
+    print(queue['models'])
     #model_list = list(queue.models)
     index = queue['models'].index(test_model_name)
     print (f"index of {test_model_name} in queue: {index}")
