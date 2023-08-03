fcf4d33c8e1a8f45a8dda766e141d50e25f614b8
SameepPanigrahi <sameepkumar.1997@gmail.com>
2023-08-03 22:39:36 +0530

    Changing the registry_ml_client

--- tests/src/automation_for_constant_library/generic_initial_automation.py
+++ tests/src/automation_for_constant_library/generic_initial_automation.py
@@ -165,7 +165,7 @@ if __name__ == "__main__":
     InferenceAndDeployment = ModelInferenceAndDeployemnt(
         test_model_name=test_model_name,
         workspace_ml_client=workspace_ml_client,
-        registry_ml_client=registry_ml_client,
+        registry_ml_client=workspace_ml_client,
         registry=queue.registry
         )
     InferenceAndDeployment.model_infernce_deployment()
