24cb9d8bbe4d7feef64476bc1f8b010400bc2396
SameepPanigrahi <sameepkumar.1997@gmail.com>
2023-08-07 18:24:06 +0530

    Uncomment the necessary code

--- tests/src/automation_for_constant_library/generic_initial_automation.py
+++ tests/src/automation_for_constant_library/generic_initial_automation.py
@@ -170,11 +170,11 @@ if __name__ == "__main__":
             )
     mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
     compute_target = create_or_get_compute_target(workspace_ml_client)
-    # environment_variables = {"test_model_name": test_model_name, 
-    #        "subscription": queue.subscription,
-    #        "resource_group": queue.resource_group,
-    #        "workspace": queue.workspace
-    #        }
+    environment_variables = {"test_model_name": test_model_name, 
+           "subscription": queue.subscription,
+           "resource_group": queue.resource_group,
+           "workspace": queue.workspace
+           }
     command_job = run_azure_ml_job(code="./", command_to_run="python generic_model_download_and_register.py", environment="automate-venv:1", compute="STANDARD-D13", environment_variables=environment_variables)
     create_and_get_job_studio_url(command_job, workspace_ml_client)
 
