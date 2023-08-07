01fdf46b907e07546b10252dd14e330e98655325
SameepPanigrahi <sameepkumar.1997@gmail.com>
2023-08-07 13:39:46 +0530

    Fixing syntax error

--- tests/src/automation_for_constant_library/create_pipeline.py
+++ tests/src/automation_for_constant_library/create_pipeline.py
@@ -1,8 +1,8 @@
 class Pipeline:
     def __init__(self, import_model ) -> None :
         self.import_model  = import_model
-        
-    def create_pipeline(self, model_id, compute) - > dict :
+
+    def create_pipeline(self, model_id, compute) -> dict :
         import_model_job = self.import_model(model_id=model_id, compute=compute)
         # Set job to not continue on failure
         import_model_job.settings.continue_on_step_failure = False 
