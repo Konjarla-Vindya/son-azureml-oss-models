cfee3f2638c607cfc98f647cd0b82299ca1d4947
SameepPanigrahi <sameepkumar.1997@gmail.com>
2023-08-07 19:30:41 +0530

    Refactoring

--- tests/src/automation_for_constant_library/generic_initial_automation.py
+++ tests/src/automation_for_constant_library/generic_initial_automation.py
@@ -175,8 +175,8 @@ if __name__ == "__main__":
            "resource_group": queue.resource_group,
            "workspace": queue.workspace
            }
-    #command_job = run_azure_ml_job(code="./", command_to_run="python generic_model_download_and_register.py", environment="automate-venv:1", compute="STANDARD-D13", environment_variables=environment_variables)
-    #create_and_get_job_studio_url(command_job, workspace_ml_client)
+    command_job = run_azure_ml_job(code="./", command_to_run="python generic_model_download_and_register.py", environment="automate-venv:1", compute="STANDARD-D13", environment_variables=environment_variables)
+    create_and_get_job_studio_url(command_job, workspace_ml_client)
 
 
 
