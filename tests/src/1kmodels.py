4a52a54ffbd250984992b5e8a439ea0c32b5e02f
PoojaRam12 <139552501+PoojaRam12@users.noreply.github.com>
2023-08-08 15:35:03 +0530

    Update 1kmodels.py

--- tests/src/1kmodels.py
+++ tests/src/1kmodels.py
@@ -28,7 +28,7 @@ original_yaml_file = "../../.github/workflows/1kmodels.yml"
 #.github/workflows/1kmodels.yml
 
 # The directory where the generated YAML files will be stored
-output_directory = "../../.github/workflows/1kmodelsyml/"
+output_directory = "../../.github/workflows/1kmodelsyml"
 
 # List of 1000 model names
 #model_names = ['model1', 'model2', ..., 'model1000']
@@ -48,6 +48,7 @@ def generate_workflow_yaml(model_name):
 
     # Write the modified YAML content to the generated YAML file
     generated_yaml_path = os.path.join(output_directory, generated_yaml_filename)
+    print("Writing to:", generated_yaml_path)
     with open(generated_yaml_path, 'w') as f:
         f.write(yaml_content)
         print("code parsed")
