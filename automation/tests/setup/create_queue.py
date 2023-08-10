e54c7c68825132ce11726546962d29ff84c6ee34
Suchetha24 <136798869+Suchetha24@users.noreply.github.com>
2023-08-10 15:17:24 +0530

    Update create_queue.py

--- automation/tests/setup/create_queue.py
+++ automation/tests/setup/create_queue.py
@@ -203,13 +203,13 @@ def main():
     workflownames=["Abc"]
     j=0
     # while j < len(models):
-        for names in models:
-            workflow_modelname=names.replace('/','-')
-            print(f"workflow_modelname: {workflow_modelname}")
-            print("beforeworkflow names",workflownames)
-            workflownames.append(workflow_modelname)
-            print("in loop workflow names: {workflownames}")
-            print("total workflow names: {workflownames}")
+    for names in models:
+        workflow_modelname=names.replace('/','-')
+        print(f"workflow_modelname: {workflow_modelname}")
+        print("beforeworkflow names",workflownames)
+        workflownames.append(workflow_modelname)
+        print("in loop workflow names: {workflownames}")
+        print("total workflow names: {workflownames}")
     #     j=j+1
     print("out of loop workflow names: {workflownames}")
     
