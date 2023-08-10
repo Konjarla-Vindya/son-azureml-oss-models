21c932a79bc7fbad46717d3f200b321c9d8b3dec
Suchetha24 <136798869+Suchetha24@users.noreply.github.com>
2023-08-10 15:33:06 +0530

    Update create_queue.py

--- automation/tests/setup/create_queue.py
+++ automation/tests/setup/create_queue.py
@@ -200,7 +200,7 @@ def main():
     
     print (f"Found {len(models)} models")
     print (f"models: {models}")
-    # workflownames=[]
+    workflownames=[]
     j=1
     while j < len(models):
         for names in models:
