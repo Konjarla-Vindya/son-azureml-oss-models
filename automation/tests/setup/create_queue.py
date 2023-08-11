b5584eef0ea972d960fd6c985cd6df742dea4ce0
Suchetha24 <136798869+Suchetha24@users.noreply.github.com>
2023-08-11 21:08:13 +0530

    Update create_queue.py

--- automation/tests/setup/create_queue.py
+++ automation/tests/setup/create_queue.py
@@ -227,7 +227,7 @@ def write_single_workflow_file(model, q, secret_name):
     print("yml file----------------------------------------",workflow_file)
     # print(workflow_file['env']['test_queue'])
     print (f"Generating workflow file: {workflow_file}")
-    # os.system(f"cp {args.workflow_template} {workflow_file}")
+    os.system(f"cp {args.workflow_template} {workflow_file}")
     # os.system(f"sed -i s/name: distilbert-base-uncased/name: {workflowname}/g' {workflow_file}")
     # # replace <test_queue> with q
     # os.system(f"sed -i 's/test_queue: .*/test_queue: {q}/g' {workflow_file}")
