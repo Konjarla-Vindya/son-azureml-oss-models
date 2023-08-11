a071ec399e330cba3cf6ece6566e787a20a88579
Suchetha24 <136798869+Suchetha24@users.noreply.github.com>
2023-08-11 13:26:58 +0530

    Update create_queue.py

--- automation/tests/setup/create_queue.py
+++ automation/tests/setup/create_queue.py
@@ -233,7 +233,7 @@ def write_single_workflow_file(workflownames, q, secret_name):
     # replace <test_keep_looping> with test_keep_looping in workflow_file
     os.system(f"sed -i 's/<test_keep_looping>/{args.test_keep_looping}/g' {workflow_file}")
     # replace <test_model_name> with model_container.name in workflow_file
-    os.system(f"sed -i 's/<test_model_name>/{workflownames}/g' {workflow_file}")
+    os.system(f"sed -i 's/distilbert-base-uncased/{workflownames}/g' {workflow_file}")
     # replace <test_set> with test_set in workflow_file
     os.system(f"sed -i 's/<test_set>/{args.test_set}/g' {workflow_file}")
     # replace <test_secret_name> 
