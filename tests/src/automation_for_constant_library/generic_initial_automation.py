ae6f1c434b139d20d32aaa387e8ef8b0c2d19e91
SameepPanigrahi <v-sameepp@microsoft.com>
2023-08-11 11:50:52 +0530

    Refactoring

--- tests/src/automation_for_constant_library/generic_initial_automation.py
+++ tests/src/automation_for_constant_library/generic_initial_automation.py
@@ -175,8 +175,15 @@ if __name__ == "__main__":
            "resource_group": queue.resource_group,
            "workspace": queue.workspace
            }
-    latest_env = Environment.get(workspace=ws, name=queue.environment, version="latest")
-    print("Latest Environment Version:", latest_env.version)
+    #latest_env = Environment.get(workspace=ws, name=queue.environment, version="latest")
+    env_list = workspace_ml_client.environments.list(name=queue.environment)
+    latest_version = 0
+    for env in env_list:
+        if latest_version <= env.version:
+            latest_version = env.verion
+    print("Latest Environment Version:", latest_version)
+    latest_env = workspace_ml_client.environments.get(name=queue.environment, version=latest_version)
+    print("Latest Environment :", latest_env)
     # command_job = run_azure_ml_job(code="./", command_to_run="python generic_model_download_and_register.py", environment=latest_env, compute=queue.compute, environment_variables=environment_variables)
     # create_and_get_job_studio_url(command_job, workspace_ml_client)
 
