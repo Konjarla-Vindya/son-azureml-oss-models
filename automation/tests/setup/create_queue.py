81f33d85ba7b18a5abe155d12ea5cdefcdc7a280
Suchetha24 <136798869+Suchetha24@users.noreply.github.com>
2023-08-11 17:05:27 +0530

    Update create_queue.py

--- automation/tests/setup/create_queue.py
+++ automation/tests/setup/create_queue.py
@@ -281,7 +281,7 @@ def main():
     print("queue",queue)
     print (f"Created queues")
     # create queue files
-    # create_queue_files(queue, workspace_list)
+    create_queue_files(queue, workspace_list)
     print (f"Created queue files")
     # create workflow files
     create_workflow_files(q, workspace_list)
