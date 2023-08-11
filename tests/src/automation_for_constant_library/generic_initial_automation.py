f2becd86bf3731a434acba6606920bd04a68484d
SameepPanigrahi <v-sameepp@microsoft.com>
2023-08-11 12:51:52 +0530

    Changing to base version

--- tests/src/automation_for_constant_library/generic_initial_automation.py
+++ tests/src/automation_for_constant_library/generic_initial_automation.py
@@ -1,4 +1,4 @@
-from azureml.core import Workspace, Environment
+from azureml.core import Workspace
 #from generic_model_download_and_register import Model
 from model_inference_and_deployment import ModelInferenceAndDeployemnt
 from create_pipeline import Pipeline
@@ -175,17 +175,8 @@ if __name__ == "__main__":
            "resource_group": queue.resource_group,
            "workspace": queue.workspace
            }
-    #latest_env = Environment.get(workspace=ws, name=queue.environment, version="latest")
-    env_list = workspace_ml_client.environments.list(name=queue.environment)
-    latest_version = 0
-    for env in env_list:
-        if latest_version <= env.version:
-            latest_version = env.verion
-    print("Latest Environment Version:", latest_version)
-    latest_env = workspace_ml_client.environments.get(name=queue.environment, version=latest_version)
-    print("Latest Environment :", latest_env)
-    # command_job = run_azure_ml_job(code="./", command_to_run="python generic_model_download_and_register.py", environment=latest_env, compute=queue.compute, environment_variables=environment_variables)
-    # create_and_get_job_studio_url(command_job, workspace_ml_client)
+    command_job = run_azure_ml_job(code="./", command_to_run="python generic_model_download_and_register.py", environment="automate-venv:2", compute="Standard-DS3-v2", environment_variables=environment_variables)
+    create_and_get_job_studio_url(command_job, workspace_ml_client)
 
 
 
@@ -216,13 +207,13 @@ if __name__ == "__main__":
 
 
 
-    # InferenceAndDeployment = ModelInferenceAndDeployemnt(
-    #     test_model_name=test_model_name,
-    #     workspace_ml_client=workspace_ml_client,
-    #     registry_ml_client=workspace_ml_client,
-    #     registry=queue.registry
-    #     )
-    # InferenceAndDeployment.model_infernce_deployment()
+    InferenceAndDeployment = ModelInferenceAndDeployemnt(
+        test_model_name=test_model_name,
+        workspace_ml_client=workspace_ml_client,
+        registry_ml_client=workspace_ml_client,
+        registry=queue.registry
+        )
+    InferenceAndDeployment.model_infernce_deployment()
 
     # model = Model(model_name=test_model_name, queue=queue)
     # model_and_tokenizer = model.download_and_register_model(workspace=ws)
