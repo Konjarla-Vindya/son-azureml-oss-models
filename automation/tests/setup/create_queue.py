810329047bda9265a60cafb23e60379fb5028a54
Suchetha24 <136798869+Suchetha24@users.noreply.github.com>
2023-08-14 19:46:48 +0530

    Update create_queue.py

--- automation/tests/setup/create_queue.py
+++ automation/tests/setup/create_queue.py
@@ -26,8 +26,8 @@ parser.add_argument("--registry_name", type=str, default="HuggingFace")
 parser.add_argument("--environment", type=str, default="automate-venv")
 parser.add_argument("--compute", type=str, default="Standard-DS13-v2")
 # argument to specify Github workflow directory. can write to local dir for testing
-# !!! main workflow files will be overwritten if set to "../../.github/workflows" !!!
-parser.add_argument("--workflow_dir", type=str, default="../../.github/workflows")
+# !!! main workflow files will be overwritten if set to "../../../.github/workflows" !!!
+parser.add_argument("--workflow_dir", type=str, default="../../../.github/workflows")
 # argument to specify queue directory
 parser.add_argument("--queue_dir", type=str, default="../../tests/config/queue")
 # queue set name (will create a folder under queue_dir with this name)
@@ -236,38 +236,38 @@ def write_single_workflow_file(model, q, secret_name):
     print("name:---------------------",name)
     print("model:--------------------",model)
     print("workflowname:-------------",workflowname)
-    # os.system(f"mv -i s=<name>={model}=g' {workflow_file}")
-    # # replace <test_queue> with q
-    # os.system(f"sed -i 's/test_queue: .*/test_queue: {q}/g' {workflow_file}")
-    # # os.system(f"sed -i 's/test-norwayeast-02/{q}/g' {workflow_file}")
-    # # replace <test_sku_type> with test_sku_type in workflow_file
-    # os.system(f"sed -i 's/test_sku_type: .*/test_sku_type: {args.test_sku_type}/g' {workflow_file}")
-    # # replace <test_registry> with test_registry in workflow_file
-    # os.system(f"sed -i 's/test_trigger_next_model: .*/test_trigger_next_model: {args.test_trigger_next_model}/g' {workflow_file}")
-    # # replace <test_keep_looping> with test_keep_looping in workflow_file
-    # os.system(f"sed -i 's/test_keep_looping: .*/test_keep_looping: {args.test_keep_looping}/g' {workflow_file}")
-    # # replace <test_model_name> with model_container.name in workflow_file
-    # os.system(f"sed -i 's=test_model_name: .*=test_model_name: {model}=g' {workflow_file}")
-    # # replace <test_set> with test_set in workflow_file
-    # os.system(f"sed -i 's/test_set: .*/test_set: {args.test_set}/g' {workflow_file}")
-    # # replace <test_secret_name> 
-    # os.system(f"sed -i 's/test_secret_name: .*/test_secret_name: {secret_name}/g' {workflow_file}")
-    # Read in the file
+    os.system(f"mv -i s=<name>={model}=g' {workflow_file}")
+    # replace <test_queue> with q
+    os.system(f"sed -i 's/test_queue: .*/test_queue: {q}/g' {workflow_file}")
+    # os.system(f"sed -i 's/test-norwayeast-02/{q}/g' {workflow_file}")
+    # replace <test_sku_type> with test_sku_type in workflow_file
+    os.system(f"sed -i 's/test_sku_type: .*/test_sku_type: {args.test_sku_type}/g' {workflow_file}")
+    # replace <test_registry> with test_registry in workflow_file
+    os.system(f"sed -i 's/test_trigger_next_model: .*/test_trigger_next_model: {args.test_trigger_next_model}/g' {workflow_file}")
+    # replace <test_keep_looping> with test_keep_looping in workflow_file
+    os.system(f"sed -i 's/test_keep_looping: .*/test_keep_looping: {args.test_keep_looping}/g' {workflow_file}")
+    # replace <test_model_name> with model_container.name in workflow_file
+    os.system(f"sed -i 's=test_model_name: .*=test_model_name: {model}=g' {workflow_file}")
+    # replace <test_set> with test_set in workflow_file
+    os.system(f"sed -i 's/test_set: .*/test_set: {args.test_set}/g' {workflow_file}")
+    # replace <test_secret_name> 
+    os.system(f"sed -i 's/test_secret_name: .*/test_secret_name: {secret_name}/g' {workflow_file}")
+    Read in the file
     
-    with open(workflow_file, 'r') as f:
-        doc = yaml.load(f)
-    # doc['name'] = model
-    # # for model in models:
-    # doc['env']['test_model_name'] = model
-    # doc['env']['test_sku_type'] = args.test_sku_type
-    # doc['env']['test_trigger_next_model'] = args.test_trigger_next_model
-    # doc['env']['test_queue'] = q
-    # doc['env']['test_set'] = args.test_set
-    # doc['env']['test_queue'] = q
+    # with open(workflow_file, 'r') as f:
+    #     doc = yaml.load(f)
+    # # doc['name'] = model
+    # # # for model in models:
+    # # doc['env']['test_model_name'] = model
+    # # doc['env']['test_sku_type'] = args.test_sku_type
+    # # doc['env']['test_trigger_next_model'] = args.test_trigger_next_model
+    # # doc['env']['test_queue'] = q
+    # # doc['env']['test_set'] = args.test_set
+    # # doc['env']['test_queue'] = q
 
-    with open(workflow_file, 'w') as f:
-        yaml.dump(doc, f, default_flow_style=True, sort_keys=False,width=float("inf"))
-        # yaml.dump(doc, f, default_flow_style=True,width=float("inf"))
+    # with open(workflow_file, 'w') as f:
+    #     yaml.dump(doc, f, default_flow_style=True, sort_keys=False,width=float("inf"))
+    #     # yaml.dump(doc, f, default_flow_style=True,width=float("inf"))
 
 def workflow_names(models):
     workflownames=[]
