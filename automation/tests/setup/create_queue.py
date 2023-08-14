695ca02dfdc2ae1f6f48d168eb31f39aae86d02c
Suchetha24 <136798869+Suchetha24@users.noreply.github.com>
2023-08-14 14:19:03 +0530

    Update create_queue.py

--- automation/tests/setup/create_queue.py
+++ automation/tests/setup/create_queue.py
@@ -228,7 +228,7 @@ def write_single_workflow_file(model, q, secret_name):
     # print(workflow_file['env']['test_queue'])
     print (f"Generating workflow file: {workflow_file}")
     os.system(f"cp {args.workflow_template} {workflow_file}")
-    name=model.replace('/','//')
+    name='model'
     print("name:---------------------",name)
     print("model:--------------------",model)
     print("workflowname:-------------",workflowname)
@@ -243,7 +243,7 @@ def write_single_workflow_file(model, q, secret_name):
     # replace <test_keep_looping> with test_keep_looping in workflow_file
     os.system(f"sed -i 's/test_keep_looping: .*/test_keep_looping: {args.test_keep_looping}/g' {workflow_file}")
     # replace <test_model_name> with model_container.name in workflow_file
-    os.system(f"sed -i 's/test_model_name: .*/test_model_name: {workflowname}.replace('-','s')/g' {workflow_file}")
+    os.system(f"sed -i 's/test_model_name: .*/test_model_name: {workflowname}/g' {workflow_file}")
     # replace <test_set> with test_set in workflow_file
     os.system(f"sed -i 's/test_set: .*/test_set: {args.test_set}/g' {workflow_file}")
     # replace <test_secret_name> 
