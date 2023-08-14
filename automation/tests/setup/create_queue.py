d15c9ccf97654a5b2f4d67bf4b37951b191ff1c6
Suchetha24 <136798869+Suchetha24@users.noreply.github.com>
2023-08-15 00:28:08 +0530

    Update create_queue.py

--- automation/tests/setup/create_queue.py
+++ automation/tests/setup/create_queue.py
@@ -256,7 +256,7 @@ def write_single_workflow_file(model, q, secret_name):
     # os.system(f"sed -i 's/test_secret_name: .*/test_secret_name: {secret_name}/g' {workflow_file}")
     # # Read in the file
     
-    with open(workflow_file, 'r') as f:
+    with open(workflow_file, 'rt') as f:
         doc = yaml.safe_load(f)
     doc['name'] = model
     doc['onion']['workflow_dispatch']=
