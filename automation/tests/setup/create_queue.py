b8e77bb7758bdb7e6c7b12cfb1291d53c5aefa83
Suchetha24 <136798869+Suchetha24@users.noreply.github.com>
2023-08-14 13:43:18 +0530

    Update create_queue.py

--- automation/tests/setup/create_queue.py
+++ automation/tests/setup/create_queue.py
@@ -228,11 +228,11 @@ def write_single_workflow_file(model, q, secret_name):
     # print(workflow_file['env']['test_queue'])
     print (f"Generating workflow file: {workflow_file}")
     os.system(f"cp {args.workflow_template} {workflow_file}")
-    name=model
+    name=model.replace('/','r"/"')
     print("name:---------------------",name)
     print("model:--------------------",model)
     print("workflowname:-------------",workflowname)
-    os.system(f"sed -i s/<name>/{workflowname}/g' {workflow_file}")
+    os.system(f"sed -i s/<name>/{name}/g' {workflow_file}")
     # replace <test_queue> with q
     os.system(f"sed -i 's/test_queue: .*/test_queue: {q}/g' {workflow_file}")
     # os.system(f"sed -i 's/test-norwayeast-02/{q}/g' {workflow_file}")
@@ -243,7 +243,7 @@ def write_single_workflow_file(model, q, secret_name):
     # replace <test_keep_looping> with test_keep_looping in workflow_file
     os.system(f"sed -i 's/test_keep_looping: .*/test_keep_looping: {args.test_keep_looping}/g' {workflow_file}")
     # replace <test_model_name> with model_container.name in workflow_file
-    os.system(f"sed -i 's/test_model_name: .*/test_model_name: suchi/vindya-chiru/g' {workflow_file}")
+    os.system(f"sed -i 's/test_model_name: .*/test_model_name: {name}/g' {workflow_file}")
     # replace <test_set> with test_set in workflow_file
     os.system(f"sed -i 's/test_set: .*/test_set: {args.test_set}/g' {workflow_file}")
     # replace <test_secret_name> 
