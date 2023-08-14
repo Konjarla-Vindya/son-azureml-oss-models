f03ca88dcddf39596096b5887299fb58c93007ba
Suchetha24 <136798869+Suchetha24@users.noreply.github.com>
2023-08-14 20:24:15 +0530

    Update create_queue.py

--- automation/tests/setup/create_queue.py
+++ automation/tests/setup/create_queue.py
@@ -266,10 +266,10 @@ def write_single_workflow_file(model, q, secret_name):
     doc['env']['test_queue'] = q
 
     with open(workflow_file, 'w') as f:
-        yaml.dump(doc, f, default_flow_style=True, sort_keys=False,width=float("inf"))
-        yml=yaml.dump(doc, f, default_flow_style=True, sort_keys=False,width=float("inf"))
+       yml= yaml.dump(doc, f, default_flow_style=False, sort_keys=False,width=float("inf"))
+        # yml=yaml.dump(doc, f, default_flow_style=True, sort_keys=False,width=float("inf"))
         # yaml.dump(doc, f, default_flow_style=True,width=float("inf"))
-    workflow_filecopy=f"{args.workflow_dir}/{workflowname}.yml"
+    workflow_filecopy=f"{args.workflow_dir}/suchitest{workflowname}.yml"
     os.system(f"cp {yml} {workflow_filecopy}")
 def workflow_names(models):
     workflownames=[]
