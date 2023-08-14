ab0c6d641016896a7f7fe0955798523e1a3a8d8f
Suchetha24 <136798869+Suchetha24@users.noreply.github.com>
2023-08-15 00:04:42 +0530

    Update create_queue.py

--- automation/tests/setup/create_queue.py
+++ automation/tests/setup/create_queue.py
@@ -259,7 +259,7 @@ def write_single_workflow_file(model, q, secret_name):
     with open(workflow_file, 'r') as f:
         doc = yaml.load(f)
     doc['name'] = model
-    # doc['true']['workflow_dispatch']='namera'
+    # doc['True']['workflow_dispatch']='namera'
     # for model in models:
     doc['env']['test_model_name'] = model
     doc['env']['test_sku_type'] = args.test_sku_type
