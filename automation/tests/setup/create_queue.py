0afa1ef1f65a2841dc1e8ae90e4ca869e56c13d6
Suchetha24 <136798869+Suchetha24@users.noreply.github.com>
2023-08-14 13:49:11 +0530

    Update create_queue.py

--- automation/tests/setup/create_queue.py
+++ automation/tests/setup/create_queue.py
@@ -228,7 +228,7 @@ def write_single_workflow_file(model, q, secret_name):
     # print(workflow_file['env']['test_queue'])
     print (f"Generating workflow file: {workflow_file}")
     os.system(f"cp {args.workflow_template} {workflow_file}")
-    name=model.replace('/','r"/"')
+    name=model.replace('/','r/')
     print("name:---------------------",name)
     print("model:--------------------",model)
     print("workflowname:-------------",workflowname)
