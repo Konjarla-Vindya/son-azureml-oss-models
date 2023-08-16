483d62e7a24ffa0978f36738ea04688b658899cd
Suchetha24 <136798869+Suchetha24@users.noreply.github.com>
2023-08-16 21:02:05 +0530

    Update create_queue.py

--- automation/tests/setup/create_queue.py
+++ automation/tests/setup/create_queue.py
@@ -324,7 +324,7 @@ def write_single_workflow_file(model, q, secret_name):
         # yaml_content=yaml.safe_load(f)
     
     updated_yaml_content = yaml_content.replace("name: distl", "name: model")
-    yml_content=concat(""""",updated_yaml_content,""""")
+    yml_content=r"updated_yaml_content"
     print("updated_yaml_content-----------------------",updated_yaml_content)
     with open(workflow_file, 'w') as yaml_file:
         yaml_file.write(yml_content)
