66ba1d376d630b686dc9600439d2a6b4b3e17276
Suchetha24 <136798869+Suchetha24@users.noreply.github.com>
2023-08-16 15:45:22 +0530

    Update create_queue.py

--- automation/tests/setup/create_queue.py
+++ automation/tests/setup/create_queue.py
@@ -238,38 +238,38 @@ def write_single_workflow_file(model, q, secret_name):
     # # print("name:---------------------",name)
     # print("model:--------------------",model)
     # print("workflowname:-------------",workflowname)
-    # os.system(f"sed -i s/test_name: .*/name: {model}/g' {workflow_file}")
-    # # replace <test_queue> with q
-    # os.system(f"sed -i 's/test_queue: .*/test_queue: {q}/g' {workflow_file}")
-    # # os.system(f"sed -i 's/test-norwayeast-02/{q}/g' {workflow_file}")
-    # # replace <test_sku_type> with test_sku_type in workflow_file
-    # os.system(f"sed -i 's/test_sku_type: .*/test_sku_type: {args.test_sku_type}/g' {workflow_file}")
-    # # replace <test_registry> with test_registry in workflow_file
-    # os.system(f"sed -i 's/test_trigger_next_model: .*/test_trigger_next_model: {args.test_trigger_next_model}/g' {workflow_file}")
-    # # replace <test_keep_looping> with test_keep_looping in workflow_file
-    # os.system(f"sed -i 's/test_keep_looping: .*/test_keep_looping: {args.test_keep_looping}/g' {workflow_file}")
-    # # replace <test_model_name> with model_container.name in workflow_file
-    # os.system(f"sed -i 's=test_model_name: .*=test_model_name: {model}=g' {workflow_file}")
-    # # replace <test_set> with test_set in workflow_file
-    # os.system(f"sed -i 's/test_set: .*/test_set: {args.test_set}/g' {workflow_file}")
-    # # replace <test_secret_name> 
-    # os.system(f"sed -i 's/test_secret_name: .*/test_secret_name: {secret_name}/g' {workflow_file}")
+    os.system(f"sed -i s/name: .*/name: {model}/g' {workflow_file}")
+    # replace <test_queue> with q
+    os.system(f"sed -i 's/test_queue: .*/test_queue: {q}/g' {workflow_file}")
+    # os.system(f"sed -i 's/test-norwayeast-02/{q}/g' {workflow_file}")
+    # replace <test_sku_type> with test_sku_type in workflow_file
+    os.system(f"sed -i 's/test_sku_type: .*/test_sku_type: {args.test_sku_type}/g' {workflow_file}")
+    # replace <test_registry> with test_registry in workflow_file
+    os.system(f"sed -i 's/test_trigger_next_model: .*/test_trigger_next_model: {args.test_trigger_next_model}/g' {workflow_file}")
+    # replace <test_keep_looping> with test_keep_looping in workflow_file
+    os.system(f"sed -i 's/test_keep_looping: .*/test_keep_looping: {args.test_keep_looping}/g' {workflow_file}")
+    # replace <test_model_name> with model_container.name in workflow_file
+    os.system(f"sed -i 's=test_model_name: .*=test_model_name: {model}=g' {workflow_file}")
+    # replace <test_set> with test_set in workflow_file
+    os.system(f"sed -i 's/test_set: .*/test_set: {args.test_set}/g' {workflow_file}")
+    # replace <test_secret_name> 
+    os.system(f"sed -i 's/test_secret_name: .*/test_secret_name: {secret_name}/g' {workflow_file}")
     # # Read in the file
     
-    with open(workflow_file, 'rt') as f:
-        doc = yaml.safe_load(f)
-        # ,Loader=yaml.FullLoader
-    print("dict-----------------------",doc)
-    doc['name'] = model
-    # doc['onion']['workflow_dispatch']=
-    # for model in models:
-    doc['env']['test_model_name'] = model
-    doc['env']['test_sku_type'] = args.test_sku_type
-    doc['env']['test_trigger_next_model'] = args.test_trigger_next_model
-    doc['env']['test_queue'] = q
-    doc['env']['test_set'] = args.test_set
-    doc['env']['test_queue'] = q
-    print("post change of dict------------",doc)
+    # with open(workflow_file, 'rt') as f:
+    #     doc = yaml.safe_load(f)
+    #     # ,Loader=yaml.FullLoader
+    # print("dict-----------------------",doc)
+    # doc['name'] = model
+    # # doc['onion']['workflow_dispatch']=
+    # # for model in models:
+    # doc['env']['test_model_name'] = model
+    # doc['env']['test_sku_type'] = args.test_sku_type
+    # doc['env']['test_trigger_next_model'] = args.test_trigger_next_model
+    # doc['env']['test_queue'] = q
+    # doc['env']['test_set'] = args.test_set
+    # doc['env']['test_queue'] = q
+    # print("post change of dict------------",doc)
     # # doc.replace('true:','on:')
     # # os.system(f"sed -i 's/true: .*/on: .*/g' {workflow_file}")
     with open(workflow_file, 'w') as f:
