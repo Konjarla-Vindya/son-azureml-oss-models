7934f82de466c076fecae09dab73bde006169ba2
Amarah <83214142+amarahAI@users.noreply.github.com>
2023-08-18 15:37:28 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -6,7 +6,7 @@ from github import Github, Auth
 
 class dashboard():
     def __init__(self): 
-        self.github_token = os.environ["GITHUB_TOKEN"]
+        self.github_token = os.environ["WORKFLOW_TOKEN"]
         self.token = Auth.Token(self.github_token)
         self.auth = Github(auth=self.token)
 
