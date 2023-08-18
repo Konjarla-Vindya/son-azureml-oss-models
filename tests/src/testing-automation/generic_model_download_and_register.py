7dbdc4d905f6a27e7f0590dfd6c979a810e379ee
SameepPanigrahi <v-sameepp@microsoft.com>
2023-08-18 18:17:07 +0530

    Adding comments and updating the docstring

--- tests/src/testing-automation/generic_model_download_and_register.py
+++ tests/src/testing-automation/generic_model_download_and_register.py
@@ -37,6 +37,7 @@ class Model:
     def get_library_to_load_model(self, task) -> str:
         """ Takes the task name and load the  json file findout the library 
         which is applicable for that task and retyrun it 
+
         Args:
             task (str): required the task name 
         Returns:
@@ -52,19 +53,36 @@ class Model:
         return model_with_library.get(task)
 
     def get_task(self) -> str:
+        """ This method will read the huggin face api url data in a dataframe. Then it will findout 
+        the model which is of transformer type . Then it will find that particular model and its task
+
+        Returns:
+            str: task name
+        """
         response = urlopen(URL)
+        #Load all the data with the help of json
         data_json = json.loads(response.read())
+        # Convert it into dataframe and mention the specific column
         df = pd.DataFrame(data_json, columns=COLUMNS_TO_READ)
+        # Find the data with the model which will be having trasnfomer tag
         df = df[df.tags.apply(lambda x: STRING_TO_CHECK in x)]
+        # Find the data with that particular name
         required_data = df[df.modelId.apply(lambda x: x == self.model_name)]
+        # Get the task 
         required_data = required_data["pipeline_tag"].to_string()
         pattern = r'[0-9\s+]'
         final_data = re.sub(pattern, '', required_data)
         return final_data
 
-    def get_sample_input_data(self, task):
-        #final_data = self.get_task_and_sample_data()
-        #task = final_data
+    def get_sample_input_data(self, task:str):
+        """This method will load the sample input data based on the task name
+
+        Args:
+            task (str): task name
+
+        Returns:
+            _type_: _description_
+        """
         scoring_file = f"sample_inputs/{task}.json"
         # check of scoring_file exists
         try:
@@ -77,7 +95,16 @@ class Model:
 
         return scoring_input
 
-    def download_model_and_tokenizer(self, task) -> dict:
+    def download_model_and_tokenizer(self, task:str) -> dict:
+        """" This method will download the model and tokenizer and return it in a 
+        dictionary
+
+        Args:
+            task (str): task name
+
+        Returns:
+            dict: model and tokenizer
+        """
         # model_detail = AutoConfig.from_pretrained(self.model_name)
         # res_dict = model_detail.to_dict().get("architectures")
         # if res_dict is not None:
@@ -86,35 +113,49 @@ class Model:
         #     rare_model_dict = self.load_rare_model()
         #     model_library_name = rare_model_dict.get(self.model_name)
         #model_library_name = model_detail.to_dict()["architectures"][0]
+        
+        # Get the library name from this method from which we will load the model
         model_library_name = self.get_library_to_load_model(task=task)
         print("Library name is this one : ", model_library_name)
+        # Load the library from the transformer 
         model_library = getattr(transformers, model_library_name)
+        # From the library load the model
         model = model_library.from_pretrained(self.model_name)
         tokenizer = AutoTokenizer.from_pretrained(self.model_name)
         model_and_tokenizer = {"model": model, "tokenizer": tokenizer}
         return model_and_tokenizer
 
-    def register_model_in_workspace(self, model_and_tokenizer, sample_data, task):
-        #task = self.queue.models[self.model_name].task
+    def register_model_in_workspace(self, model_and_tokenizer:dict, sample_data:ConfigBox, task:str):
+        """ I will load the pipeline with the model name if its a fill mask task then it will get the 
+        masked token and convert the input to that model type . It will generate the model signature . 
+        It will log and register the model with mlflow
+
+        Args:
+            model_and_tokenizer (dict): contains model and tokenizer
+            sample_data (ConfigBox): contains the data
+            task (str): task name
+        """
+        # Load the transformer pipeline with the help of model and task
         model_pipeline = transformers.pipeline(
             task=task, model=self.model_name)
+        # If the task is fill-mask then get the mask_token and replace the input data with that mask token
         if task == "fill-mask":
             pipeline_tokenizer = model_pipeline.tokenizer
             for index in range(len(sample_data.inputs)):
                 sample_data.inputs[index] = sample_data.inputs[index].replace(
                     "<mask>", pipeline_tokenizer.mask_token).replace("[MASK]", pipeline_tokenizer.mask_token)
 
+        # Generate the transformer model output for that particular model
         output = generate_signature_output(model_pipeline, sample_data.inputs)
+        # It will infer the signature directly from input and output
         signature = infer_signature(sample_data.inputs, output)
+        # If threr will be model namr with / then replace it
         model_name = self.model_name.replace("/", "-")
-        # if len(self.model_name) > 22:
-        #     model_name = self.model_name.replace("/", "-")[:22]
-        #     model_name = model_name.rstrip("-")
-        # else:
-        #     model_name = self.model_name
         artifact_path = model_name + "-artifact"
         registered_model_name = model_name
         # mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
+
+        # With the help of mlflow log and register the model in the workspace
         mlflow.transformers.log_model(
             transformers_model=model_and_tokenizer,
             task=task,
@@ -125,10 +166,19 @@ class Model:
         )
 
     def download_and_register_model(self) -> dict:
+        """ This method will be controlling all execution of methods 
+
+        Returns:
+            dict: _description_
+        """
+        # Get the task name
         task = self.get_task()
         print("This is the task associated to the model : ", task)
+        # Get the model and tokenizer
         model_and_tokenizer = self.download_model_and_tokenizer(task=task)
+        # Get the sample input data
         sample_data = self.get_sample_input_data(task=task)
+        # Register the model in the workspace
         self.register_model_in_workspace(
             model_and_tokenizer=model_and_tokenizer,
             sample_data=sample_data,
