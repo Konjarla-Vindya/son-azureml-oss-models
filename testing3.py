4accadb0e2935bc8de3ec4f699922400caaa3be1
SwathiPriyaRV <66841534+Swathi23345@users.noreply.github.com>
2023-08-22 19:22:44 +0530

    Update testing3.py

--- testing3.py
+++ testing3.py
@@ -15,29 +15,6 @@ class Dashboard():
             "updated_at": [], "status": [], "conclusion": [], "badge": []
         }
         
-    def fetch_all_workflows(self):
-        query = """
-        query {
-          repository(owner: "Konjarla-Vindya", name: "son-azureml-oss-models") {
-            workflows(first: 100) {
-              nodes {
-                name
-              }
-            }
-          }
-        }
-        """
-        headers = {
-            "Authorization": f"Bearer {self.github_token}",
-            "Accept": "application/vnd.github.v4+json"
-        }
-        response = requests.post("https://api.github.com/graphql", json={"query": query}, headers=headers)
-        response.raise_for_status()
-        
-        workflows = response.json()["data"]["repository"]["workflows"]["nodes"]
-        workflow_names = [workflow["name"] for workflow in workflows]
-        return workflow_names
-        
     def workflow_last_run(self):
         headers = {
             "Authorization": f"Bearer {self.github_token}",
@@ -45,7 +22,27 @@ class Dashboard():
             "Accept": "application/vnd.github+json"
         }
         
-        workflows_to_include = self.fetch_all_workflows()
+        workflows_to_include = [
+            "CAMeL-Lab-bert-base-arabic-camelbert-ca-sentiment.yml",
+            "Helsinki-NLP-opus-mt-trk-en.yml",
+            "Helsinki-NLP-opus-mt-en-bem.yml",
+            "Helsinki-NLP-opus-mt-it-bg.yml",
+            "SauravMaheshkar-clr-finetuned-albert-base.yml",
+            "DoyyingFace-bert-asian-hate-tweets-asian-unclean-freeze-4.yml",
+            "Helsinki-NLP-opus-mt-en-bi.yml",
+            "Helsinki-NLP-opus-mt-ja-pt.yml",
+            "Helsinki-NLP-opus-mt-en-bzs.yml",
+            "Helsinki-NLP-opus-mt-en-ht.yml",
+            "Geotrend-bert-base-uk-cased.yml",
+            "Helsinki-NLP-opus-mt-sv-uk.yml",
+            "Helsinki-NLP-opus-mt-es-zai.yml",
+            "EasthShin-Android_Ios_Classification.yml",
+            "Helsinki-NLP-opus-mt-en-lua.yml",
+
+         
+            "ahotrod-electra_large_discriminator_squad2_512.yml",
+            "ARTeLab-it5-summarization-fanpage.yml"
+        ]
 
         for workflow_name in workflows_to_include:
             try:
@@ -59,6 +56,7 @@ class Dashboard():
                 
                 lastrun = runs["workflow_runs"][0]
                 badgeurl = f"https://github.com/{self.repo_full_name}/actions/workflows/{workflow_name}/badge.svg"
+                runurl = "https://github.com/{}/actions/runs/{}/job/{}".format(self.repo_full_name,lastrun["id"],job["jobs"][0]["id"])
 
                 self.data["workflow_id"].append(lastrun["workflow_id"])
                 self.data["workflow_name"].append(workflow_name.replace(".yml", ""))
@@ -67,13 +65,13 @@ class Dashboard():
                 self.data["updated_at"].append(lastrun["updated_at"])
                 self.data["status"].append(lastrun["status"])
                 self.data["conclusion"].append(lastrun["conclusion"])
-                self.data["badge"].append(f"[![{workflow_name}]({badgeurl})]({badgeurl.replace('/badge.svg', '')})")
+                #self.data["badge"].append(f"[![{workflow_name}]({badgeurl})]({badgeurl.replace('/badge.svg', '')})")
+                self.data["badge"].append("[![{}]({})]({})".format(self.workflow_name_ext,self.badgeurl,self.runurl ))
             except requests.exceptions.RequestException as e:
                 print(f"An error occurred while fetching run information for workflow '{workflow_name}': {e}")
 
         return self.data
 
-    # ... rest of the class methods ...
     def results(self, last_runs_dict):
         results_dict = {"total": 0, "success": 0, "failure": 0, "cancelled": 0, "not_tested": 0, "total_duration": 0}
         summary = []
@@ -103,6 +101,8 @@ class Dashboard():
             f.write(os.linesep)
             f.write(os.linesep)
             f.write(models_md)
+       
+
 def main():
     my_class = Dashboard()
     last_runs_dict = my_class.workflow_last_run()
