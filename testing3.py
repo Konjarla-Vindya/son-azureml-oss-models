03df01d7b70718a80f2363c405db6b0f5c1c62c2
SwathiPriyaRV <66841534+Swathi23345@users.noreply.github.com>
2023-08-24 17:32:58 +0530

    Update testing3.py

--- testing3.py
+++ testing3.py
@@ -1,84 +1,77 @@
-import github
-import requests
 import os
+import requests
 import pandas
 from github import Github, Auth
-import sys
 
-class dashboard():
+class Dashboard():
     def __init__(self): 
         self.github_token = os.environ["GIT_TOKEN"]
-        
         self.token = Auth.Token(self.github_token)
         self.auth = Github(auth=self.token)
-
         self.repo = self.auth.get_repo("Konjarla-Vindya/son-azureml-oss-models")
         self.repo_full_name = self.repo.full_name
-        self.dict = {"workflow_id": [], "workflow_name": [], "last_runid": [], "created_at": [], "updated_at": [], "status": [], "conclusion": [], "badge": []}
+        self.data = {
+            "workflow_id": [], "workflow_name": [], "last_runid": [], "created_at": [],
+            "updated_at": [], "status": [], "conclusion": [], "badge": []
+        }
         
-        self.workflow_path = ".github/workflows/"
-
+    def get_all_workflow_names(self):
+        headers = {
+            "Authorization": f"Bearer {self.github_token}",
+            "Accept": "application/vnd.github.v3+json"
+        }
+        response = requests.get(f"https://api.github.com/repos/{self.repo_full_name}/actions/workflows", headers=headers)
+        response.raise_for_status()
+        
+        workflows = response.json()
+        workflow_name = [workflow["name"] for workflow in workflows["workflows"]]
+        print(workflow_name)
+        return workflow_name
+        
     def workflow_last_run(self):
-        workflows = self.repo.get_workflows()
-        headers = {"Authorization": f"Bearer {self.github_token}",
-                   "X-GitHub-Api-Version": "2022-11-28",
-                   "Accept": "application/vnd.github+json"}
+        headers = {
+            "Authorization": f"Bearer {self.github_token}",
+            "X-GitHub-Api-Version": "2022-11-28",
+            "Accept": "application/vnd.github+json"
+        }
         
-        for workflow in workflows:
-            workflow_temp = workflow.name.replace(".github/workflows/", "")
-            print("workflow is this :  ", workflow)
-            workflow_temp = workflow_temp + ".yml"
-            # if workflow_name != "":
-            #     continue
+        workflows_to_include = self.get_all_workflow_names()
+        normalized_workflows = [workflow_name.replace("/", "-") for workflow_name in workflows_to_include]
 
-            workflow_name = workflow_temp.replace("/", "-")
-            print("Final workflow_name : ", workflow_name)
-            
+
+        for workflow_name in normalized_workflows:
             try:
-                response = requests.get(f"https://api.github.com/repos/{self.repo_full_name}/actions/workflows/{workflow_name}/runs", headers=headers)
-                response.raise_for_status()  # Raise an error if the response status code is not successful
+                response = requests.get(f"https://api.github.com/repos/{self.repo_full_name}/actions/workflows/{workflow_name}.yml/runs", headers=headers)
+                response.raise_for_status()
                 
                 runs = response.json()
-                #print(runs)
-                print("Type is : ",type(runs["workflow_runs"]))
-                print("Length is this : ", len(runs["workflow_runs"]))
+                if not runs["workflow_runs"]: 
+                    print(f"No runs found for workflow '{workflow_names}'. Skipping...")
+                    continue
                 if len(runs["workflow_runs"]) != 0:
-                    #continue
                     lastrun = runs["workflow_runs"][0]
-                    self.workflow_name_ext = lastrun["name"].replace(self.workflow_path, "")
-                    badgeurl = f"https://github.com/{self.repo_full_name}/actions/workflows/{workflow_name}/badge.svg"
-                    id = lastrun['id']
-                    job_id_url = f"https://github.com/{self.repo_full_name}/actions/runs/{id}/jobs"
-                    job_details = requests.get(job_id_url)
-                    job_details.raise_for_status()
+                    jobresponse = requests.get("https://api.github.com/repos/{}/actions/runs/{}/jobs".format(self.repo_full_name,lastrun["id"]), headers = headers) 
+                    job = jobresponse.json()
+                    print(job["jobs"][0]["id"])
                     
-                    job_detail_json = job_details.json()
-                    job_detail = job_detail_json["jobs"][0]
-                    job_id = job_detail["id"]
-                    final_job_url = f"https://github.com/{self.repo_full_name}/actions/runs/{id}/job/{job_id}"
-
-                    self.dict["workflow_id"].append(lastrun["workflow_id"])
-                    self.dict["workflow_name"].append(self.workflow_name_ext.replace(".yml", ""))
-                    self.dict["last_runid"].append(lastrun["id"])
-                    self.dict["created_at"].append(lastrun["created_at"])
-                    self.dict["updated_at"].append(lastrun["updated_at"])
-                    self.dict["status"].append(lastrun["status"])
-                    self.dict["conclusion"].append(lastrun["conclusion"])
-                    #self.dict["badge"].append(f"[![{workflow_name}]({badgeurl})]({badgeurl.replace('/badge.svg', '')})")
-                    self.dict["badge"].append(f"[![{workflow_name}]({final_job_url})]({badgeurl.replace('/badge.svg', '')})")
-
+                    badgeurl = f"https://github.com/{self.repo_full_name}/actions/workflows/{workflow_name}/badge.svg"
+                    runurl = "https://github.com/{}/actions/runs/{}/job/{}".format(self.repo_full_name,lastrun["id"],job["jobs"][0]["id"])
+    
+                    self.data["workflow_id"].append(lastrun["workflow_id"])
+                    self.data["workflow_name"].append(workflow_name.replace(".yml", ""))
+                    self.data["last_runid"].append(lastrun["id"])
+                    self.data["created_at"].append(lastrun["created_at"])
+                    self.data["updated_at"].append(lastrun["updated_at"])
+                    self.data["status"].append(lastrun["status"])
+                    self.data["conclusion"].append(lastrun["conclusion"])
+                    #self.data["badge"].append(f"[![{workflow_name}]({badgeurl})]({badgeurl.replace('/badge.svg', '')})")
+                    self.data["badge"].append("[![{}]({})]({})".format(workflow_name,badgeurl,runurl))
             except requests.exceptions.RequestException as e:
-                _, _, exc_tb = sys.exc_info()
                 print(f"An error occurred while fetching run information for workflow '{workflow_name}': {e}")
-                print(f"The exception occured at this line no : {exc_tb.tb_lineno} ")
-            break
 
+        return self.data
 
-        return self.dict
-
-
-
-    def results(self,last_runs_dict):
+    def results(self, last_runs_dict):
         results_dict = {"total": 0, "success": 0, "failure": 0, "cancelled": 0, "not_tested": 0, "total_duration": 0}
         summary = []
 
@@ -92,30 +85,27 @@ class dashboard():
         failure_rate = results_dict["failure"]/results_dict["total"]*100.00
         cancel_rate = results_dict["cancelled"]/results_dict["total"]*100.00
 
-            
         summary.append("🚀Total|✅Success|❌Failure|🚫Cancelled|")
         summary.append("-----|-------|-------|-------|")
         summary.append(f"{results_dict['total']}|{results_dict['success']}|{results_dict['failure']}|{results_dict['cancelled']}|")
-        summary.append(f"100.0%|{success_rate}|{failure_rate}|{cancel_rate}|")
+        summary.append(f"100.0%|{success_rate:.2f}%|{failure_rate:.2f}%|{cancel_rate:.2f}%|")
 
-        models = {"Model": last_runs_dict["workflow_name"],"Status": last_runs_dict["badge"]}
+        models = {"Model": last_runs_dict["workflow_name"], "Status": last_runs_dict["badge"]}
         models_md = pandas.DataFrame.from_dict(models).to_markdown()
 
-        summary_text = ""
-        for row in summary:
-            summary_text += row + "\n"
+        summary_text = "\n".join(summary)
 
-        with open("testing2.md", "w", encoding="utf-8") as f:
+        with open("testing.md", "w", encoding="utf-8") as f:
             f.write(summary_text)
             f.write(os.linesep)
             f.write(os.linesep)
             f.write(models_md)
+       
 
 def main():
-    my_class = dashboard()
+    my_class = Dashboard()
     last_runs_dict = my_class.workflow_last_run()
     my_class.results(last_runs_dict)
-    
 
 if __name__ == "__main__":
     main()
