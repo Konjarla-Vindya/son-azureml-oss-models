0bf82c53c82520b11c6e88be8d585397538ab06a
SwathiPriyaRV <66841534+Swathi23345@users.noreply.github.com>
2023-08-25 00:31:29 +0530

    Update testing3.py

--- testing3.py
+++ testing3.py
@@ -51,7 +51,7 @@ class Dashboard():
                 if len(runs["workflow_runs"]) != 0:
                     lastrun = runs["workflow_runs"][0]
                     URL_1 = f"https://github.com/repos/{self.repo_full_name}/actions/runs/{lastrun['id']}/jobs"
-                    jobresponse = requests.get(URL) 
+                    jobresponse = requests.get(url) 
                     print("URL : ",URL_1)
                     print("URL : ",url)
                     job = jobresponse.json()
