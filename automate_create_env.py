12f3c457f4d956195d9d427f073346ae2a254bf9
priyaray21 <139554535+priyaray21@users.noreply.github.com>
2023-08-26 15:20:49 +0530

    Update automate_create_env.py

--- automate_create_env.py
+++ automate_create_env.py
@@ -15,11 +15,20 @@ conda_content = StringIO(response.text)
 response = requests.get(CONFIG_JSON_URL)
 response.raise_for_status()
 config_content = response.json()
-
 # Connect to Azure ML Workspace using the fetched config.json
-ws = Workspace.from_config(path=config_content)
+
+ subscription_id: "80c77c76-74ba-4c8c-8229-4c3b2957990c"
+ resource_group: "huggingface-registry-test1"
+ workspace_name: "test-eastus"
+
+ws = Workspace(
+    subscription_id=config_content.get(subscription_id),
+    resource_group=config_content.get(resource_group),
+    workspace_name=config_content.get(workspace_name)
+)
+
 
 # Create and register the environment using the fetched conda.yaml
-env_name = "automate-create-env.py"  
+env_name = "automate-create-env"  
 testenv = Environment.from_stream(workspace=ws, name=env_name, stream=conda_content)
 testenv.register(workspace=ws)
