0e73fa5e64bb5eb09ca58dca11ec0cb964609f0b
SwathiPriyaRV <66841534+Swathi23345@users.noreply.github.com>
2023-08-27 21:12:18 +0530

    Update testing3.py

--- testing3.py
+++ testing3.py
@@ -78,7 +78,7 @@ class Dashboard():
                         self.data["badge"].append("[![{}]({})]({})".format(workflow_name,badgeurl,html_url))
                         self.data["jobs_url"].append(html_url)
                     else:
-                        self.data["badge"].append("[![{}]({})]".format(workflow_name,badgeurl))
+                        self.data["badge"].append("[![{}]({})]".format(workflow_name,badgeurl,workflow_runs))
                         self.data["jobs_url"].append(workflow_runs)
             except requests.exceptions.RequestException as e:
                 print(f"An error occurred while fetching run information for workflow '{workflow_name}': {e}")
