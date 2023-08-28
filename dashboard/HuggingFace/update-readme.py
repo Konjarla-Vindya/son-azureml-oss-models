c767072797acb3fbb1773de22295e9f1c06c2110
SwathiPriyaRV <66841534+Swathi23345@users.noreply.github.com>
2023-08-28 16:57:05 +0530

    Update update-readme.py

--- dashboard/HuggingFace/update-readme.py
+++ dashboard/HuggingFace/update-readme.py
@@ -20,7 +20,7 @@ class Dashboard():
             "Authorization": f"Bearer {self.github_token}",
             "Accept": "application/vnd.github.v3+json"
         }
-        response = requests.get(f"https://api.github.com/repos/{self.repo_full_name}/actions/workflows?per_page=100", headers=headers)
+        response = requests.get(f"https://api.github.com/repos/{self.repo_full_name}/actions/workflows", headers=headers)
         response.raise_for_status()
         
         workflows = response.json()
