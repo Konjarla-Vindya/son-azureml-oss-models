8410377e36f4bc30f4dc727e561fef6e83a73219
priyaray21 <139554535+priyaray21@users.noreply.github.com>
2023-08-29 01:50:37 +0530

    Update automate_create_env.py

--- automate_create_env.py
+++ automate_create_env.py
@@ -1,34 +1,30 @@
-import os
-import requests
-from azureml.core import Workspace, Environment
+import yaml
 
-# Define your Azure ML workspace details
-subscription_id = "80c77c76-74ba-4c8c-8229-4c3b2957990c"
-resource_group = "huggingface-registry-test1"
-workspace_name = "test-eastus"
+def create_conda_yaml(channels, conda_dependencies, pip_dependencies, env_name):
+    # Define the environment
+    environment = {
+        'name': env_name,
+        'channels': channels,
+        'dependencies': conda_dependencies + [{'pip': pip_dependencies}]
+    }
 
-# URLs for GitHub raw content
-CONDA_YAML_URL = 'https://raw.githubusercontent.com/Konjarla-Vindya/son-azureml-oss-models/main/.github/conda.yml'
+    # Write to a YAML file
+    with open("conda.yaml", "w") as file:
+        yaml.safe_dump(environment, file)
+# Usage
+channels = ["conda-forge", "defaults"]
+conda_dependencies = ["python=3.10", "numpy", "pandas", "pip"]
+pip_dependencies = [
+   "azureml-mlflow","azureml-core","ipython","datasets","accelerate==0.21.0",
+   "ipykernel","evaluate","azure-ai-ml","numpy","tensorflow==2.9","mlflow==2.4",
+   "cffi==1.15.1","dill==0.3.6","google-api-core==2.11.0","ipython==8.8.0",
+   "numpy==1.23.5","packaging==21.3","protobuf==3.20.3","pyyaml==6.0",
+   "requests==2.28.2","safetensors==0.3.1","scikit-learn==1.2.2","scipy==1.10.1",
+   "torch==2.0.1","torchvision==0.15.2","transformers==4.29.0",
+   "xformers==0.0.20","azureml-mlflow==1.52.0","azure-core==1.27.1","torchvision",
+   "sacremoses","python-box","sentencepiece","fugashi[unidic-lite]"
+   ]
+   
+env_name = "auto_testenv"
 
-# Download conda.yaml file from GitHub
-response = requests.get(CONDA_YAML_URL)
-response.raise_for_status()
-with open('conda.yaml', 'wb') as f:
-    f.write(response.content)
-
-# Connect to Azure ML Workspace
-ws = Workspace(
-    subscription_id=subscription_id,
-    resource_group=resource_group,
-    workspace_name=workspace_name
-)
-
-# Create and register Azure ML Environment from conda.yaml
-env_name = "automate-create-env"
-env = Environment.from_conda_specification(
-    name=env_name,
-    file_path="conda.yaml"
-)
-env.register(workspace=ws)
-
-print(f"Environment {env_name} has been created and registered.")
+create_conda_yaml(channels, conda_dependencies, pip_dependencies, env_name)
