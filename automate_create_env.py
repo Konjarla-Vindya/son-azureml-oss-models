6e42d6aa8299b0d7171c3ce0a81b3dcc0cc01c95
priyaray21 <139554535+priyaray21@users.noreply.github.com>
2023-08-28 15:32:24 +0530

    Update automate_create_env.py

--- automate_create_env.py
+++ automate_create_env.py
@@ -3,17 +3,17 @@ from azureml.core import Workspace, Environment
 from azureml.core.authentication import ServicePrincipalAuthentication
 
 # Retrieve environment variables
-tenant_id = os.environ['AZURE_TENANT_ID']
-client_id = os.environ['AZURE_CLIENT_ID']
-client_secret = os.environ['AZURE_CLIENT_SECRET']
-subscription_id = os.environ['AZURE_SUBSCRIPTION_ID']
+#tenant_id = os.environ['AZURE_TENANT_ID']
+#client_id = os.environ['AZURE_CLIENT_ID']
+#client_secret = os.environ['AZURE_CLIENT_SECRET']
+#subscription_id = os.environ['AZURE_SUBSCRIPTION_ID']
 
 # Service principal authentication
-sp_auth = ServicePrincipalAuthentication(
-    tenant_id=tenant_id,
-    service_principal_id=client_id,
-    service_principal_password=client_secret
-)
+#sp_auth = ServicePrincipalAuthentication(
+    #tenant_id=tenant_id,
+    #service_principal_id=client_id,
+    #service_principal_password=client_secret
+#)
 
 # Connect to existing Azure ML Workspace
 subscription_id = "80c77c76-74ba-4c8c-8229-4c3b2957990c"
