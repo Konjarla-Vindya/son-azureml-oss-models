9dc80b81a990077affccc3acac0e417719508d51
priyaray21 <139554535+priyaray21@users.noreply.github.com>
2023-08-28 16:29:14 +0530

    Update automate_create_env.py

--- automate_create_env.py
+++ automate_create_env.py
@@ -1,36 +1,34 @@
 import os
+import requests
 from azureml.core import Workspace, Environment
-from azureml.core.authentication import ServicePrincipalAuthentication
 
-# Retrieve environment variables
-#tenant_id = os.environ['AZURE_TENANT_ID']
-#client_id = os.environ['AZURE_CLIENT_ID']
-#client_secret = os.environ['AZURE_CLIENT_SECRET']
-#subscription_id = os.environ['AZURE_SUBSCRIPTION_ID']
-
-# Service principal authentication
-#sp_auth = ServicePrincipalAuthentication(
-    #tenant_id=tenant_id,
-    #service_principal_id=client_id,
-    #service_principal_password=client_secret
-#)
-
-# Connect to existing Azure ML Workspace
+# Define your Azure ML workspace details
 subscription_id = "80c77c76-74ba-4c8c-8229-4c3b2957990c"
 resource_group = "huggingface-registry-test1"
 workspace_name = "test-eastus"
 
+# URLs for GitHub raw content
+CONDA_YAML_URL = 'https://raw.githubusercontent.com/Konjarla-Vindya/son-azureml-oss-models/main/.github/conda.yml'
+
+# Download conda.yaml file from GitHub
+response = requests.get(CONDA_YAML_URL)
+response.raise_for_status()
+with open('conda.yml', 'wb') as f:
+    f.write(response.content)
+
+# Connect to Azure ML Workspace
 ws = Workspace(
     subscription_id=subscription_id,
     resource_group=resource_group,
     workspace_name=workspace_name
 )
 
-# Create and register the environment
+# Create and register Azure ML Environment from conda.yaml
 env_name = "automate-create-env"
-
 env = Environment.from_conda_specification(
     name=env_name,
-    file_path= "../Konjarla-Vindya/son-azureml-oss-models/blob/main/.github/conda.yml"  
+    file_path="conda.yml"
 )
 env.register(workspace=ws)
+
+print(f"Environment {env_name} has been created and registered.")
