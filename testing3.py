a8fcce59e624c309872ab2c686f9b143a664b722
SwathiPriyaRV <66841534+Swathi23345@users.noreply.github.com>
2023-08-29 09:18:49 +0530

    Update testing3.py

--- testing3.py
+++ testing3.py
@@ -50,7 +50,7 @@ class Dashboard():
                 offset+=limit
         
             # If this is 0, we'll exit the while loop.
-            results_len = len(workflows["workflows"]) 
+                results_len = len(workflows["workflows"]) 
         # response = requests.get(f"https://api.github.com/repos/{self.repo_full_name}/actions/workflows?per_page=50", headers=headers)
         # response.raise_for_status()
         
