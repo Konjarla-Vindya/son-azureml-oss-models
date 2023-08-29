c2c2a566c69b4714d2eeee83fee00698ca07364a
SwathiPriyaRV <66841534+Swathi23345@users.noreply.github.com>
2023-08-29 22:42:39 +0530

    Update testing4.py

--- testing4.py
+++ testing4.py
@@ -144,8 +144,8 @@ def create_badge(results_per_model, status, clock_time):
 
 def main():
     runs = get_github_workflows(get_github_token())
-    workflows_to_include = list(runs.items())
-    models = [value.replace("/", "-") for value in workflows_to_include]
+    workflows_to_include = get_github_workflows(get_github_token())
+    models = [runs.replace("/", "-") for runs in workflows_to_include]
     # # if mode_workflow is api, get github workflows using github rest api
     # if args.mode_workflow == "api":
     #     runs = get_github_workflows(get_github_token())
