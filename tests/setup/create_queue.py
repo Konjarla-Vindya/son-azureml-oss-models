36d26f272923a28ba31f165934f684f0ddc5fe17
SameepPanigrahi <v-sameepp@microsoft.com>
2023-08-30 17:42:21 +0530

    Refactoring to overwrite name

--- tests/setup/create_queue.py
+++ tests/setup/create_queue.py
@@ -191,7 +191,7 @@ def write_single_workflow_file(model, q, secret_name):
     # # print(workflow_file['env']['test_queue'])
     print (f"Generating workflow file: {workflow_file}")
     os.system(f"cp {args.workflow_template} {workflow_file}")
-    os.system(f"sed -i s/name: .*/name: {model}/g' {workflow_file}")
+    os.system(f"sed -i s/name: <test_model_name>.*/name: {model}/g' {workflow_file}")
     # replace <test_queue> with q
     os.system(f"sed -i 's/test_queue: .*/test_queue: {q}/g' {workflow_file}")
     # os.system(f"sed -i 's/test-norwayeast-02/{q}/g' {workflow_file}")
