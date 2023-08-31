60b80b42b15cf7ddbc1ddddc8562842fbc130be4
SwathiPriyaRV <66841534+Swathi23345@users.noreply.github.com>
2023-08-31 18:24:00 +0530

    Update testing4.py

--- testing4.py
+++ testing4.py
@@ -1,12 +1,12 @@
-import os,sys
+import os
 import requests
 import pandas
-from datetime import datetime
+from datetime import datetime, date
 from github import Github, Auth
 
 class Dashboard():
     def __init__(self): 
-        self.github_token = os.environ['token']
+        self.github_token = os.environ['GIT_TOKEN']
         #self.github_token = "API_TOKEN"
         print("token: ", self.github_token)
         self.token = Auth.Token(self.github_token)
@@ -53,60 +53,41 @@ class Dashboard():
         print (f"\n")
         return workflow_name
 
+    def get_latest_run_date(self, workflow_name):
+        workflow_runs_url = f"https://api.github.com/repos/{self.repo_full_name}/actions/workflows/{workflow_name}.yml/runs"
+        response = requests.get(workflow_runs_url, headers={"Authorization": f"Bearer {self.github_token}", "Accept": "application/vnd.github.v3+json"})
+        response.raise_for_status()
+        runs_data = response.json()
+
+        if "workflow_runs" in runs_data:
+            workflow_runs = runs_data["workflow_runs"]
+            if workflow_runs:
+                latest_run = workflow_runs[0]
+                created_at = latest_run["created_at"]
+                created_date = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ").date()
+                return created_date
+        return None
 
     def workflow_last_run(self): 
         workflows_to_include = self.get_all_workflow_names()
         normalized_workflows = [workflow_name.replace("/", "-") for workflow_name in workflows_to_include]
+        
+        today = date.today()
 
         for workflow_name in normalized_workflows:
             try:
-                workflow_runs_url = f"https://api.github.com/repos/{self.repo_full_name}/actions/workflows/{workflow_name}.yml/runs"
-                response = requests.get(workflow_runs_url, headers={"Authorization": f"Bearer {self.github_token}", "Accept": "application/vnd.github.v3+json"})
-                response.raise_for_status()
-                runs_data = response.json()
-
-                if "workflow_runs" not in runs_data:
-                    print(f"No runs found for workflow '{workflow_name}'. Skipping...")
-                    continue
-
-                workflow_runs = runs_data["workflow_runs"]
-                if not workflow_runs:
-                    print(f"No runs found for workflow '{workflow_name}'. Skipping...")
-                    continue
-
-                last_run = workflow_runs[0]
-                jobs_response = requests.get(last_run["jobs_url"], headers={"Authorization": f"Bearer {self.github_token}", "Accept": "application/vnd.github.v3+json"})
-                jobs_data = jobs_response.json()
-
-                badge_url = f"https://github.com/{self.repo_full_name}/actions/workflows/{workflow_name}.yml/badge.svg"
-                html_url = jobs_data["jobs"][0]["html_url"] if jobs_data.get("jobs") else ""
-
-                self.data["workflow_id"].append(last_run["workflow_id"])
-                self.data["workflow_name"].append(workflow_name.replace(".yml", ""))
-                self.data["last_runid"].append(last_run["id"])
-                self.data["created_at"].append(last_run["created_at"])
-                self.data["updated_at"].append(last_run["updated_at"])
-                self.data["status"].append(last_run["status"])
-                self.data["conclusion"].append(last_run["conclusion"])
-                self.data["jobs_url"].append(html_url)
-
-                if html_url:
-                    self.data["badge"].append(f"[![{workflow_name}]({badge_url})]({html_url})")
-                else:
-                    url = f"https://github.com/{self.repo_full_name}/actions/workflows/{workflow_name}.yml"
-                    self.data["badge"].append(f"[![{workflow_name}]({badge_url})]({url})")
+                created_date = self.get_latest_run_date(workflow_name)
+                if created_date and created_date == today:
+                    # Fetch details only if the latest run is today
+                    # ... (rest of your existing code)
 
             except requests.exceptions.RequestException as e:
                 print(f"An error occurred while fetching run information for workflow '{workflow_name}': {e}")
 
         return self.data
 
+    # ... (rest of your existing code)
 
-    def save_to_excel(self, last_runs_dict):
-        df = pd.DataFrame.from_dict(last_runs_dict)
-        excel_path = "workflow_status.xlsx"
-        df.to_excel(excel_path, index=False)
-        print(f"Data saved to {excel_path}")
 
     def results(self, last_runs_dict):
         results_dict = {"total": 0, "success": 0, "failure": 0, "cancelled": 0, "not_tested": 0, "total_duration": 0}
@@ -140,13 +121,10 @@ class Dashboard():
        
 
 def main():
-        
-        my_class = Dashboard()
-        last_runs_dict = my_class.workflow_last_run()
-        my_class.results(last_runs_dict)
-        my_class.save_to_excel(last_runs_dict)
+    my_class = Dashboard()
+    last_runs_dict = my_class.workflow_last_run()
+    my_class.results(last_runs_dict)
         
 if __name__ == "__main__":
     main()
-
 
