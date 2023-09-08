5b061a9fe35d135633c0bf6c5c5a6f8c5688986d
SwathiPriyaRV <66841534+Swathi23345@users.noreply.github.com>
2023-09-08 19:13:12 +0530

    Update update-readme.py

--- dashboard/HuggingFace/update-readme.py
+++ dashboard/HuggingFace/update-readme.py
@@ -4,6 +4,8 @@ import pandas
 from datetime import datetime
 from github import Github, Auth
 
+ 
+
 class Dashboard():
     def __init__(self): 
         self.github_token = os.environ['token']
@@ -17,7 +19,7 @@ class Dashboard():
             "workflow_id": [], "workflow_name": [], "last_runid": [], "created_at": [],
             "updated_at": [], "status": [], "conclusion": [], "badge": [], "jobs_url": []
         }
-        
+
     def get_all_workflow_names(self):
         API = "https://api.github.com/repos/Konjarla-Vindya/son-azureml-oss-models/actions/workflows"
         print (f"Getting github workflows from {API}")
@@ -26,7 +28,7 @@ class Dashboard():
         per_page = 100
         workflow_name = []
         while total_pages is None or current_page <= total_pages:
-            
+
             headers = {
                 "Authorization": f"Bearer {self.github_token}",
                 "Accept": "application/vnd.github.v3+json"
@@ -59,42 +61,49 @@ class Dashboard():
         #     json.dump(workflow_name, f, indent=4)
         return workflow_name
 
+ 
+
+ 
 
     def workflow_last_run(self): 
         workflows_to_include = self.get_all_workflow_names()
         normalized_workflows = [workflow_name.replace("/", "-") for workflow_name in workflows_to_include]
-    
-        cutoff_date = datetime(2023, 8, 25)  # August 25, 2023
-    
+
+ 
+
         for workflow_name in normalized_workflows:
             try:
                 workflow_runs_url = f"https://api.github.com/repos/{self.repo_full_name}/actions/workflows/{workflow_name}.yml/runs"
                 response = requests.get(workflow_runs_url, headers={"Authorization": f"Bearer {self.github_token}", "Accept": "application/vnd.github.v3+json"})
                 response.raise_for_status()
                 runs_data = response.json()
-    
+
+ 
+
                 if "workflow_runs" not in runs_data:
                     print(f"No runs found for workflow '{workflow_name}'. Skipping...")
                     continue
-    
+
+ 
+
                 workflow_runs = runs_data["workflow_runs"]
                 if not workflow_runs:
                     print(f"No runs found for workflow '{workflow_name}'. Skipping...")
                     continue
-    
+
+ 
+
                 last_run = workflow_runs[0]
-                last_run_date = datetime.strptime(last_run["created_at"], "%Y-%m-%dT%H:%M:%SZ")
-    
-                if last_run_date >= cutoff_date:
-                    print(f"Last run for workflow '{workflow_name}' is after August 25, 2023. Skipping...")
-                    continue
-    
                 jobs_response = requests.get(last_run["jobs_url"], headers={"Authorization": f"Bearer {self.github_token}", "Accept": "application/vnd.github.v3+json"})
                 jobs_data = jobs_response.json()
-    
+
+ 
+
                 badge_url = f"https://github.com/{self.repo_full_name}/actions/workflows/{workflow_name}.yml/badge.svg"
                 html_url = jobs_data["jobs"][0]["html_url"] if jobs_data.get("jobs") else ""
-    
+
+ 
+
                 self.data["workflow_id"].append(last_run["workflow_id"])
                 self.data["workflow_name"].append(workflow_name.replace(".yml", ""))
                 self.data["last_runid"].append(last_run["id"])
@@ -103,54 +112,74 @@ class Dashboard():
                 self.data["status"].append(last_run["status"])
                 self.data["conclusion"].append(last_run["conclusion"])
                 self.data["jobs_url"].append(html_url)
-    
+
+ 
+
                 if html_url:
                     self.data["badge"].append(f"[![{workflow_name}]({badge_url})]({html_url})")
                 else:
                     url = f"https://github.com/{self.repo_full_name}/actions/workflows/{workflow_name}.yml"
                     self.data["badge"].append(f"[![{workflow_name}]({badge_url})]({url})")
-    
+
+ 
+
             except requests.exceptions.RequestException as e:
                 print(f"An error occurred while fetching run information for workflow '{workflow_name}': {e}")
-    
+
+ 
+
         return self.data
+
+ 
 
     def results(self, last_runs_dict):
         results_dict = {"total": 0, "success": 0, "failure": 0, "cancelled": 0, "not_tested": 0, "total_duration": 0}
         summary = []
+
+ 
 
         df = pandas.DataFrame.from_dict(last_runs_dict)
         results_dict["total"] = df["workflow_id"].count()
         results_dict["success"] = df.loc[(df['status'] == 'completed') & (df['conclusion'] == 'success')]['workflow_id'].count()
         results_dict["failure"] = df.loc[(df['status'] == 'completed') & (df['conclusion'] == 'failure')]['workflow_id'].count()
         results_dict["cancelled"] = df.loc[(df['status'] == 'completed') & (df['conclusion'] == 'cancelled')]['workflow_id'].count()
-    
+
         success_rate = results_dict["success"]/results_dict["total"]*100.00
         failure_rate = results_dict["failure"]/results_dict["total"]*100.00
         cancel_rate = results_dict["cancelled"]/results_dict["total"]*100.00
+
+ 
 
         summary.append("🚀Total|✅Success|❌Failure|🚫Cancelled|")
         summary.append("-----|-------|-------|-------|")
         summary.append(f"{results_dict['total']}|{results_dict['success']}|{results_dict['failure']}|{results_dict['cancelled']}|")
         summary.append(f"100.0%|{success_rate:.2f}%|{failure_rate:.2f}%|{cancel_rate:.2f}%|")
 
+ 
+
         models = {"Model": last_runs_dict["workflow_name"], "Status": last_runs_dict["badge"]}
         models_md = pandas.DataFrame.from_dict(models).to_markdown()
 
+ 
+
         summary_text = "\n".join(summary)
+
+ 
 
         with open("README.md", "w", encoding="utf-8") as f:
             f.write(summary_text)
             f.write(os.linesep)
             f.write(os.linesep)
             f.write(models_md)
-       
+
+
+ 
 
 def main():
-        
+
         my_class = Dashboard()
         last_runs_dict = my_class.workflow_last_run()
         my_class.results(last_runs_dict)
-        
+
 if __name__ == "__main__":
     main()
