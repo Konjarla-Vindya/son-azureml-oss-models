21a66178a07a5017a64a2abfb509301017de71ca
Suchetha24 <136798869+Suchetha24@users.noreply.github.com>
2023-09-12 13:35:13 +0530

    Update create_queue.py

--- tests/setup/create_queue.py
+++ tests/setup/create_queue.py
@@ -183,7 +183,7 @@ def create_workflow_files(q,workspace_list):
 def write_single_workflow_file(model, q, secret_name):
     # print a single dot without a newline to show progress
     print (".", end="", flush=True)
-    workflowname=model.replace('/','-')
+    workflowname="MLFlow-"+model.replace('/','-')
     # os.system(f"sed -i 's/name: .*/name: {model}/g' {args.workflow_template}")
     workflow_file=f"{args.workflow_dir}/{workflowname}.yml"
     os.system(f"rm -rf {args.workflow_dir}/demo_{workflowname}.yml") 
