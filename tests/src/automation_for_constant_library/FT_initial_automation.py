c1d9ac22b89a0d193711c57ebb0aa598144993eb
Suchetha24 <136798869+Suchetha24@users.noreply.github.com>
2023-09-12 15:21:12 +0530

    Update FT_initial_automation.py

--- tests/src/automation_for_constant_library/FT_initial_automation.py
+++ tests/src/automation_for_constant_library/FT_initial_automation.py
@@ -181,7 +181,7 @@ if __name__ == "__main__":
     latest_env = workspace_ml_client.environments.get(
         name=queue.environment, version=str(latest_version))
     print("Latest Environment :", latest_env)
-    command_job = run_azure_ml_job(code="./", command_to_run="python generic_model_download_and_register.py",
+    command_job = run_azure_ml_job(code="./", command_to_run="python FT.py",
                                    environment=latest_env, compute=queue.compute, environment_variables=environment_variables)
     create_and_get_job_studio_url(command_job, workspace_ml_client)
 
