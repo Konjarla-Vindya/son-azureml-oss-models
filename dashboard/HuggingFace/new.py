8731643fc382a6af8be67a62bf6b354878132ef0
SwathiPriyaRV <66841534+Swathi23345@users.noreply.github.com>
2023-09-14 18:50:23 +0530

    Update new.py

--- dashboard/HuggingFace/new.py
+++ dashboard/HuggingFace/new.py
@@ -121,7 +121,7 @@ class Dashboard():
                 run_link = f"https://github.com/{self.repo_full_name}/actions/runs/{last_run['id']}"
                 models_entry = {
                     "Model": workflow_name.replace(".yml", ""),
-                    "Status": "![PASS](https://via.placeholder.com/15/00FF00/000000?text=+) PASS" if last_run["conclusion"] == "success" else "![FAIL](https://via.placeholder.com/15/FF0000/000000?text=+) FAIL"
+                    "Status": "![PASS](https://via.placeholder.com/15/00FF00/000000?text=+) PASS" if last_run["conclusion"] == "success" else "![FAIL](https://via.placeholder.com/15/FF0000/000000?text=+) FAIL",
                     # "Status": "<font color='green'>PASS</font>" if last_run["conclusion"] == "success" else "<font color='red'>FAIL</font>",
                     "Link": f"[Run Link]({run_link})",
                     "LastRun_Timestamp": last_run["created_at"]
