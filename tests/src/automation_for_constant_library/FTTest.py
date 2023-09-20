9313963438c71df43d40eb8d5509369ef62e867c
Suchetha24 <136798869+Suchetha24@users.noreply.github.com>
2023-09-20 19:11:46 +0530

    Update FTTest.py

--- tests/src/automation_for_constant_library/FTTest.py
+++ tests/src/automation_for_constant_library/FTTest.py
@@ -2,6 +2,42 @@ import os
 from FT_initial_automation import load_model  
 import mlflow
 import transformers
+import os
+import torch
+import json
+import pandas as pd
+from FT_initial_automation import load_model  
+import transformers
+import mlflow
+import datetime
+from azure.ai.ml import MLClient
+from azure.identity import DefaultAzureCredential
+from azure.ai.ml.constants import AssetTypes
+from azure.ai.ml.entities import (
+    ManagedOnlineEndpoint,
+    ManagedOnlineDeployment,
+    Model,
+    ModelConfiguration,
+    ModelPackage,
+    Environment,
+    CodeConfiguration,
+    AzureMLOnlineInferencingServer
+)
+from azureml.core import Workspace
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, Seq2SeqTrainingArguments, Trainer, DataCollatorForSeq2Seq
+from datasets import load_dataset
+import numpy as np
+# import evaluate
+import argparse
+import os
+from azureml.core import Workspace
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, TrainingArguments, Trainer
+from transformers import AutoModelForMaskedLM
+from datasets import load_dataset
+import numpy as np
+# import evaluate
+import pandas as pd
+
 def data_set():
     exit_status = os.system("python ./download-dataset.py --download_dir emotion-dataset")
     print("exit_status----------",exit_status)
@@ -29,6 +65,38 @@ def data_set():
         label_df = label_df[["label", "label_string"]]
         label_df.head()
     print("downloaded data set-------------")
+
+def get_library_to_load_model(self, task: str) -> str:
+        """ Takes the task name and load the  json file findout the library 
+        which is applicable for that task and retyrun it 
+
+        Args:
+            task (str): required the task name 
+        Returns:
+            str: return the library name
+        """
+        try:
+            with open(FILE_NAME) as f:
+                model_with_library = ConfigBox(json.load(f))
+                print(f"scoring_input file:\n\n {model_with_library}\n\n")
+        except Exception as e:
+            print(
+                f"::warning:: Could not find scoring_file: {model_with_library}. Finishing without sample scoring: \n{e}")
+        return model_with_library.get(task)
+
+ def download_model_and_tokenizer(self, task: str) -> dict:
+        model_library_name = self.get_library_to_load_model(task=task)
+        print("Library name is this one : ", model_library_name)
+        # Load the library from the transformer
+        model_library = getattr(transformers, model_library_name)
+        # From the library load the model
+        model_name = loaded_model.model 
+        model_name.config.num_labels = 6 
+        model_name.classifier = torch.nn.Linear(model_name.config.hidden_size, model_name.config.num_labels) 
+        Text_classification_model = model_library.from_pretrained(
+            config=model_name.config) 
+        Text_classification_model.load_state_dict(model_name.state_dict(), strict=False)
+     
 if __name__ == "__main__":
   model_source_uri=os.environ.get('model_source_uri')
   test_model_name = os.environ.get('test_model_name')
