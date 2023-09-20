2bffbc2398db11fdb9c9e88f4b52e91ef38b6fa8
Suchetha24 <136798869+Suchetha24@users.noreply.github.com>
2023-09-20 14:41:02 +0530

    Update FTTest.py

--- tests/src/automation_for_constant_library/FTTest.py
+++ tests/src/automation_for_constant_library/FTTest.py
@@ -1,5 +1,6 @@
 import os
 from FT_initial_automation import load_model  
+model_source_uri=os.environ.get('model_source_uri')
 test_model_name = os.environ.get('test_model_name')
 print("-----------------Im inside the python code")
 print("test_model_name-----------------",test_model_name)
