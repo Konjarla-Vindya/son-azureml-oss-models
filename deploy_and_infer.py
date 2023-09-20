6743a22fe4affeead569b4f0a5a0ed2e3ae05b67
priyaray21 <139554535+priyaray21@users.noreply.github.com>
2023-09-20 13:35:13 +0530

    Update deploy_and_infer.py

--- deploy_and_infer.py
+++ deploy_and_infer.py
@@ -1,101 +1,209 @@
-import json
+import os
 import time
-from azure.ai.ml import MLClient
+import json
 from azure.identity import DefaultAzureCredential
-from azure.ai.ml.constants import AssetTypes
-from azure.ai.ml.entities import (
-    ManagedOnlineEndpoint,
-    ManagedOnlineDeployment,
-    Model,
-    ModelConfiguration,
-    ModelPackage,
-    Environment,
-    CodeConfiguration,
-    AzureMLOnlineInferencingServer,
+from azureml.core import Workspace, Model
+import mlflow
+from transformers import (
+    AutoTokenizer,
+    AutoModelForQuestionAnswering,
+    TrainingArguments,
+    Trainer,
+    default_data_collator,
+    pipeline,
+    AutoConfig,
 )
-from box import ConfigBox
 
-# Load configuration from config.json
-with open("credentials.json", "r") as config_file:
-    config = json.load(config_file)
+def load_azure_credentials(credentials):
+    # Load Azure credentials from a JSON file
+    with open(credentials, "r") as creds_file:
+        azure_credentials = json.load(creds_file)
+    return azure_credentials
 
-# Define your configuration
-config_box = ConfigBox(config)
+def authenticate_to_azure(azure_credentials):
+    # Authenticate to Azure Machine Learning workspace
+    subscription_id = azure_credentials["subscription_id"]
+    resource_group = azure_credentials["resource_group"]
+    workspace_name = azure_credentials["workspace_name"]
+    
+    credential = DefaultAzureCredential()
+    ws = Workspace(subscription_id, resource_group, workspace_name)
+    mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
+    return ws
 
-# Extract parameters from the configuration
-model_name = config_box.model_name
-model_version = config_box.model_version
-question = config_box.question
-context = config_box.context
-subscription_id = config_box.subscription_id
-resource_group = config_box.resource_group
-workspace_name = config_box.workspace_name
-online_endpoint_name_prefix = config_box.online_endpoint_name_prefix
-deployment_name = config_box.deployment_name
-instance_type = config_box.instance_type
-instance_count = config_box.instance_count
-initial_delay = config_box.initial_delay
-traffic_percentage = config_box.traffic_percentage
+def download_model(model_name, model_version, target_dir):
+    # Download the model to a local directory
+    model = Model(ws, name=model_name, version=model_version)
+    model.download(target_dir=target_dir, exist_ok=True)
 
-# Initialize Azure ML Workspace
-credential = DefaultAzureCredential()
-ws = Workspace(subscription_id, resource_group, workspace_name)
+def prepare_and_fine_tune_model(tokenizer, model, datasets):
+    max_length = 384  # The maximum length of a feature (question and context)
+    doc_stride = 128  # The authorized overlap between two parts of the context when splitting it is needed.
+    pad_on_right = tokenizer.padding_side == "right"
 
-# Fetch the registered model
-version_list = list(ws.models.list(model_name))
-foundation_model = ""
-if len(version_list) == 0:
-    print("Model not found in registry")
-else:
-    model_version = version_list[0].version
-    foundation_model = ws.models.get(model_name, model_version)
-    print(
-        f"Using model name: {foundation_model.name}, version: {foundation_model.version}, id: {foundation_model.id} for inferencing"
+    
+def prepare_train_features(examples):
+    examples["question"] = [q.lstrip() for q in examples["question"]]
+
+    tokenized_examples = tokenizer(
+        examples["question" if pad_on_right else "context"],
+        examples["context" if pad_on_right else "question"],
+        truncation="only_second" if pad_on_right else "only_first",
+        max_length=max_length,
+        stride=doc_stride,
+        return_overflowing_tokens=True,
+        return_offsets_mapping=True,
+        padding="max_length",
     )
 
-print(f"Latest model {foundation_model.name} version {foundation_model.version} created at {foundation_model.creation_context.created_at}")
+    sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
+    offset_mapping = tokenized_examples.pop("offset_mapping")
 
-# Load the registered model pipeline
-model_source_uri = foundation_model.properties["mlflow.modelSourceUri"]
-registered_model_pipeline = mlflow.transformers.load_model(model_uri=model_source_uri)
+    tokenized_examples["start_positions"] = []
+    tokenized_examples["end_positions"] = []
 
-# Define a question-answering configuration
-question_answering = ConfigBox(
-    {
-        "inputs": {
-            "question": question,
-            "context": context,
-        }
-    }
-)
+    for i, offsets in enumerate(offset_mapping):
 
-# Perform inference using the registered model pipeline
-inference_result = registered_model_pipeline(question_answering.inputs)
+        input_ids = tokenized_examples["input_ids"][i]
+        cls_index = input_ids.index(tokenizer.cls_token_id)
 
-print("Inference Result:")
-print(inference_result)
+        sequence_ids = tokenized_examples.sequence_ids(i)
 
-# Create an online endpoint
-timestamp = int(time.time())
-online_endpoint_name = online_endpoint_name_prefix + str(timestamp)
-endpoint = ManagedOnlineEndpoint(
-    name=online_endpoint_name,
-    description="Online endpoint for " + foundation_model.name + ", qa",
-    auth_mode="key",
-)
-ws.ml.endpoints.create_or_update(endpoint).wait()
+        sample_index = sample_mapping[i]
+        answers = examples["answers"][sample_index]
 
-# Create a deployment
-demo_deployment = ManagedOnlineDeployment(
-    name=deployment_name,
-    endpoint_name=online_endpoint_name,
-    model=foundation_model.id,
-    instance_type=instance_type,
-    instance_count=instance_count,
-    liveness_probe=ProbeSettings(initial_delay=initial_delay),
-)
-ws.online_deployments.create_or_update(demo_deployment).wait()
-endpoint.traffic = {deployment_name: traffic_percentage}
-ws.endpoints.create_or_update(endpoint).result()
+        # If no answers are given, set the cls_index as the answer.
+        if len(answers["answer_start"]) == 0:
+            tokenized_examples["start_positions"].append(cls_index)
+            tokenized_examples["end_positions"].append(cls_index)
+        else:
+            # Start/end character index of the answer in the text.
+            start_char = answers["answer_start"][0]
+            end_char = start_char + len(answers["text"][0])
 
-print(f"Online endpoint '{online_endpoint_name}' deployed with deployment '{deployment_name}'")
+            # Start token index of the current span in the text.
+            token_start_index = 0
+            while sequence_ids[token_start_index] != (1 if pad_on_right else 0):
+                token_start_index += 1
+
+            # End token index of the current span in the text.
+            token_end_index = len(input_ids) - 1
+            while sequence_ids[token_end_index] != (1 if pad_on_right else 0):
+                token_end_index -= 1
+
+            # Detect if the answer is out of the span (in which case this feature is labeled with the CLS index).
+            if not (offsets[token_start_index][0] <= start_char and offsets[token_end_index][1] >= end_char):
+                tokenized_examples["start_positions"].append(cls_index)
+                tokenized_examples["end_positions"].append(cls_index)
+            else:
+                # Otherwise, move the token_start_index and token_end_index to the two ends of the answer.
+                # Note: we could go after the last offset if the answer is the last word (edge case).
+                while token_start_index < len(offsets) and offsets[token_start_index][0] <= start_char:
+                    token_start_index += 1
+                tokenized_examples["start_positions"].append(token_start_index - 1)
+                while offsets[token_end_index][1] >= end_char:
+                    token_end_index -= 1
+                tokenized_examples["end_positions"].append(token_end_index + 1)
+
+    return tokenized_examples
+
+    # Load and preprocess the dataset
+    tokenized_datasets = datasets.map(
+        prepare_train_features, batched=True, remove_columns=datasets["train"].column_names
+    )
+
+    # Fine-tuning the model
+    batch_size = 16
+    args = TrainingArguments(output_dir="./results", evaluation_strategy="epoch")
+    data_collator = default_data_collator
+
+    subset_train_indices = range(100)
+    subset_validation_indices = range(100)
+
+    subset_train_dataset = tokenized_datasets["train"].select(subset_train_indices)
+    subset_validation_dataset = tokenized_datasets["validation"].select(subset_validation_indices)
+
+    trainer = Trainer(
+        model=model,
+        args=args,
+        train_dataset=subset_train_dataset,
+        eval_dataset=subset_validation_dataset,
+        data_collator=data_collator,
+        tokenizer=tokenizer,
+    )
+
+    fine_tune_results = trainer.train()
+    evaluation_results = trainer.evaluate()
+
+    # Save the fine-tuned model
+    model_save_directory = "./ft_qa_model"
+    trainer.save_model(model_save_directory)
+    ft_model = AutoModelForQuestionAnswering.from_pretrained(model_save_directory)
+    tokenizer.save_pretrained(model_save_directory)
+    ft_tokenizer = AutoTokenizer.from_pretrained(model_save_directory)
+
+    return ft_model, ft_tokenizer
+
+def register_and_deploy_model(ft_model, ft_tokenizer, model_name):
+    model_pipeline = pipeline(task="question-answering", model=ft_model, tokenizer=ft_tokenizer)
+    
+    with mlflow.start_run():
+        model_info = mlflow.transformers.log_model(
+            transformers_model=model_pipeline, artifact_path=model_name
+        )
+    registered_model = mlflow.register_model(model_info.model_uri, model_name)
+
+    timestamp = int(time.time())
+    online_endpoint_name = "qa-" + str(timestamp)
+
+    # Create an online endpoint
+    endpoint = ManagedOnlineEndpoint(
+        name=online_endpoint_name,
+        description="Online endpoint for " + registered_model.name + ", qa",
+        auth_mode="key",
+    )
+    workspace_ml_client.begin_create_or_update(endpoint).wait()
+
+    # Create a deployment
+    demo_deployment = ManagedOnlineDeployment(
+        name="demo",
+        endpoint_name=online_endpoint_name,
+        model=registered_model.id,
+        instance_type="Standard_DS3_v2",
+        instance_count=1,
+        liveness_probe=ProbeSettings(initial_delay=600),
+    )
+    workspace_ml_client.online_deployments.begin_create_or_update(demo_deployment).wait()
+    endpoint.traffic = {"demo": 100}
+    workspace_ml_client.begin_create_or_update(endpoint).result()
+
+    print(f"Model deployed as an online endpoint: {online_endpoint_name}")
+
+if __name__ == "__main__":
+    # Load Azure credentials from a JSON file
+    azure_credentials = load_azure_credentials("azure_credentials.json")
+    
+    # Authenticate to Azure Machine Learning workspace
+    ws = authenticate_to_azure(azure_credentials)
+    
+    # Load model details from a JSON file
+    model_details = load_model_details("model_details.json")
+    model_name = model_details["model_name"]
+    model_version = model_details["model_version"]
+    
+    # Download the model
+    download_model(model_name, model_version, "./downloaded_models")
+    
+    # Load the downloaded tokenizer and model
+    model_component_path = "./downloaded_models/{}/components/tokenizer".format(model_name)
+    model_model_path = "./downloaded_models/{}/model".format(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_component_path)
+    model = AutoModelForQuestionAnswering.from_pretrained(model_model_path)
+    
+    # Load the SQuAD dataset
+    from datasets import load_dataset
+    datasets = load_dataset("squad")
+    
+    # Prepare, fine-tune, register, and deploy the model
+    ft_model, ft_tokenizer = prepare_and_fine_tune_model(tokenizer, model, datasets)
+    register_and_deploy_model(ft_model, ft_tokenizer, model_name)
