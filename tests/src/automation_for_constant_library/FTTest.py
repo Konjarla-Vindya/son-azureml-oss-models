c7ab59903a2e634f37e49317c9c58f81a2804992
Suchetha24 <136798869+Suchetha24@users.noreply.github.com>
2023-09-20 18:16:15 +0530

    Update FTTest.py

--- tests/src/automation_for_constant_library/FTTest.py
+++ tests/src/automation_for_constant_library/FTTest.py
@@ -1,8 +1,10 @@
 import os
 from FT_initial_automation import load_model  
+import mlflow
+import tranformers
 model_source_uri=os.environ.get('model_source_uri')
 test_model_name = os.environ.get('test_model_name')
 print("-----------------Im inside the python code")
 print("test_model_name-----------------",test_model_name)
-LM=load_model(model_detail)
-print("loaded_model-----------------------------",LM)
+loaded_model = mlflow.transformers.load_model(model_uri=model_source_uri, return_type="pipeline")
+print("loaded_model-----------------------------",loaded_model)
