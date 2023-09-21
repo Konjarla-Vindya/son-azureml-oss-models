eabeb9d112eb965ada53865b6a8221e33952d74b
Amarah <83214142+amarahAI@users.noreply.github.com>
2023-09-21 14:19:19 +0530

    Update FT_TC.py

--- tests/src/automation_for_constant_library/FT_TC.py
+++ tests/src/automation_for_constant_library/FT_TC.py
@@ -36,33 +36,33 @@ from datasets import load_dataset
 import numpy as np
 import evaluate
 
-def data_set():
-    exit_status = os.system("python ./download-dataset.py --download_dir emotion-dataset")
-    print("exit_status----------",exit_status)
-    if exit_status != 0:
-        raise Exception("Error downloading dataset")
-    # load the ./emotion-dataset/train.jsonl file into a pandas dataframe and show the first 5 rows
+# def data_set():
+#     exit_status = os.system("python ./download-dataset.py --download_dir emotion-dataset")
+#     print("exit_status----------",exit_status)
+#     if exit_status != 0:
+#         raise Exception("Error downloading dataset")
+#     # load the ./emotion-dataset/train.jsonl file into a pandas dataframe and show the first 5 rows
     
 
-    pd.set_option(
-        "display.max_colwidth", 0
-    )  # set the max column width to 0 to display the full text
-    df = pd.read_json("./emotion-dataset/train.jsonl", lines=True)
-    df.head()
+#     pd.set_option(
+#         "display.max_colwidth", 0
+#     )  # set the max column width to 0 to display the full text
+#     df = pd.read_json("./emotion-dataset/train.jsonl", lines=True)
+#     df.head()
 
     # load the id2label json element of the ./emotion-dataset/label.json file into pandas table with keys as 'label' column of int64 type and values as 'label_string' column as string type
     
 
-    with open("./emotion-dataset/label.json") as f:
-        id2label = json.load(f)
-        id2label = id2label["id2label"]
-        label_df = pd.DataFrame.from_dict(
-            id2label, orient="index", columns=["label_string"]
-        )
-        label_df["label"] = label_df.index.astype("int64")
-        label_df = label_df[["label", "label_string"]]
-        label_df.head()
-    print("downloaded data set-------------")
+    # with open("./emotion-dataset/label.json") as f:
+    #     id2label = json.load(f)
+    #     id2label = id2label["id2label"]
+    #     label_df = pd.DataFrame.from_dict(
+    #         id2label, orient="index", columns=["label_string"]
+    #     )
+    #     label_df["label"] = label_df.index.astype("int64")
+    #     label_df = label_df[["label", "label_string"]]
+    #     label_df.head()
+    # print("downloaded data set-------------")
 
 # def get_library_to_load_model(self, task: str) -> str:
 #         """ Takes the task name and load the  json file findout the library 
@@ -101,4 +101,4 @@ if __name__ == "__main__":
   print("test_model_name-----------------",test_model_name)
   loaded_model = mlflow.transformers.load_model(model_uri=model_source_uri, return_type="pipeline")
   print("loaded_model---------------------",loaded_model)
-  data_set()
+  #data_set()
