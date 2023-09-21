b7f3b32d882276c1d877d96cc2edf0c0e4cbcae0
PoojaRam12 <139552501+PoojaRam12@users.noreply.github.com>
2023-09-21 16:13:29 +0530

    Add files via upload

---
+++ tests/src/automation_for_constant_library/batch_inference_and_deployment.py
@@ -0,0 +1,212 @@
+import time
+import json
+import os
+from azure.ai.ml.constants import AssetTypes
+from azure.ai.ml.entities import (
+    ManagedOnlineEndpoint,
+    ManagedOnlineDeployment,
+    OnlineRequestSettings,
+    ProbeSettings,
+    Model,
+    ModelConfiguration,
+    ModelPackage,
+    AzureMLOnlineInferencingServer
+)
+from utils.logging import get_logger
+from fetch_task import HfTask
+import mlflow
+from box import ConfigBox
+import re
+import sys
+import time
+from azureml.core.webservice import BatchEndpoint
+from azureml.core.webservice import BatchDeployment
+from azureml.core.webservice import BatchEndpoint
+from azureml.core.webservice import BatchRetrySettings
+from azureml.core.datastore import Datastore
+from azureml.data import InputDataType
+from azureml.core import Workspace
+
+
+
+logger = get_logger(__name__)
+class BatchInferenceAndDeployemnt:
+    def __init__(self, test_model_name, workspace_ml_client, registry) -> None:
+        self.test_model_name = test_model_name
+        self.workspace_ml_client = workspace_ml_client
+        self.registry = registry
+
+    def get_error_messages(self):
+        # load ../../config/errors.json into a dictionary
+        with open('../../config/errors.json') as f:
+            return json.load(f)
+
+
+    def prase_logs(self, logs):
+        error_messages = self.get_error_messages()
+        # split logs by \n
+        logs_list = logs.split("\n")
+        # loop through each line in logs_list
+        for line in logs_list:
+            # loop through each error in errors
+            for error in error_messages:
+                # if error is found in line, print error message
+                if error['parse_string'] in line:
+                    logger.error(
+                        f"::error:: {error_messages['error_category']}: {line}")
+                    
+
+
+
+
+
+
+
+
+
+     
+                    
+
+
+    def create_or_update_batch_endpoint(workspace_ml_client, foundation_model, description=""):
+        # Generate a unique endpoint name based on the current timestamp
+        timestamp = int(time.time())
+        endpoint_name = f"fill-maskws-{timestamp}"
+
+        # Create or update the Batch Endpoint
+        endpoint = BatchEndpoint(
+            name=endpoint_name,
+            description=f"Batch endpoint for {foundation_model.name}, for fill-mask task{description}",
+        )
+        workspace_ml_client.begin_create_or_update(endpoint).result()
+
+        return endpoint
+
+    # Example usage:
+    # Replace the parameters with your desired values
+    foundation_model = your_foundation_model  # Provide your foundation model object
+    description = "Custom description (optional)"
+
+    created_endpoint = create_or_update_batch_endpoint(workspace_ml_client, foundation_model, description)
+
+
+
+
+    def create_or_update_batch_deployment(
+        workspace_ml_client,
+        deployment_name,
+        endpoint_name,
+        foundation_model,
+        compute_name,
+        error_threshold=0,
+        instance_count=1,
+        logging_level="info",
+        max_concurrency_per_instance=2,
+        mini_batch_size=10,
+        output_file_name="predictions.csv",
+        max_retries=3,
+        timeout=300,
+    ):
+        deployment = BatchDeployment(
+            name=deployment_name,
+            endpoint_name=endpoint_name,
+            model=foundation_model.id,
+            compute=compute_name,
+            error_threshold=error_threshold,
+            instance_count=instance_count,
+            logging_level=logging_level,
+            max_concurrency_per_instance=max_concurrency_per_instance,
+            mini_batch_size=mini_batch_size,
+            output_file_name=output_file_name,
+            retry_settings=BatchRetrySettings(max_retries=max_retries, timeout=timeout),
+        )
+
+        workspace_ml_client.begin_create_or_update(deployment).result()
+
+        return deployment
+
+
+
+    
+
+    def set_default_batch_deployment(workspace_ml_client, endpoint_name, deployment_name):
+        # Get the existing Batch Endpoint
+        endpoint = workspace_ml_client.batch_endpoints.get(endpoint_name)
+
+        # Update the default deployment name
+        endpoint.defaults.deployment_name = deployment_name
+
+        # Save the updated endpoint
+        workspace_ml_client.begin_create_or_update(endpoint).wait()
+
+        # Retrieve and print the default deployment name
+        updated_endpoint = workspace_ml_client.batch_endpoints.get(endpoint_name)
+        print(f"The default deployment is {updated_endpoint.defaults.deployment_name}")
+
+
+
+    def invoke_batch_job(
+        workspace: Workspace,
+        endpoint_name: str,
+        batch_inputs_dir: str,
+    ):
+        # Get the Batch Endpoint
+        endpoint = workspace.batch_endpoints[endpoint_name]
+
+        # Define the input data
+        input_data = Datastore(workspace=workspace, name=batch_inputs_dir)
+        input = Input(input_data, input_type=InputDataType.MOUNT)
+
+        # Invoke the batch job
+        job = endpoint.invoke(input)
+
+        # Stream job logs (optional)
+        job.wait_for_completion()
+        job_logs = workspace.jobs.stream(job.name)
+        for log_line in job_logs:
+            print(log_line)
+
+
+
+    
+
+    
+            
+
+if __name__ == "__main__":
+    # Example usage:
+    # Replace the parameters with your desired values
+    deployment_name = "demo"
+    endpoint_name = "your_endpoint_name"  # Provide the actual endpoint name
+    foundation_model = your_foundation_model  # Provide your foundation model object
+    compute_name = "your_compute_name"  # Provide the compute name
+
+    created_deployment = create_or_update_batch_deployment(
+        workspace_ml_client,
+        deployment_name,
+        endpoint_name,
+        foundation_model,
+        compute_name,
+        error_threshold=0,
+        instance_count=1,
+        logging_level="info",
+        max_concurrency_per_instance=2,
+        mini_batch_size=10,
+        output_file_name="predictions.csv",
+        max_retries=3,
+        timeout=300,
+    )
+    set_default_batch_deployment(workspace_ml_client, endpoint_name, deployment_name)
+
+    # Example usage:
+    # Replace the parameters with your desired values
+    endpoint_name = "your_endpoint_name"  # Provide the actual endpoint name
+    deployment_name = "your_new_deployment_name"  # Provide the new default deployment name
+
+    # Example usage:
+    # Replace the parameters with your desired values
+    workspace = your_workspace  # Provide your Azure ML workspace object
+    endpoint_name = "your_endpoint_name"  # Provide the actual endpoint name
+    batch_inputs_dir = "your_datastore_path"  # Provide the path to the input data in the datastore
+
+    invoke_batch_job(workspace, endpoint_name, batch_inputs_dir)
---
+++ tests/src/automation_for_constant_library/initial.py
@@ -0,0 +1,274 @@
+from azureml.core import Workspace, Environment
+from model_inference_and_deployment import ModelInferenceAndDeployemnt
+from azure.identity import DefaultAzureCredential, InteractiveBrowserCredential
+from azure.ai.ml.entities import AmlCompute
+from azure.ai.ml import command
+from azure.ai.ml import MLClient
+import mlflow
+import json
+import os
+import sys
+from box import ConfigBox
+from utils.logging import get_logger
+from azureml.core.compute import AmlCompute
+from azureml.core.compute_target import ComputeTargetException
+
+# constants
+check_override = True
+
+# logger = get_logger(__name__)
+
+# def get_error_messages():
+#     # load ../config/errors.json into a dictionary
+#     with open('../../config/errors.json') as f:
+#         return json.load(f)
+
+
+# error_messages = get_error_messages()
+
+# model to test
+test_model_name = os.environ.get('test_model_name')
+
+# test cpu or gpu template
+test_sku_type = os.environ.get('test_sku_type')
+
+# bool to decide if we want to trigger the next model in the queue
+test_trigger_next_model = os.environ.get('test_trigger_next_model')
+
+# test queue name - the queue file contains the list of models to test with with a specific workspace
+test_queue = os.environ.get('test_queue')
+
+# test set - the set of queues to test with. a test queue belongs to a test set
+test_set = os.environ.get('test_set')
+
+# bool to decide if we want to keep looping through the queue,
+# which means that the first model in the queue is triggered again after the last model is tested
+test_keep_looping = os.environ.get('test_keep_looping')
+
+# function to load the workspace details from test queue file
+# even model we need to test belongs to a queue. the queue name is passed as environment variable test_queue
+# the queue file contains the list of models to test with with a specific workspace
+# the queue file also contains the details of the workspace, registry, subscription, resource group
+
+
+def get_test_queue() -> ConfigBox:
+    queue_file = f"../../config/queue/{test_set}/{test_queue}.json"
+    with open(queue_file) as f:
+        return ConfigBox(json.load(f))
+# function to load the sku override details from sku-override file
+# this is useful if you want to force a specific sku for a model
+
+
+def get_sku_override():
+    try:
+        with open(f'../../config/sku-override/{test_set}.json') as json_file:
+            return json.load(json_file)
+    except Exception as e:
+        print(f"::warning:: Could not find sku-override file: \n{e}")
+        return None
+
+
+# finds the next model in the queue and sends it to github step output
+# so that the next step in this job can pick it up and trigger the next model using 'gh workflow run' cli command
+def set_next_trigger_model(queue):
+    # logger.info("In set_next_trigger_model...")
+# file the index of test_model_name in models list queue dictionary
+    model_list = list(queue.models)
+    #model_name_without_slash = test_model_name.replace('/', '-')
+    check_mlflow_model = "MLFlow-"+test_model_name
+    index = model_list.index(check_mlflow_model)
+    #index = model_list.index(test_model_name)
+    # logger.info(f"index of {test_model_name} in queue: {index}")
+# if index is not the last element in the list, get the next element in the list
+    if index < len(model_list) - 1:
+        next_model = model_list[index + 1]
+    else:
+        if (test_keep_looping == "true"):
+            next_model = queue[0]
+        else:
+            # logger.warning("::warning:: finishing the queue")
+            next_model = ""
+# write the next model to github step output
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        # logger.info(f'NEXT_MODEL={next_model}')
+        print(f'NEXT_MODEL={next_model}', file=fh)
+
+
+
+def create_or_update_compute(workspace_ml_client, compute_name, description, vm_size, min_instances, max_instances, idle_time_before_scale_down):
+    try:
+        # Check if the compute target already exists
+        compute_target = AmlCompute(workspace_ml_client, name=compute_name)
+        print(f'Using existing compute target: {compute_target.name}')
+    except ComputeTargetException:
+        # If the compute target doesn't exist, create a new one
+        config = AmlCompute.provisioning_configuration(
+            vm_size=vm_size,
+            min_nodes=min_instances,
+            max_nodes=max_instances,
+            idle_seconds_before_scaledown=idle_time_before_scale_down,
+        )
+        compute_target = AmlCompute.create(workspace_ml_client, name=compute_name, provisioning_configuration=config)
+        compute_target.wait_for_completion(show_output=True)
+
+    return compute_target
+
+
+def run_azure_ml_job(code, command_to_run, environment, compute, environment_variables):
+    # logger.info("Creating the command object method")
+    command_job = command(
+        code=code,
+        command=command_to_run,
+        environment=environment,
+        compute=compute_name,
+        environment_variables=environment_variables
+    )
+    return command_job
+
+def create_and_get_job_studio_url(command_job, workspace_ml_client):
+
+    #ml_client = mlflow.tracking.MlflowClient()
+    returned_job = workspace_ml_client.jobs.create_or_update(command_job)
+    # wait for the job to complete
+    workspace_ml_client.jobs.stream(returned_job.name)
+    return returned_job.studio_url
+
+def load_model(model_detail):
+    loaded_model = mlflow.transformers.load_model(model_uri=model_detail.source, return_type="pipeline")
+    print("Inside load model")
+    print("loaded_model---------------",loaded_model)
+    return loaded_model
+
+if __name__ == "__main__":
+    # if any of the above are not set, exit with error
+    if test_model_name is None or test_sku_type is None or test_queue is None or test_set is None or test_trigger_next_model is None or test_keep_looping is None:
+        # logger.error("::error:: One or more of the environment variables test_model_name, test_sku_type, test_queue, test_set, test_trigger_next_model, test_keep_looping are not set")
+        exit(1)
+
+    queue = get_test_queue()
+
+    # sku_override = get_sku_override()
+    # if sku_override is None:
+    #     check_override = False
+
+    if test_trigger_next_model == "true":
+        set_next_trigger_model(queue)
+    # print values of all above variables
+    print (f"test_subscription_id: {queue['subscription']}")
+    print (f"test_resource_group: {queue['resource_group']}")
+    print (f"test_workspace_name: {queue['workspace']}")
+    print (f"test_model_name: {test_model_name}")
+    print (f"test_sku_type: {test_sku_type}")
+    print (f"test_registry: queue['registry']")
+    print (f"test_trigger_next_model: {test_trigger_next_model}")
+    print (f"test_queue: {test_queue}")
+    print (f"test_set: {test_set}")
+    print("Here is my test model name : ", test_model_name)
+    try:
+        credential = DefaultAzureCredential()
+        credential.get_token("https://management.azure.com/.default")
+    except Exception as ex:
+        # Fall back to InteractiveBrowserCredential in case DefaultAzureCredential not work
+        print("workspace_name : ", queue.workspace)
+        credential = InteractiveBrowserCredential()
+    # logger.info(f"workspace_name : {queue.workspace}")
+    try:
+        workspace_ml_client = MLClient.from_config(credential=credential)
+    except:
+        workspace_ml_client = MLClient(
+            credential=credential,
+            subscription_id=queue.subscription,
+            resource_group_name=queue.resource_group,
+            workspace_name=queue.workspace
+        )
+    ws = Workspace(
+        subscription_id=queue.subscription,
+        resource_group=queue.resource_group,
+        workspace_name=queue.workspace
+    )
+    mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
+    compute_target = create_or_update_compute(
+        workspace_ml_client, queue.compute)
+    # environment_variables = {"test_model_name": test_model_name}
+    env_list = workspace_ml_client.environments.list(name=queue.environment)
+    latest_version = 0
+    for env in env_list:
+        if latest_version <= int(env.version):
+            latest_version = int(env.version)
+    print("Latest Environment Version:", latest_version)
+    latest_env = workspace_ml_client.environments.get(
+        name=queue.environment, version=str(latest_version))
+    print("Latest Environment :", latest_env)
+    version_list = list(workspace_ml_client.models.list(test_model_name))
+
+    client = MlflowClient()
+    
+    registered_model_detail = client.get_latest_versions(
+    
+        name=test_model_name, stages=["None"])
+    
+    model_detail = registered_model_detail[0]
+    
+    print("Latest registered model version is : ", model_detail.version)
+    
+    # loaded_model = mlflow.transformers.load_model(model_uri=model_detail.source, return_type="pipeline")
+    # model_source_uri = foundation_model.properties["mlflow.modelSourceUri"]
+    # print("model_source_uri---------------------",model_source_uri)
+    # loaded_model = mlflow.transformers.load_model(model_uri=model_source_uri)
+    # LM=load_model(model_detail)
+    # print("LM-----------------------------",LM)
+    environment_variables = {"test_model_name": test_model_name
+                            ,"model_source_uri": model_detail.source}
+    print("environment_variables-------------",environment_variables)
+    print("queue.compute---",queue.compute)
+    print("queue.workspace====",queue.workspace)
+
+    command_job = run_azure_ml_job(code="./", command_to_run="python batch_infernce_and_deployment.py",
+                                   environment=latest_env, compute=queue.compute, environment_variables=environment_variables)
+    
+    # Example usage:
+    # Replace the parameters with your desired values
+    compute_name = "cpu-cluster"
+    description = "An AML compute cluster"
+    vm_size = "Standard_DS3_V2"
+    min_instances = 0
+    max_instances = 3
+    idle_time_before_scale_down = 120
+
+    compute_cluster = create_or_update_compute(workspace_ml_client, compute_name, description, vm_size, min_instances, max_instances, idle_time_before_scale_down)
+
+    
+
+
+    InferenceAndDeployment = BatchInferenceAndDeployemnt(
+            test_model_name=test_model_name,
+            workspace_ml_client=workspace_ml_client,
+            registry=queue.registry
+        )
+    InferenceAndDeployment.batch_infernce_and_deployment(
+            instance_type=queue.instance_type
+        )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
