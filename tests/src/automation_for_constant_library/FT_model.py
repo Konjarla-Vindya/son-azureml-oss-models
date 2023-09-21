91227317302be6eb10119b87df990ecc643c4348
priyaray21 <139554535+priyaray21@users.noreply.github.com>
2023-09-21 17:36:42 +0530

    Update FT_model.py

--- tests/src/automation_for_constant_library/FT_model.py
+++ tests/src/automation_for_constant_library/FT_model.py
@@ -30,46 +30,113 @@ import numpy as np
 import argparse
 import os
 from azureml.core import Workspace
+from transformers import DataCollatorForTokenClassification
+from transformers import TrainingArguments, Trainer, DataCollatorForTokenClassification
+import numpy as np
+from datasets import load_metric
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, TrainingArguments, Trainer
 from transformers import AutoModelForMaskedLM
 from datasets import load_dataset
 import numpy as np
 import evaluate
 
-import os
-import json
-import pandas as pd
-import subprocess
+# def create_training_args(model_name, task, batch_size=16, num_train_epochs=3):
+#     return TrainingArguments(
+#         f"{model_name}-finetuned-{task}",
+#         evaluation_strategy="epoch",
+#         learning_rate=2e-5,
+#         per_device_train_batch_size=batch_size,
+#         per_device_eval_batch_size=batch_size,
+#         num_train_epochs=num_train_epochs,
+#         weight_decay=0.01,
+#         push_to_hub=False,
+#     )
 
-# Define a function to download and prepare a dataset using a provided Python script
-def download_and_prepare_dataset(dataset_name, download_script, download_dir1, label_mapping=None):
-    # Create the download directory if it does not exist
-    if not os.path.exists(download_dir1):
-        os.makedirs(download_dir1)
+# def create_data_collator(tokenizer):
+#     return DataCollatorForTokenClassification(tokenizer)
 
-    # Run the provided download script to download the dataset
-    exit_status = subprocess.call(f"python {download_script} --download_dir1 {download_dir1}", shell=True)
-    if exit_status != 0:
-        raise Exception(f"Error downloading {dataset_name} dataset")
+# def create_compute_metrics(label_list):
+#     metric = load_metric("seqeval")
 
-    # Load the JSONL file into a pandas DataFrame and show the first 5 rows
-    pd.set_option("display.max_colwidth", 0)
-    df = pd.read_json(os.path.join(download_dir1, "train.jsonl"), lines=True)
-    df.head()
+#     def compute_metrics(p):
+#         predictions, labels = p
+#         predictions = np.argmax(predictions, axis=2)
 
-    # Load the id2label JSON element of the label.json file into a pandas table
-    with open(os.path.join(download_dir1, "label.json")) as f:
-        id2label = json.load(f)["id2label"]
-        label_df = pd.DataFrame.from_dict(id2label, orient="index", columns=["label_string"])
-        label_df["label"] = label_df.index.astype("int64")
-        label_df = label_df[["label", "label_string"]]
-        label_df.head()
+#         # Remove ignored index (special tokens)
+#         true_predictions = [
+#             [label_list[p] for (p, l) in zip(prediction, label) if l != -100]
+#             for prediction, label in zip(predictions, labels)
+#         ]
+#         true_labels = [
+#             [label_list[l] for (p, l) in zip(prediction, label) if l != -100]
+#             for prediction, label in zip(predictions, labels)
+#         ]
 
-    print(f"{dataset_name} dataset downloaded and prepared successfully in {download_dir1} -------------")
+#         results = metric.compute(predictions=true_predictions, references=true_labels)
+#         return {
+#             "precision": results["overall_precision"],
+#             "recall": results["overall_recall"],
+#             "f1": results["overall_f1"],
+#             "accuracy": results["overall_accuracy"],
+#         }
 
-if __name__ == "__main__":
-    # Download and prepare the SQuAD dataset using the provided script
-    download_and_prepare_dataset("SQuAD", "./download-dataset-squad.py", "squad-dataset", label_mapping=None)
+# def tokenize_and_align_labels(dataset, tokenizer, task, label_all_tokens=True):
+#     def tokenize_and_align(examples):
+#         tokenized_inputs = tokenizer(examples["tokens"], truncation=True, is_split_into_words=True)
+
+#         labels = []
+#         for i, label in enumerate(examples[f"{task}_tags"]):
+#             word_ids = tokenized_inputs.word_ids(batch_index=i)
+#             previous_word_idx = None
+#             label_ids = []
+#             for word_idx in word_ids:
+#                 # Special tokens have a word id that is None. We set the label to -100 so they are automatically
+#                 # ignored in the loss function.
+#                 if word_idx is None:
+#                     label_ids.append(-100)
+#                 # We set the label for the first token of each word.
+#                 elif word_idx != previous_word_idx:
+#                     label_ids.append(label[word_idx])
+#                 # For the other tokens in a word, we set the label to either the current label or -100, depending on
+#                 # the label_all_tokens flag.
+#                 else:
+#                     label_ids.append(label[word_idx] if label_all_tokens else -100)
+#                 previous_word_idx = word_idx
+
+#             labels.append(label_ids)
+
+#         tokenized_inputs["labels"] = labels
+#         return tokenized_inputs
+
+#     tokenized_datasets = dataset.map(
+#         lambda examples: tokenize_and_align(examples),
+#         batched=True,
+#     )
+
+#     return tokenized_datasets
+
+
+
+
+def load_custom_dataset(dataset_name, task, batch_size):
+    # Load the dataset
+    datasets = load_dataset(dataset_name)
+
+    # Define label_list based on the task
+    if task == "question-answering":
+        label_list = datasets["train"].features[f"{task}_tags"].feature.names
+    else:
+        label_list = None
+
+   
+    print(f"Loaded dataset: {dataset_name}")
+    print(f"Task: {task}")
+    print(f"Label List: {label_list}")
+    print(f"Batch Size: {batch_size}")
+
+
+    return datasets, label_list, batch_size
+
 
 
 # def get_library_to_load_model(self, task: str) -> str:
@@ -109,4 +176,36 @@ if __name__ == "__main__":
   print("test_model_name-----------------",test_model_name)
   loaded_model = mlflow.transformers.load_model(model_uri=model_source_uri, return_type="pipeline")
   print("loaded_model---------------------",loaded_model)
-  data_set()
+  tokenizer=loaded_model.tokenizer
+  dataset_name = "squad"
+  task = "question-answering"
+  batch_size = 16
+  datasets, label_list, batch_size = load_custom_dataset(dataset_name, task, batch_size)
+  #task = "question-answering"
+  label_all_tokens = True
+  tokenized_datasets = tokenize_and_align_labels(datasets, tokenizer, task, label_all_tokens)
+  model_name = test_model_name
+
+  num_train_epochs = 3
+
+  training_args = create_training_args(model_name, task, batch_size, num_train_epochs)
+  data_collator = create_data_collator(tokenizer)
+  label_list = label_list  # You should define label_list based on your dataset
+  compute_metrics_fn = create_compute_metrics(label_list)
+
+  trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=subset_train_dataset,  
+    eval_dataset=subset_validation_dataset,  
+    data_collator=data_collator,
+    tokenizer=tokenizer,
+    compute_metrics=compute_metrics_fn
+   )
+
+  fine_tune_results = trainer.train()
+  print(fine_tune_results)
+  evaluation_results = trainer.evaluate()
+  print(evaluation_results)
+  #data_set()
+    
