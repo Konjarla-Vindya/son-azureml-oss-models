d75517496f30e1247f290668dc2cac9211cc077a
Suchetha24 <136798869+Suchetha24@users.noreply.github.com>
2023-09-21 13:13:41 +0530

    Update FTTest.py

--- tests/src/automation_for_constant_library/FTTest.py
+++ tests/src/automation_for_constant_library/FTTest.py
@@ -35,33 +35,11 @@ from transformers import AutoModelForMaskedLM
 from datasets import load_dataset
 import numpy as np
 import evaluate
+from datasets import load_dataset
 
-def data_set():
-    exit_status = os.system("python ./download-dataset.py --download_dir emotion-dataset")
-    print("exit_status----------",exit_status)
-    if exit_status != 0:
-        raise Exception("Error downloading dataset")
-    # load the ./emotion-dataset/train.jsonl file into a pandas dataframe and show the first 5 rows
-    
-
-    pd.set_option(
-        "display.max_colwidth", 0
-    )  # set the max column width to 0 to display the full text
-    df = pd.read_json("./emotion-dataset/train.jsonl", lines=True)
-    df.head()
-
-    # load the id2label json element of the ./emotion-dataset/label.json file into pandas table with keys as 'label' column of int64 type and values as 'label_string' column as string type
-    
-
-    with open("./emotion-dataset/label.json") as f:
-        id2label = json.load(f)
-        id2label = id2label["id2label"]
-        label_df = pd.DataFrame.from_dict(
-            id2label, orient="index", columns=["label_string"]
-        )
-        label_df["label"] = label_df.index.astype("int64")
-        label_df = label_df[["label", "label_string"]]
-        label_df.head()
+def data_set(): 
+    dataset = load_dataset("yelp_review_full")
+    dataset["train"][5]
     print("downloaded data set-------------")
 
 # def get_library_to_load_model(self, task: str) -> str:
