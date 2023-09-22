37fe9e63b0afa05581c910816a7dc3dae88ed113
priyaray21 <139554535+priyaray21@users.noreply.github.com>
2023-09-22 14:13:37 +0530

    Update FT_QA_model.py

--- tests/src/automation_for_constant_library/FT_QA_model.py
+++ tests/src/automation_for_constant_library/FT_QA_model.py
@@ -1,41 +1,10 @@
 import os
-from FT_QA_model_automation import load_model  
 import mlflow
 import transformers
-#import os
 import torch
-import json
-import pandas as pd
-import mlflow
-import datetime
-from azure.ai.ml import MLClient
-from azure.identity import DefaultAzureCredential
-from azure.ai.ml.constants import AssetTypes
-from azure.ai.ml.entities import (
-    ManagedOnlineEndpoint,
-    ManagedOnlineDeployment,
-    Model,
-    ModelConfiguration,
-    ModelPackage,
-    Environment,
-    CodeConfiguration,
-    AzureMLOnlineInferencingServer
-)
-from azureml.core import Workspace
-#from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, Seq2SeqTrainingArguments, Trainer, DataCollatorForSeq2Seq
+import numpy as np
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering, Trainer, TrainingArguments
 from datasets import load_dataset
-import numpy as np
-# import evaluate
-import argparse
-import os
-from azureml.core import Workspace
-#from transformers import DataCollatorForTokenClassification
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering, DataCollatorForQuestionAnswering, Trainer
-import numpy as np
-from datasets import load_metric
-#from transformers import AutoModelForSequenceClassification, AutoTokenizer, TrainingArguments, Trainer
-from transformers import AutoModelForMaskedLM
-import evaluate
 
 def create_training_args(model_name, batch_size=16, num_train_epochs=3):
     return TrainingArguments(
@@ -109,7 +78,6 @@ if __name__ == "__main__":
     num_train_epochs = 3
 
     training_args = create_training_args(model_name, batch_size, num_train_epochs)
-    data_collator = DataCollatorForQuestionAnswering(tokenizer)
 
     subset_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(100))
     subset_validation_dataset = tokenized_datasets["validation"].shuffle(seed=42).select(range(100))
@@ -119,7 +87,6 @@ if __name__ == "__main__":
         args=training_args,
         train_dataset=subset_train_dataset,
         eval_dataset=subset_validation_dataset,
-        data_collator=data_collator,
         tokenizer=tokenizer,
     )
 
