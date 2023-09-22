42628c7d8db82ded2671e52eaf2cc62695b3635d
SwathiPriyaRV <66841534+Swathi23345@users.noreply.github.com>
2023-09-22 17:24:05 +0530

    Update dashboard.py

--- dashboard/HuggingFace/dashboard.py
+++ dashboard/HuggingFace/dashboard.py
@@ -68,7 +68,7 @@ class Dashboard():
 
     def workflow_last_run(self): 
         workflows_to_include = self.get_all_workflow_names()
-        normalized_workflows = [workflow_name for workflow_name in workflows_to_include]
+        normalized_workflows = [workflow_name.replace("/","-") for workflow_name in workflows_to_include]
         # normalized_workflows = [hf_name for hf_name in workflows_to_include]
         # hf_name = [hf_name for hf_name in workflows_to_include]
         #print(workflow_name)
