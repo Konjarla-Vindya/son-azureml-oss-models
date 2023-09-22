7ab655857c3495e138918a67d5af885edf898962
Amarah <83214142+amarahAI@users.noreply.github.com>
2023-09-22 17:30:48 +0530

    Update FT_TC_Test.py

--- tests/src/automation_for_constant_library/FT_TC_Test.py
+++ tests/src/automation_for_constant_library/FT_TC_Test.py
@@ -2,52 +2,125 @@ import os
 import mlflow
 import transformers
 import numpy as np
-from datasets import load_dataset, load_metric
+from datasets import load_metric
 from transformers import (
     AutoModelForTokenClassification,
     AutoTokenizer,
     TrainingArguments,
     Trainer,
     DataCollatorForTokenClassification,
-    pipeline,
 )
 
-def fine_tune_and_register_model(model_name, task):
-    # Define the dataset, batch size, and other relevant parameters
+def fine_tune_model(model_name, task):
+    # Define functions
+    def create_training_args(model_name, task, batch_size=16, num_train_epochs=3):
+        return TrainingArguments(
+            f"{model_name}-finetuned-{task}",
+            evaluation_strategy="epoch",
+            learning_rate=2e-5,
+            per_device_train_batch_size=batch_size,
+            per_device_eval_batch_size=batch_size,
+            num_train_epochs=num_train_epochs,
+            weight_decay=0.01,
+            push_to_hub=False,
+        )
+
+    def create_data_collator(tokenizer):
+        return DataCollatorForTokenClassification(tokenizer)
+
+    def create_compute_metrics(label_list):
+        metric = load_metric("seqeval")
+
+        def compute_metrics(p):
+            predictions, labels = p
+            predictions = np.argmax(predictions, axis=2)
+
+            true_predictions = [
+                [label_list[p] for (p, l) in zip(prediction, label) if l != -100]
+                for prediction, label in zip(predictions, labels)
+            ]
+            true_labels = [
+                [label_list[l] for (p, l) in zip(prediction, label) if l != -100]
+                for prediction, label in zip(predictions, labels)
+            ]
+
+            results = metric.compute(predictions=true_predictions, references=true_labels)
+            return {
+                "precision": results["overall_precision"],
+                "recall": results["overall_recall"],
+                "f1": results["overall_f1"],
+                "accuracy": results["overall_accuracy"],
+            }
+        
+    def tokenize_and_align_labels(dataset, tokenizer, task, label_all_tokens=True):
+        def tokenize_and_align(examples):
+            tokenized_inputs = tokenizer(examples["tokens"], truncation=True, is_split_into_words=True)
+
+            labels = []
+            for i, label in enumerate(examples[f"{task}_tags"]):
+                word_ids = tokenized_inputs.word_ids(batch_index=i)
+                previous_word_idx = None
+                label_ids = []
+                for word_idx in word_ids:
+                    if word_idx is None:
+                        label_ids.append(-100)
+                    elif word_idx != previous_word_idx:
+                        label_ids.append(label[word_idx])
+                    else:
+                        label_ids.append(label[word_idx] if label_all_tokens else -100)
+                    previous_word_idx = word_idx
+
+                labels.append(label_ids)
+
+            tokenized_inputs["labels"] = labels
+            return tokenized_inputs
+
+        tokenized_datasets = dataset.map(
+            lambda examples: tokenize_and_align(examples),
+            batched=True,
+        )
+
+        return tokenized_datasets
+
+    def load_custom_dataset(dataset_name, task, batch_size):
+        datasets = load_dataset(dataset_name)
+
+        if task == "ner":
+            label_list = datasets["train"].features[f"{task}_tags"].feature.names
+        else:
+            label_list = None
+
+        print(f"Loaded dataset: {dataset_name}")
+        print(f"Task: {task}")
+        print(f"Label List: {label_list}")
+        print(f"Batch Size: {batch_size}")
+
+        return datasets, label_list, batch_size
+
+    # Load model and tokenizer
+    model_source_uri = os.environ.get('model_source_uri')
+    loaded_model = mlflow.transformers.load_model(model_uri=model_source_uri, return_type="pipeline")
+    tokenizer = loaded_model.tokenizer
+
+    # Load dataset and preprocess
     dataset_name = "conll2003"
     batch_size = 16
-    num_train_epochs = 3
+    datasets, label_list, batch_size = load_custom_dataset(dataset_name, task, batch_size)
     label_all_tokens = True
-
-    # Load the dataset
-    datasets = load_dataset(dataset_name)
-    label_list = datasets["train"].features[f"{task}_tags"].feature.names if task == "ner" else None
-
-    # Load the tokenizer and tokenize the dataset
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
     tokenized_datasets = tokenize_and_align_labels(datasets, tokenizer, task, label_all_tokens)
-
-    # Define training arguments and data collator
-    training_args = TrainingArguments(
-        f"{model_name}-finetuned-{task}",
-        evaluation_strategy="epoch",
-        learning_rate=2e-5,
-        per_device_train_batch_size=batch_size,
-        per_device_eval_batch_size=batch_size,
-        num_train_epochs=num_train_epochs,
-        weight_decay=0.01,
-        push_to_hub=False,
-    )
-    data_collator = DataCollatorForTokenClassification(tokenizer)
+    metric = load_metric("seqeval")
     
-    # Define the model for token classification
+    # Initialize training arguments, model, and trainer
+    num_train_epochs = 3
+    training_args = create_training_args(model_name, task, batch_size, num_train_epochs)
+    data_collator = create_data_collator(tokenizer)
+    compute_metrics_fn = create_compute_metrics(label_list)
     model = AutoModelForTokenClassification.from_pretrained(model_name, num_labels=9)
     
-    # Create a subset of the training and validation datasets
+    # Subset and fine-tune
     subset_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(100))
     subset_validation_dataset = tokenized_datasets["validation"].shuffle(seed=42).select(range(100))
     
-    # Create a Trainer for fine-tuning
     trainer = Trainer(
         model=model,
         args=training_args,
@@ -55,37 +128,37 @@ def fine_tune_and_register_model(model_name, task):
         eval_dataset=subset_validation_dataset,
         data_collator=data_collator,
         tokenizer=tokenizer,
-        compute_metrics=create_compute_metrics(label_list),
+        compute_metrics=compute_metrics_fn
     )
-
-    # Fine-tune the model
+    
     fine_tune_results = trainer.train()
+    print(fine_tune_results)
     evaluation_results = trainer.evaluate()
-
-    # Save the fine-tuned model and tokenizer
+    print(evaluation_results)
+    
+    # Save the fine-tuned model
     save_directory = f"./fine_tuned_model_{task}"
     trainer.save_model(save_directory)
+    fine_tuned_model = AutoModelForTokenClassification.from_pretrained(save_directory)
     tokenizer.save_pretrained(save_directory)
+    fine_tuned_tokenizer = AutoTokenizer.from_pretrained(save_directory)
+    print(fine_tuned_model)
+    print(tokenizer)
+    mlflow.end_run()
     
-    # Create a pipeline for inference with the fine-tuned model
-    model_pipeline = pipeline(task="token-classification", model=save_directory, tokenizer=save_directory)
-
-    # Register the fine-tuned model with MLflow
+    # Register the model with MLflow
+    model_pipeline = transformers.pipeline(task="token-classification", model=fine_tuned_model, tokenizer=fine_tuned_tokenizer)
+    model_name = f"ft-tkc-{model_name}"
     with mlflow.start_run():
         model_info = mlflow.transformers.log_model(
             transformers_model=model_pipeline,
-            artifact_path=f"ft-tkc-{model_name}-{task}"
+            artifact_path=model_name
         )
     
-    registered_model = mlflow.register_model(model_info.model_uri, f"ft-tkc-{model_name}-{task}")
-    
-    return registered_model
-
+    registered_model = mlflow.register_model(model_info.model_uri, model_name)
+    print(registered_model)
 
 if __name__ == "__main__":
-  model_name = os.environ.get('test_model_name')
-  task = "ner"
-  print("Model Name:", model_name)
-  print("Task:", task)
-  registered_model = fine_tune_and_register_model(model_name, task)
-  print("Registered Model:", registered_model)
+    model_name = os.environ.get('test_model_name')
+    task = "ner"
+    fine_tune_model(model_name, task)
