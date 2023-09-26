5320eebb02f668d36be185d1502f46507c770cd6
PoojaRam12 <139552501+PoojaRam12@users.noreply.github.com>
2023-09-26 16:23:31 +0530

    Update initial.py

--- tests/src/automation_for_constant_library/initial.py
+++ tests/src/automation_for_constant_library/initial.py
@@ -271,8 +271,8 @@ if __name__ == "__main__":
     # LM=load_model(model_detail)
     # print("LM-----------------------------",LM)
     environment_variables = {"test_model_name": foundation_model,
-                            workspace_ml_client=workspace_ml_client,registry=queue.registry,foundation_model_ID=foundation_model.id,
-                            queue=queue.compute,workspace=queue.workspace}
+                            "workspace_ml_client":workspace_ml_client,"registry":queue.registry,"foundation_model_ID":foundation_model.id,
+                            "queue":queue.compute,"workspace":queue.workspace}
     print("environment_variables-------------",environment_variables)
     print("queue.compute---", queue.compute)
     print("queue.workspace====", queue.workspace)
