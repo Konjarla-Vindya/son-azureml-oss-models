cb92a4cbcb15cdbd20ddd49013fdd9b1a4759176
SwathiPriyaRV <66841534+Swathi23345@users.noreply.github.com>
2023-09-27 13:00:50 +0530

    Update dashboard.py

--- dashboard/HuggingFace/dashboard.py
+++ dashboard/HuggingFace/dashboard.py
@@ -20,7 +20,7 @@ class Dashboard():
         }
         self.models_data = []  # Initialize models_data as an empty list
 
-    def get_all_workflow_names(self,limit=30):
+    def get_all_workflow_names(self,limit=50):
         #workflow_name = ["MLFlow-codellama/CodeLlama-13b-Instruct-hf","MLFlow-mosaicml/mpt-7b-storywriter","MLFlow-microsoft/MiniLM-L12-H384-uncased"]
         API = "https://api.github.com/repos/Konjarla-Vindya/son-azureml-oss-models/actions/workflows"
         print (f"Getting github workflows from {API}")
