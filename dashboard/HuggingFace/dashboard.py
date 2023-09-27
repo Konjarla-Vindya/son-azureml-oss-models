48ec2febd15d22a62a7716d4ccd4d739851aaa77
SwathiPriyaRV <66841534+Swathi23345@users.noreply.github.com>
2023-09-27 13:56:06 +0530

    Update dashboard.py

--- dashboard/HuggingFace/dashboard.py
+++ dashboard/HuggingFace/dashboard.py
@@ -75,7 +75,7 @@ class Dashboard():
         # hf_name = [hf_name for hf_name in workflows_to_include]
         #print(workflow_name)
         # print(hf_name)
-        for workflow_name in normalized_workflows:
+        for workflow_actual_name, workflow_name in zip(workflow_actual_names, normalized_workflows):
             try:
                 workflow_runs_url = f"https://api.github.com/repos/{self.repo_full_name}/actions/workflows/{workflow_name}.yml/runs"
                 response = requests.get(workflow_runs_url, headers={"Authorization": f"Bearer {self.github_token}", "Accept": "application/vnd.github.v3+json"})
@@ -126,7 +126,7 @@ class Dashboard():
                     #self.data["badge"].append(f"[![{workflow_name}]({badge_url})]({url})")
                 run_link = f"https://github.com/{self.repo_full_name}/actions/runs/{last_run['id']}"
                 models_entry = {
-                    "Model": workflow_name,
+                    "Model": workflow_actual_name,
                     # "HFLink": f"[Link](https://huggingface.co/{workflow_name.replace(".yml", "").replace("MLFlow-","")})",
                     # "Status": "<span style='background-color: #00FF00; padding: 2px 6px; border-radius: 3px;'>PASS</span>" if last_run["conclusion"] == "success" else "<span style='background-color: #FF0000; padding: 2px 6px; border-radius: 3px;'>FAIL</span>",
                     # "Status": " ✅ PASS" if last_run["conclusion"] == "success" elif last_run["conclusion"] == "failure" "❌ FAIL",
