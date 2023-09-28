d3684038f7f850755d03807073ff72f17c6b323c
SwathiPriyaRV <66841534+Swathi23345@users.noreply.github.com>
2023-09-28 16:14:41 +0530

    Update dashboard_tasks.py

--- dashboard/HuggingFace/dashboard_tasks.py
+++ dashboard/HuggingFace/dashboard_tasks.py
@@ -228,7 +228,7 @@ class Dashboard():
         #     f.write(os.linesep)
         #     f.write(models_md)
 
-        with open("README.md", "w", encoding="utf-8") as f:
+        with open("dashboard_tasks.md", "w", encoding="utf-8") as f:
             f.write(summary_text)
             f.write(os.linesep)
             f.write(os.linesep)
