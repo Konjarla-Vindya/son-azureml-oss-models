9771c69a44739d955122de9def016dcc47441c52
SameepPanigrahi <v-sameepp@microsoft.com>
2023-10-03 17:17:32 +0530

    Adding try catch to the class for tracking

--- tests/src/pipeline_component/azureml_pipeline.py
+++ tests/src/pipeline_component/azureml_pipeline.py
@@ -54,26 +54,32 @@ class AzurePipeline:
             raise Exception(ex)
 
     def run_pipeline(self, data_path, foundation_model):
-        pipeline_jobs = []
-        experiment_name = "text-translation-evaluation"
-        pipeline_object = self.evaluation_pipeline(
-                mlflow_model=Input(type=AssetTypes.MLFLOW_MODEL, path=f"{foundation_model.id}"),
-                data_path = data_path
+        try:
+            pipeline_jobs = []
+            experiment_name = "text-translation-evaluation"
+            pipeline_object = self.evaluation_pipeline(
+                    mlflow_model=Input(type=AssetTypes.MLFLOW_MODEL, path=f"{foundation_model.id}"),
+                    data_path = data_path,
+                )
+            # don't reuse cached results from previous jobs
+            pipeline_object.settings.force_rerun = True
+            pipeline_object.settings.default_compute = COMPUTE_CLUSTER
+
+            # set continue on step failure to False
+            pipeline_object.settings.continue_on_step_failure = False
+
+            timestamp = str(int(time.time()))
+            pipeline_object.display_name = f"eval-{foundation_model.name}-{timestamp}"
+            pipeline_job = self.workspace_ml_client.jobs.create_or_update(
+                pipeline_object, experiment_name=experiment_name
             )
-        # don't reuse cached results from previous jobs
-        pipeline_object.settings.force_rerun = True
-        pipeline_object.settings.default_compute = COMPUTE_CLUSTER
-
-        # set continue on step failure to False
-        pipeline_object.settings.continue_on_step_failure = False
-
-        timestamp = str(int(time.time()))
-        pipeline_object.display_name = f"eval-{foundation_model.name}-{timestamp}"
-        pipeline_job = self.workspace_ml_client.jobs.create_or_update(
-            pipeline_object, experiment_name=experiment_name
-        )
-        # add model['name'] and pipeline_job.name as key value pairs to a dictionary
-        pipeline_jobs.append({"model_name": foundation_model.name, "job_name": pipeline_job.name})
-        # wait for the pipeline job to complete
-        self.workspace_ml_client.jobs.stream(pipeline_job.name)
-        return pipeline_jobs
+            # add model['name'] and pipeline_job.name as key value pairs to a dictionary
+            pipeline_jobs.append({"model_name": foundation_model.name, "job_name": pipeline_job.name})
+            # wait for the pipeline job to complete
+            self.workspace_ml_client.jobs.stream(pipeline_job.name)
+            return pipeline_jobs
+        except Exception as ex:
+            _, _, exc_tb = sys.exc_info()
+            logger.error(f"The exception occured at this line no : {exc_tb.tb_lineno}" +
+                         f" the exception is this one : \n {ex}")
+            raise Exception(ex)
