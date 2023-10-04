0fd62d072cc8301c1ed0966a501d7d54af61e834
SameepPanigrahi <v-sameepp@microsoft.com>
2023-10-04 11:07:49 +0530

    Refactoring

--- tests/src/pipeline_component/dataset_loader.py
+++ tests/src/pipeline_component/dataset_loader.py
@@ -21,9 +21,11 @@ class LoadDataset:
                 test_data_df = pd.DataFrame(hf_test_data.take(100))
                 test_data_df["input_string"] = test_data_df["translation"].apply(lambda x: x["en"])
                 test_data_df["ro"] = test_data_df["translation"].apply(lambda x: x["ro"])
-                test_data_df.to_json(self.data_path, lines=True, orient="records")
+                with open(self.data_path, "w") as f:
+                    f.write(test_data_df.to_json(lines=True, orient="records"))
+                # test_data_df.to_json(self.data_path, lines=True, orient="records")
                 df = pd.read_json(self.data_path, lines=True)
-                logger.info(f"Here is ths value{df.head(0)}")
+                logger.info(f"Here is ths value{df.head(2)}")
         except Exception as ex:
             _, _, exc_tb = sys.exc_info()
             logger.error(
