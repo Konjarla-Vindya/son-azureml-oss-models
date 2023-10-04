5b1603d0bb10de62070264739b1d91dfacda5c9a
PoojaRam12 <139552501+PoojaRam12@users.noreply.github.com>
2023-10-04 14:39:19 +0530

    Update Batch_Endpoint.py

--- tests/src/automation_for_constant_library/Batch_Endpoint.py
+++ tests/src/automation_for_constant_library/Batch_Endpoint.py
@@ -89,10 +89,11 @@ def get_task_specified_input(task):
     folder_input = Input(path=folder_path, type=AssetTypes.URI_FOLDER)
     
     # Now you can include both the folder input and individual file inputs in the job
-    job_inputs = [folder_input] + inputs
-    return job_inputs
     # job_inputs = [folder_input] + inputs
-    # return folder_path
+    # return job_inputs
+    job_inputs = [folder_input] + inputs
+    print("job_inputs:", {job_inputs})
+    return folder_path
     
     # # Invoke the batch endpoint with the list of inputs
     # job = workspace_ml_client.batch_endpoints.invoke(endpoint_name=endpoint.name, input=job_inputs)
