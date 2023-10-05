0e21b1f2b7f418c0d5a04f897fd06afb9555763d
PoojaRam12 <139552501+PoojaRam12@users.noreply.github.com>
2023-10-05 21:02:45 +0530

    Update Batch_Endpoint.py

--- tests/src/automation_for_constant_library/Batch_Endpoint.py
+++ tests/src/automation_for_constant_library/Batch_Endpoint.py
@@ -105,10 +105,8 @@ def get_task_specified_input(task, test_model_name):
             file_input = Input(path=file_path, type=AssetTypes.URI_FILE)
             # Handle the "fill-mask" task by replacing [MASK] with <mask> in the input data
             if task.lower() == "fill-mask":
-                tokenizer = AutoTokenizer.from_pretrained(
-                test_model_name)
-                #tokenizer = AutoTokenizer.from_pretrained(
-                test_model_name, trust_remote_code=True, use_auth_token=True)
+                tokenizer = AutoTokenizer.from_pretrained(test_model_name)
+                #tokenizer = AutoTokenizer.from_pretrained(test_model_name, trust_remote_code=True, use_auth_token=True)
                 mask_token = tokenizer.mask_token  
                 process_input_for_fill_mask_task(file_path, mask_token)
             # if task.lower() == "fill-mask":
