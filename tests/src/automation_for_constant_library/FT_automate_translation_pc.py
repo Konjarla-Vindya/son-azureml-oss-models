bafa29024a555866bdd4574ccfc1eb56e4995650
priyaray21 <139554535+priyaray21@users.noreply.github.com>
2023-10-11 17:16:14 +0530

    Update FT_automate_translation_pc.py

--- tests/src/automation_for_constant_library/FT_automate_translation_pc.py
+++ tests/src/automation_for_constant_library/FT_automate_translation_pc.py
@@ -19,6 +19,141 @@ compute_cluster_size = config['compute_cluster_size']
 compute_cluster = config['compute_cluster']
 experiment_name = config['experiment_name']
 
+# from azure.ai.ml.entities import MLClient
+
+check_override = True
+
+
+# def get_error_messages():
+#     # load ../config/errors.json into a dictionary
+#     with open('../../config/errors.json') as f:
+#         return json.load(f)
+
+
+# error_messages = get_error_messages()
+
+# model to test
+test_model_name = os.environ.get('test_model_name')
+
+# test cpu or gpu template
+test_sku_type = os.environ.get('test_sku_type')
+
+# bool to decide if we want to trigger the next model in the queue
+test_trigger_next_model = os.environ.get('test_trigger_next_model')
+
+# test queue name - the queue file contains the list of models to test with with a specific workspace
+test_queue = os.environ.get('test_queue')
+
+# test set - the set of queues to test with. a test queue belongs to a test set
+test_set = os.environ.get('test_set')
+
+# bool to decide if we want to keep looping through the queue,
+# which means that the first model in the queue is triggered again after the last model is tested
+test_keep_looping = os.environ.get('test_keep_looping')
+
+# function to load the workspace details from test queue file
+# even model we need to test belongs to a queue. the queue name is passed as environment variable test_queue
+# the queue file contains the list of models to test with with a specific workspace
+# the queue file also contains the details of the workspace, registry, subscription, resource group
+def get_test_queue() -> ConfigBox:
+    queue_file = f"../../config/queue/{test_set}/{test_queue}.json"
+    with open(queue_file) as f:
+        return ConfigBox(json.load(f))
+# function to load the sku override details from sku-override file
+# this is useful if you want to force a specific sku for a model
+
+
+def get_sku_override():
+    try:
+        with open('../../config/sku-override/{test_set}.json') as json_file:
+            return json.load(json_file)
+    except Exception as e:
+        print(f"::warning:: Could not find sku-override file: \n{e}")
+        return None
+    # finds the next model in the queue and sends it to github step output
+# so that the next step in this job can pick it up and trigger the next model using 'gh workflow run' cli command
+def set_next_trigger_model(queue):
+    print("In set_next_trigger_model...")
+# file the index of test_model_name in models list queue dictionary
+    model_list = list(queue.models)
+    #model_name_without_slash = test_model_name.replace('/', '-')
+    index = model_list.index(test_model_name)
+    #index = model_list.index(test_model_name)
+    print(f"index of {test_model_name} in queue: {index}")
+# if index is not the last element in the list, get the next element in the list
+    if index < len(model_list) - 1:
+        next_model = model_list[index + 1]
+    else:
+        if (test_keep_looping == "true"):
+            next_model = queue[0]
+        else:
+            print("::warning:: finishing the queue")
+            next_model = ""
+# write the next model to github step output
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        print(f'NEXT_MODEL={next_model}')
+        print(f'NEXT_MODEL={next_model}', file=fh)
+
+
+# def create_or_get_compute_target(ml_client,  compute):
+#     cpu_compute_target = compute
+#     try:
+#         compute = ml_client.compute.get(cpu_compute_target)
+#     except Exception:
+#         print("Creating a new cpu compute target...")
+#         compute = AmlCompute(
+#             name=cpu_compute_target, size=compute, min_instances=0, max_instances=3, idle_time_before_scale_down = 120
+#         )
+#         ml_client.compute.begin_create_or_update(compute).result()
+#     print(f"New compute target created: {compute.name}")
+#     return compute
+def run_azure_ml_job(code, command_to_run, environment, compute, environment_variables):
+    command_job = command(
+        code=code,
+        command=command_to_run,
+        environment=environment,
+        compute=compute,
+        environment_variables=environment_variables
+    )
+    return command_job
+def create_and_get_job_studio_url(command_job, workspace_ml_client):
+
+    #ml_client = mlflow.tracking.MlflowClient()
+    returned_job = workspace_ml_client.jobs.create_or_update(command_job)
+    # wait for the job to complete
+    workspace_ml_client.jobs.stream(returned_job.name)
+    return returned_job.studio_url
+def get_latest_model_version(workspace_ml_client, test_model_name):
+    print("In get_latest_model_version...")
+    version_list = list(workspace_ml_client.models.list(test_model_name))
+    
+    if len(version_list) == 0:
+        print("Model not found in registry")
+        foundation_model_name = None  # Set to None if the model is not found
+        foundation_model_id = None  # Set id to None as well
+    else:
+        model_version = version_list[0].version
+        foundation_model = workspace_ml_client.models.get(
+            test_model_name, model_version)
+        print(
+            "\n\nUsing model name: {0}, version: {1}, id: {2} for inferencing".format(
+                foundation_model.name, foundation_model.version, foundation_model.id
+            )
+        )
+        foundation_model_name = foundation_model.name  # Assign the value to a new variable
+        foundation_model_id = foundation_model.id  # Assign the id to a new variable
+        # Check if foundation_model_name and foundation_model_id are None or have values
+    if foundation_model_name and foundation_model_id:
+        print(f"Latest model {foundation_model_name} version {foundation_model.version} created at {foundation_model.creation_context.created_at}")
+        print("foundation_model.name:", foundation_model_name)
+        print("foundation_model.id:", foundation_model_id)
+    else:
+        print("No model found in the registry.")
+    
+    #print(f"Model Config : {latest_model.config}")
+    return foundation_model
+
+
 # Define the Azure Machine Learning client and credential setup
 try:
     credential = DefaultAzureCredential()
