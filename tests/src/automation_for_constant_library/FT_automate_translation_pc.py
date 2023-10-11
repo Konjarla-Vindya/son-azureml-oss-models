701377172e3b59613411b098bcbcc18d5dbb31f6
priyaray21 <139554535+priyaray21@users.noreply.github.com>
2023-10-11 18:44:04 +0530

    Update FT_automate_translation_pc.py

--- tests/src/automation_for_constant_library/FT_automate_translation_pc.py
+++ tests/src/automation_for_constant_library/FT_automate_translation_pc.py
@@ -1,12 +1,19 @@
 import os
 import time
 import json
+import sys
+import mlflow
+import ast
 import pandas as pd
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from azure.ai.ml import command
+from box import ConfigBox
 from azure.ai.ml import MLClient
 from azure.identity import DefaultAzureCredential, InteractiveBrowserCredential
 from azure.ai.ml.entities import AmlCompute
 from azure.ai.ml.dsl import pipeline
-from azure.ai.ml.entities import Input
+from azure.ai.ml.entities import CommandComponent, PipelineComponent, Job, Component
+from azure.ai.ml import PyTorchDistribution, Input
 
 # Load configuration from a JSON file
 with open('configpc.json') as config_file:
@@ -19,18 +26,10 @@ compute_cluster_size = config['compute_cluster_size']
 compute_cluster = config['compute_cluster']
 experiment_name = config['experiment_name']
 
-# from azure.ai.ml.entities import MLClient
 
 check_override = True
 
 
-# def get_error_messages():
-#     # load ../config/errors.json into a dictionary
-#     with open('../../config/errors.json') as f:
-#         return json.load(f)
-
-
-# error_messages = get_error_messages()
 
 # model to test
 test_model_name = os.environ.get('test_model_name')
@@ -95,18 +94,6 @@ def set_next_trigger_model(queue):
         print(f'NEXT_MODEL={next_model}', file=fh)
 
 
-# def create_or_get_compute_target(ml_client,  compute):
-#     cpu_compute_target = compute
-#     try:
-#         compute = ml_client.compute.get(cpu_compute_target)
-#     except Exception:
-#         print("Creating a new cpu compute target...")
-#         compute = AmlCompute(
-#             name=cpu_compute_target, size=compute, min_instances=0, max_instances=3, idle_time_before_scale_down = 120
-#         )
-#         ml_client.compute.begin_create_or_update(compute).result()
-#     print(f"New compute target created: {compute.name}")
-#     return compute
 def run_azure_ml_job(code, command_to_run, environment, compute, environment_variables):
     command_job = command(
         code=code,
@@ -154,24 +141,24 @@ def get_latest_model_version(workspace_ml_client, test_model_name):
     return foundation_model
 
 
-# Define the Azure Machine Learning client and credential setup
-try:
-    credential = DefaultAzureCredential()
-    credential.get_token("https://management.azure.com/.default")
-except Exception as ex:
-    credential = InteractiveBrowserCredential()
+# # Define the Azure Machine Learning client and credential setup
+# try:
+#     credential = DefaultAzureCredential()
+#     credential.get_token("https://management.azure.com/.default")
+# except Exception as ex:
+#     credential = InteractiveBrowserCredential()
 
-try:
-    workspace_ml_client = MLClient.from_config(credential=credential)
-except:
-    workspace_ml_client = MLClient(
-        credential,
-        subscription_id=subscription_id,
-        resource_group_name=resource_group_name,
-        workspace_name=workspace_name,
-    )
+# try:
+#     workspace_ml_client = MLClient.from_config(credential=credential)
+# except:
+#     workspace_ml_client = MLClient(
+#         credential,
+#         subscription_id=subscription_id,
+#         resource_group_name=resource_group_name,
+#         workspace_name=workspace_name,
+#     )
     
-registry_ml_client = MLClient(credential, registry_name="azureml-preview-test1")
+#registry_ml_client = MLClient(credential, registry_name="azureml-preview-test1")
 
 # download the dataset using the helper script. This needs datasets library: https://pypi.org/project/datasets/
 import os
@@ -204,14 +191,14 @@ test_df.sample(frac=frac).to_json(
 )
 
 
-# Compute Cluster Creation
-try:
-    compute = workspace_ml_client.compute.get(compute_cluster)
-    print("The compute cluster already exists! Reusing it for the current run")
-except Exception as ex:
-    print(
-        f"Looks like the compute cluster doesn't exist. Creating a new one with compute size {compute_cluster_size}!"
-    )
+# # Compute Cluster Creation
+# try:
+#     compute = workspace_ml_client.compute.get(compute_cluster)
+#     print("The compute cluster already exists! Reusing it for the current run")
+# except Exception as ex:
+#     print(
+#         f"Looks like the compute cluster doesn't exist. Creating a new one with compute size {compute_cluster_size}!"
+#     )
 #     # Create a new compute cluster 
 #     import ast
 
@@ -311,42 +298,195 @@ except Exception as ex:
 #         f"Number of GPU's in compute {compute.size} not found. Available skus are: {available_sku_sizes}."
 #         f"This should not happen. Please check the selected compute cluster: {compute_cluster} and try again."
 #     )
-# fetch the pipeline component
-pipeline_component_func = registry_ml_client.components.get(
-    name="translation_pipeline", label="latest"
-)
-# fetch Model from WS    
-model_name = "t5-small"
-foundation_model = workspace_ml_client.models.get(model_name, label="latest")
-print(
-    "\n\nUsing model name: {0}, version: {1}, id: {2} for fine tuning".format(
-        foundation_model.name, foundation_model.version, foundation_model.id
-    )
-)
-# Input data specification as a dictionary
-input_data = {
-    "train_file_path": {
-        "type": "uri_file",
-        "path": "./wmt16-en-ro-dataset/small_train.jsonl"
-    },
-    "validation_file_path": {
-        "type": "uri_file",
-        "path": "./wmt16-en-ro-dataset/small_validation.jsonl"
-    },
-    "test_file_path": {
-        "type": "uri_file",
-        "path": "./wmt16-en-ro-dataset/small_test.jsonl"
-    },
-    "evaluation_config": {
-        "type": "uri_file",
-        "path": "./translation-config.json"
-    }
-}
+# # fetch the pipeline component
+# pipeline_component_func = registry_ml_client.components.get(
+#     name="translation_pipeline", label="latest"
+# )
+# # fetch Model from WS    
+# model_name = "t5-small"
+# foundation_model = workspace_ml_client.models.get(model_name, label="latest")
+# print(
+#     "\n\nUsing model name: {0}, version: {1}, id: {2} for fine tuning".format(
+#         foundation_model.name, foundation_model.version, foundation_model.id
+#     )
+# )
+# # Input data specification as a dictionary
+# input_data = {
+#     "train_file_path": {
+#         "type": "uri_file",
+#         "path": "./wmt16-en-ro-dataset/small_train.jsonl"
+#     },
+#     "validation_file_path": {
+#         "type": "uri_file",
+#         "path": "./wmt16-en-ro-dataset/small_validation.jsonl"
+#     },
+#     "test_file_path": {
+#         "type": "uri_file",
+#         "path": "./wmt16-en-ro-dataset/small_test.jsonl"
+#     },
+#     "evaluation_config": {
+#         "type": "uri_file",
+#         "path": "./translation-config.json"
+#     }
+# }
 
-# Model Training and Pipeline Setup
-@pipeline()
-def create_pipeline():
-    translation_pipeline = pipeline_component_func(**input_data,
+# # Model Training and Pipeline Setup
+# @pipeline()
+# def create_pipeline():
+#     translation_pipeline = pipeline_component_func(**input_data,
+#         # specify the foundation model available in the azureml system registry id identified in step #3
+#         mlflow_model_path=foundation_model.id,
+#         # huggingface_id = 't5-small', # if you want to use a huggingface model, uncomment this line and comment the above line
+#         compute_model_import=compute_cluster,
+#         compute_preprocess=compute_cluster,
+#         compute_finetune=compute_cluster,
+#         compute_model_evaluation=compute_cluster,
+#         # map the dataset splits to parameters
+#         train_file_path=Input(
+#             type="uri_file", path="./wmt16-en-ro-dataset/small_train.jsonl"
+#         ),
+#         validation_file_path=Input(
+#             type="uri_file", path="./wmt16-en-ro-dataset/small_validation.jsonl"
+#         ),
+#         test_file_path=Input(
+#             type="uri_file", path="./wmt16-en-ro-dataset/small_test.jsonl"
+#         ),
+#         evaluation_config=Input(type="uri_file", path="./translation-config.json"),
+#         # The following parameters map to the dataset fields
+#         # source_lang parameter maps to the "en" field in the wmt16 dataset
+#         source_lang="en",
+#          # target_lang parameter maps to the "ro" field in the wmt16 dataset
+#         target_lang="ro",
+#         # training settings
+#         number_of_gpu_to_use_finetuning=gpus_per_node,  # set to the number of GPUs available in the compute
+#         **training_parameters,
+#         **optimization_parameters
+#     )
+#     return {
+#         # map the output of the fine tuning job to the output of the pipeline job so that we can easily register the fine tuned model
+#         # registering the model is required to deploy the model to an online or batch endpoint
+#         "trained_model": translation_pipeline.outputs.mlflow_model_folder
+#     }
+    
+    # Training parameters
+def get_training_and_optimization_parameters(foundation_model):
+    # Training parameters
+    training_parameters = {
+        "num_train_epochs": 3,
+        "per_device_train_batch_size": 1,
+        "per_device_eval_batch_size": 1,
+        "learning_rate": 2e-5,
+        "metric_for_best_model": "bleu",
+    }
+    print(f"The following training parameters are enabled - {training_parameters}")
+    # Optimization parameters
+    if "model_specific_defaults" in foundation_model.tags:
+        optimization_parameters = ast.literal_eval(foundation_model.tags["model_specific_defaults"])
+    else:
+        optimization_parameters = {
+            "apply_lora": "true",
+            "apply_deepspeed": "true",
+            "apply_ort": "true",
+        }
+    print(f"The following optimizations are enabled - {optimization_parameters}")
+
+    return training_parameters, optimization_parameters
+
+# # Optimization parameters - As these parameters are packaged with the model itself, lets retrieve those parameters
+# if "model_specific_defaults" in foundation_model.tags:
+#     optimization_parameters = ast.literal_eval(
+#         foundation_model.tags["model_specific_defaults"]
+#     )  # convert string to python dict
+# else:
+#     optimization_parameters = dict(
+#         apply_lora="true", apply_deepspeed="true", apply_ort="true"
+#     )
+# print(f"The following optimizations are enabled - {optimization_parameters}")
+
+def create_or_get_aml_compute(workspace_ml_client, compute_cluster, compute_cluster_size, computes_allow_list=None):
+    try:
+        compute = workspace_ml_client.compute.get(compute_cluster)
+        print(f"The compute cluster '{compute_cluster}' already exists! Reusing it for the current run")
+    except Exception as ex:
+        print(f"Looks like the compute cluster '{compute_cluster}' doesn't exist. Creating a new one with compute size '{compute_cluster_size}'!")
+
+        # Define a list of VM sizes that are not supported for finetuning
+        unsupported_gpu_vm_list = ["standard_nc6", "standard_nc12", "standard_nc24", "standard_nc24r"]
+
+        try:
+            print("Attempt #1 - Trying to create a dedicated compute")
+            tier = "Dedicated"
+            if compute_cluster_size.lower() in unsupported_gpu_vm_list:
+                raise ValueError(f"VM size '{compute_cluster_size}' is not supported for finetuning.")
+        except ValueError as e:
+            print(e)
+            raise
+
+        try:
+            print("Attempt #2 - Trying to create a low priority compute. Since this is a low priority compute, the job could get pre-empted before completion.")
+            tier = "LowPriority"
+            if compute_cluster_size.lower() in unsupported_gpu_vm_list:
+                raise ValueError(f"VM size '{compute_cluster_size}' is not supported for finetuning.")
+        except ValueError as e:
+            print(e)
+            raise
+
+        # Provision the compute
+        compute = AmlCompute(
+            name=compute_cluster,
+            size=compute_cluster_size,
+            tier=tier,
+            max_instances=2,  # For multi-node training, set this to an integer value more than 1
+        )
+        workspace_ml_client.compute.begin_create_or_update(compute).wait()
+
+    # Sanity check on the created compute
+    compute = workspace_ml_client.compute.get(compute_cluster)
+
+    if compute.provisioning_state.lower() == "failed":
+        raise ValueError(f"Provisioning failed. Compute '{compute_cluster}' is in a failed state. Please try creating a different compute.")
+
+    if computes_allow_list is not None:
+        computes_allow_list_lower_case = [x.lower() for x in computes_allow_list]
+        if compute.size.lower() not in computes_allow_list_lower_case:
+            raise ValueError(f"VM size '{compute.size}' is not in the allow-listed computes for finetuning.")
+    
+    # Determine the number of GPUs in a single node of the selected 'compute_cluster_size' compute
+    gpu_count_found = False
+    workspace_compute_sku_list = workspace_ml_client.compute.list_sizes()
+    available_sku_sizes = []
+
+    for compute_sku in workspace_compute_sku_list:
+        available_sku_sizes.append(compute_sku.name)
+        if compute_sku.name.lower() == compute.size.lower():
+            gpus_per_node = compute_sku.gpus
+            gpu_count_found = True
+
+    # If the GPU count is not found, print an error
+    if gpu_count_found:
+        print(f"Number of GPUs in compute '{compute_cluster}': {gpus_per_node}")
+    else:
+        raise ValueError(f"Number of GPUs in compute '{compute_cluster}' not found. Available skus are: {available_sku_sizes}. This should not happen. Please check the selected compute cluster: {compute_cluster} and try again.")
+    
+    return compute, gpus_per_node, compute_cluster
+
+def create_and_run_azure_ml_pipeline(
+    foundation_model,
+    compute_cluster,
+    gpus_per_node,
+    training_parameters,
+    optimization_parameters,
+    experiment_name,
+):
+
+    # Fetch the pipeline component
+    pipeline_component_func = registry_ml_client.components.get(
+        name="translation_pipeline", label="latest"
+    )
+    # Model Training and Pipeline Setup
+    @pipeline()
+    def create_pipeline():
+        translation_pipeline = pipeline_component_func(
         # specify the foundation model available in the azureml system registry id identified in step #3
         mlflow_model_path=foundation_model.id,
         # huggingface_id = 't5-small', # if you want to use a huggingface model, uncomment this line and comment the above line
@@ -380,28 +520,9 @@ def create_pipeline():
         # registering the model is required to deploy the model to an online or batch endpoint
         "trained_model": translation_pipeline.outputs.mlflow_model_folder
     }
+
+
     
-    # Training parameters
-training_parameters = dict(
-    num_train_epochs=3,
-    per_device_train_batch_size=1,
-    per_device_eval_batch_size=1,
-    learning_rate=2e-5,
-    metric_for_best_model="bleu",
-)
-print(f"The following training parameters are enabled - {training_parameters}")
-
-# Optimization parameters - As these parameters are packaged with the model itself, lets retrieve those parameters
-if "model_specific_defaults" in foundation_model.tags:
-    optimization_parameters = ast.literal_eval(
-        foundation_model.tags["model_specific_defaults"]
-    )  # convert string to python dict
-else:
-    optimization_parameters = dict(
-        apply_lora="true", apply_deepspeed="true", apply_ort="true"
-    )
-print(f"The following optimizations are enabled - {optimization_parameters}")
-
 pipeline_object = create_pipeline()
 
 # Don't use cached results from previous jobs
@@ -417,3 +538,90 @@ pipeline_job = workspace_ml_client.jobs.create_or_update(
 
 # Wait for the pipeline job to complete
 workspace_ml_client.jobs.stream(pipeline_job.name)
+
+if __name__ == "__main__":
+    # if any of the above are not set, exit with error
+    if test_model_name is None or test_sku_type is None or test_queue is None or test_set is None or test_trigger_next_model is None or test_keep_looping is None:
+        print("::error:: One or more of the environment variables test_model_name, test_sku_type, test_queue, test_set, test_trigger_next_model, test_keep_looping are not set")
+        exit(1)
+
+    queue = get_test_queue()
+
+    # sku_override = get_sku_override()
+    # if sku_override is None:
+    #     check_override = False
+
+    if test_trigger_next_model == "true":
+        set_next_trigger_model(queue)
+    # print values of all above variables
+    print (f"test_subscription_id: {queue['subscription']}")
+    print (f"test_resource_group: {queue['resource_group']}")
+    print (f"test_workspace_name: {queue['workspace']}")
+    print (f"test_model_name: {test_model_name}")
+    print (f"test_sku_type: {test_sku_type}")
+    print (f"test_registry: queue['registry']")
+    print (f"test_trigger_next_model: {test_trigger_next_model}")
+    print (f"test_queue: {test_queue}")
+    print (f"test_set: {test_set}")
+    print("Here is my test model name : ", test_model_name)
+    try:
+        credential = DefaultAzureCredential()
+        credential.get_token("https://management.azure.com/.default")
+    except Exception as ex:
+        # Fall back to InteractiveBrowserCredential in case DefaultAzureCredential not work
+        credential = InteractiveBrowserCredential()
+    print("workspace_name : ", queue.workspace)
+    workspace_ml_client = MLClient(
+            credential=credential,
+            subscription_id=queue.subscription,
+            resource_group_name=queue.resource_group,
+            workspace_name=queue.workspace
+        )
+    ws = Workspace(
+        subscription_id=queue.subscription,
+        resource_group=queue.resource_group,
+        workspace_name=queue.workspace
+    )
+    mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
+    registry_ml_client = MLClient(credential, registry_name="azureml-preview-test1")
+    experiment_name = "PC_translation_wmt16"
+
+    # # generating a unique timestamp that can be used for names and versions that need to be unique
+    # timestamp = str(int(time.time()))
+
+    # Define the compute cluster name and size
+    compute_cluster = "gpu-cluster-big"
+    compute_cluster_size = "Standard_NC6s_v3"
+    
+    # Optional: Define a list of allowed compute sizes (if any)
+    computes_allow_list = ["standard_nc6s_v3", "standard_nc12s_v2"]
+    
+    # Call the function
+    compute, gpus_per_node, compute_cluster = create_or_get_aml_compute(workspace_ml_client, compute_cluster, compute_cluster_size, computes_allow_list)
+
+
+
+
+    
+    # compute = create_or_get_compute_target(workspace_ml_client, queue.compute)
+    print("printing:",{compute})
+    env_list = workspace_ml_client.environments.list(name=queue.environment)
+    latest_version = 0
+    for env in env_list:
+        if latest_version <= int(env.version):
+            latest_version = int(env.version)
+    print("Latest Environment Version:", latest_version)
+    latest_env = workspace_ml_client.environments.get(
+        name=queue.environment, version=str(latest_version))
+    print("Latest Environment :", latest_env)
+    version_list = list(workspace_ml_client.models.list(test_model_name))
+
+    client = MlflowClient()
+    
+    #foundation_model, foundation_model_name = get_latest_model_version(workspace_ml_client, test_model_name.lower())
+    foundation_model = get_latest_model_version(workspace_ml_client, test_model_name.lower())
+    training_parameters, optimization_parameters = get_training_and_optimization_parameters(foundation_model)
+    #gpus_per_node = find_gpus_in_compute(workspace_ml_client, compute)
+    print(f"Number of GPUs in compute: {gpus_per_node}")
+    pipeline_job = create_and_run_azure_ml_pipeline(foundation_model, compute_cluster, gpus_per_node, training_parameters, optimization_parameters, experiment_name)
+    print("Completed")
