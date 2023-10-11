150789d98c555e2dbc19654821c338dd1f49eb78
PoojaRam12 <139552501+PoojaRam12@users.noreply.github.com>
2023-10-11 15:40:12 +0530

    Update FT_P_TC.py

--- tests/src/pipeline_component/FT_P_TC.py
+++ tests/src/pipeline_component/FT_P_TC.py
@@ -99,18 +99,18 @@ def set_next_trigger_model(queue):
         print(f'NEXT_MODEL={next_model}', file=fh)
 
 
-def create_or_get_compute_target(ml_client,  compute):
-    cpu_compute_target = compute
-    try:
-        compute = ml_client.compute.get(cpu_compute_target)
-    except Exception:
-        print("Creating a new cpu compute target...")
-        compute = AmlCompute(
-            name=cpu_compute_target, size=compute, min_instances=0, max_instances=3, idle_time_before_scale_down = 120
-        )
-        ml_client.compute.begin_create_or_update(compute).result()
-    print(f"New compute target created: {compute.name}")
-    return compute
+# def create_or_get_compute_target(ml_client,  compute):
+#     cpu_compute_target = compute
+#     try:
+#         compute = ml_client.compute.get(cpu_compute_target)
+#     except Exception:
+#         print("Creating a new cpu compute target...")
+#         compute = AmlCompute(
+#             name=cpu_compute_target, size=compute, min_instances=0, max_instances=3, idle_time_before_scale_down = 120
+#         )
+#         ml_client.compute.begin_create_or_update(compute).result()
+#     print(f"New compute target created: {compute.name}")
+#     return compute
 
 def run_azure_ml_job(code, command_to_run, environment, compute, environment_variables):
     command_job = command(
@@ -203,11 +203,81 @@ def get_training_and_optimization_parameters(foundation_model):
     return training_parameters, optimization_parameters
 
 
-def find_gpus_in_compute(workspace_ml_client, compute):
+# def find_gpus_in_compute(workspace_ml_client, compute):
+#     gpu_count_found = False
+#     workspace_compute_sku_list = workspace_ml_client.compute.list_sizes()
+#     available_sku_sizes = []
+#     gpus_per_node = 0
+
+#     for compute_sku in workspace_compute_sku_list:
+#         available_sku_sizes.append(compute_sku.name)
+#         if compute_sku.name.lower() == compute.size.lower():
+#             gpus_per_node = compute_sku.gpus
+#             gpu_count_found = True
+
+#     if gpu_count_found:
+#         print(f"Number of GPU's in compute {compute.size}: {gpus_per_node}")
+#         return gpus_per_node
+#     else:
+#         raise ValueError(
+#             f"Number of GPU's in compute {compute.size} not found. Available skus are: {available_sku_sizes}. "
+#             f"This should not happen. Please check the selected compute cluster: {compute_cluster} and try again."
+#         )
+
+
+
+def create_or_get_aml_compute(workspace_ml_client, compute_cluster, compute_cluster_size, computes_allow_list=None):
+    try:
+        compute = workspace_ml_client.compute.get(compute_cluster)
+        print(f"The compute cluster '{compute_cluster}' already exists! Reusing it for the current run")
+    except Exception as ex:
+        print(f"Looks like the compute cluster '{compute_cluster}' doesn't exist. Creating a new one with compute size '{compute_cluster_size}'!")
+
+        # Define a list of VM sizes that are not supported for finetuning
+        unsupported_gpu_vm_list = ["standard_nc6", "standard_nc12", "standard_nc24", "standard_nc24r"]
+
+        try:
+            print("Attempt #1 - Trying to create a dedicated compute")
+            tier = "Dedicated"
+            if compute_cluster_size.lower() in unsupported_gpu_vm_list:
+                raise ValueError(f"VM size '{compute_cluster_size}' is not supported for finetuning.")
+        except ValueError as e:
+            print(e)
+            raise
+
+        try:
+            print("Attempt #2 - Trying to create a low priority compute. Since this is a low priority compute, the job could get pre-empted before completion.")
+            tier = "LowPriority"
+            if compute_cluster_size.lower() in unsupported_gpu_vm_list:
+                raise ValueError(f"VM size '{compute_cluster_size}' is not supported for finetuning.")
+        except ValueError as e:
+            print(e)
+            raise
+
+        # Provision the compute
+        compute = AmlCompute(
+            name=compute_cluster,
+            size=compute_cluster_size,
+            tier=tier,
+            max_instances=2,  # For multi-node training, set this to an integer value more than 1
+        )
+        workspace_ml_client.compute.begin_create_or_update(compute).wait()
+
+    # Sanity check on the created compute
+    compute = workspace_ml_client.compute.get(compute_cluster)
+
+    if compute.provisioning_state.lower() == "failed":
+        raise ValueError(f"Provisioning failed. Compute '{compute_cluster}' is in a failed state. Please try creating a different compute.")
+
+    if computes_allow_list is not None:
+        computes_allow_list_lower_case = [x.lower() for x in computes_allow_list]
+        if compute.size.lower() not in computes_allow_list_lower_case:
+            raise ValueError(f"VM size '{compute.size}' is not in the allow-listed computes for finetuning.")
+    
+    # Determine the number of GPUs in a single node of the selected 'compute_cluster_size' compute
     gpu_count_found = False
     workspace_compute_sku_list = workspace_ml_client.compute.list_sizes()
     available_sku_sizes = []
-    gpus_per_node = 0
 
     for compute_sku in workspace_compute_sku_list:
         available_sku_sizes.append(compute_sku.name)
@@ -215,14 +285,14 @@ def find_gpus_in_compute(workspace_ml_client, compute):
             gpus_per_node = compute_sku.gpus
             gpu_count_found = True
 
+    # If the GPU count is not found, print an error
     if gpu_count_found:
-        print(f"Number of GPU's in compute {compute.size}: {gpus_per_node}")
-        return gpus_per_node
+        print(f"Number of GPUs in compute '{compute_cluster}': {gpus_per_node}")
     else:
-        raise ValueError(
-            f"Number of GPU's in compute {compute.size} not found. Available skus are: {available_sku_sizes}. "
-            f"This should not happen. Please check the selected compute cluster: {compute_cluster} and try again."
-        )
+        raise ValueError(f"Number of GPUs in compute '{compute_cluster}' not found. Available skus are: {available_sku_sizes}. This should not happen. Please check the selected compute cluster: {compute_cluster} and try again.")
+    
+    return compute, gpus_per_node
+
 
 
 
@@ -336,6 +406,21 @@ if __name__ == "__main__":
 
     # # generating a unique timestamp that can be used for names and versions that need to be unique
     # timestamp = str(int(time.time()))
+
+    # Define the compute cluster name and size
+    compute_cluster = "gpu-cluster-big"
+    compute_cluster_size = "Standard_NC6s_v3"
+    
+    # Optional: Define a list of allowed compute sizes (if any)
+    computes_allow_list = ["standard_nc6s_v3", "standard_nc12s_v2"]
+    
+    # Call the function
+    compute, gpus_per_node = create_or_get_aml_compute(workspace_ml_client, compute_cluster, compute_cluster_size, computes_allow_list)
+
+
+
+
+    
     compute = create_or_get_compute_target(workspace_ml_client, queue.compute)
     print("printing:",{compute})
     env_list = workspace_ml_client.environments.list(name=queue.environment)
