9e7908a7faa82e37b07f8b622a94a49b7eee49f2
praveen0514 <83938401+praveen0514@users.noreply.github.com>
2023-10-19 16:08:38 +0530

    Update Trigger-model.py

--- tests/src/automation_for_constant_library/Trigger-model.py
+++ tests/src/automation_for_constant_library/Trigger-model.py
@@ -26,11 +26,40 @@
 # if __name__ == "__main__":
 #     first_item = data[0]
 #     print(first_item)
+# import os
+# import json
+
+# # Directory where your JSON files are located
+# json_directory = "../../config/queue/huggingface-all"  #"/path/to/json/files"
+
+# # List all JSON files in the directory
+# json_files = [f for f in os.listdir(json_directory) if f.endswith('.json')]
+
+# # Sort the list of JSON files to process them in a specific order
+# json_files.sort()
+
+# # Function to trigger the model on JSON data
+# def trigger_model(data):
+#     # Replace this with code to trigger your model
+#     print("Model triggered with data:", data)
+
+# if __name__ == "__main__":
+#     for json_file in json_files:
+#         with open(os.path.join(json_directory, json_file), 'r') as file:
+#             data = json.load(file)
+#             if isinstance(data, list) and len(data) > 0:
+#                 first_item = data[0]
+#                 print(first_item)
+#                 #trigger_model(first_item)
 import os
 import json
 
 # Directory where your JSON files are located
 json_directory = "../../config/queue/huggingface-all"  #"/path/to/json/files"
+
+if not os.path.exists(json_directory):
+    print(f"Directory '{json_directory}' does not exist.")
+    exit(1)
 
 # List all JSON files in the directory
 json_files = [f for f in os.listdir(json_directory) if f.endswith('.json')]
@@ -43,12 +72,19 @@ def trigger_model(data):
     # Replace this with code to trigger your model
     print("Model triggered with data:", data)
 
+# This block of code will only execute if this script is run directly, not when it's imported as a module.
 if __name__ == "__main__":
+    # Iterate through JSON files
     for json_file in json_files:
-        with open(os.path.join(json_directory, json_file), 'r') as file:
-            data = json.load(file)
-            if isinstance(data, list) and len(data) > 0:
-                first_item = data[0]
-                print(first_item)
-                #trigger_model(first_item)
+        file_path = os.path.join(json_directory, json_file)
+        try:
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+                if isinstance(data, list) and len(data) > 0:
+                    first_item = data[0]
+                    print(first_item)
+                    # Uncomment the line below to trigger your model with the data.
+                    # trigger_model(first_item)
+        except Exception as e:
+            print(f"Error processing file '{file_path}': {str(e)}")
 
