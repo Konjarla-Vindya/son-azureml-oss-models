1510f9a7f30541d473a4524a50681f82588df37c
priyaray21 <139554535+priyaray21@users.noreply.github.com>
2023-10-20 12:46:38 +0530

    Add files via upload

---
+++ tests/src/automation_for_constant_library/registration_models_ws.py
@@ -0,0 +1,33 @@
+import json
+from azureml.core import Workspace, Model
+import os
+import threading
+from huggingface_hub import HfApi
+import pandas as pd
+import re
+
+# Load configuration from the JSON file
+with open("config_registration.json", "r") as config_file:
+    config = json.load(config_file)
+
+# Function to register a model in a workspace
+def register_model(workspace_name, model_name, model_path):
+    try:
+        workspace = Workspace.get(name=workspace_name, subscription_id=config["subscription_id"], resource_group=config["resource_group"])
+        model = Model.register(workspace, model_name, model_path)
+        print(f"Registered {model.name} in {workspace_name}")
+    except Exception as e:
+        print(f"An error occurred while working with {workspace_name}: {str(e)}")
+
+# Create and start threads for registration
+threads = []
+for workspace_name in config["workspace_names"]:
+    for model_name in config["model_names"]:
+        model_path = os.path.join(config["models_directory"], model_name)
+        thread = threading.Thread(target=register_model, args=(workspace_name, model_name, model_path))
+        thread.start()
+        threads.append(thread)
+
+# Wait for all threads to complete
+for thread in threads:
+    thread.join()
