05a8e41b24db367a76047309730be5eedcdc6c38
praveen0514 <83938401+praveen0514@users.noreply.github.com>
2023-10-20 16:02:59 +0530

    Update Trigger-model.py

--- tests/src/automation_for_constant_library/Trigger-model.py
+++ tests/src/automation_for_constant_library/Trigger-model.py
@@ -26,10 +26,10 @@ for json_file in json_files[:2]:
         # if isinstance(data, list) and len(data) > 0:
         #     first_item = data[0]
         #     print(first_item)
-         repository_owner = "Konjarla-Vindya"
-         repository_name = "son-azureml-oss-models"
-         file_path = ".github/workflows/data1.yml"  # Replace with the desired YAML file path
-         api_url = f"https://api.github.com/repos/{repository_owner}/{repository_name}/blob/main/{file_path}"
+        repository_owner = "Konjarla-Vindya"
+        repository_name = "son-azureml-oss-models"
+        file_path = ".github/workflows/data1.yml"  # Replace with the desired YAML file path
+        api_url = f"https://api.github.com/repos/{repository_owner}/{repository_name}/blob/main/{file_path}"
          print(api_url)
         #     subprocess.run(['gh', 'workflow', 'run', api_url])
 
