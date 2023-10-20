ad28ae73b0fb5d43a64822e09249bdac73d2c813
praveen0514 <83938401+praveen0514@users.noreply.github.com>
2023-10-20 10:56:15 +0530

    Update Trigger-model.py

--- tests/src/automation_for_constant_library/Trigger-model.py
+++ tests/src/automation_for_constant_library/Trigger-model.py
@@ -1,6 +1,7 @@
 import os
 import json
 import requests
+import subprocess
 
 # Directory where your JSON files are located
 json_directory = "../../config/queue/huggingface-all"  #"/path/to/json/files"
@@ -23,41 +24,37 @@ for json_file in json_files:
         if isinstance(data, list) and len(data) > 0:
             first_item = data[0]
             print(first_item)
-            #trigger_model(first_item)
-if __name__ == "__main__":
-    first_item = data[0]
-    print(first_item)
+            # GitHub repository and file information
+            repository_owner = "Konjarla-Vindya"
+            repository_name = "son-azureml-oss-models"
+            file_path = ".github/workflows/data[0].yml"  # Replace with the desired YAML file path
+            # GitHub Personal Access Token (PAT)
+            token = "your-personal-access-token"
+            # Create the API URL for fetching the file content
+            api_url = f"https://api.github.com/repos/{repository_owner}/{repository_name}/contents/{file_path}"
 
-# GitHub repository and file information
-repository_owner = "Konjarla-Vindya"
-repository_name = "son-azureml-oss-models"
-file_path = ".github/workflows/data[0].yml"  # Replace with the desired YAML file path
+            # Create the headers with the Authorization token
+            headers = {
+               "Authorization": f"token {token}"
+                }
+            # Send a GET request to the GitHub API to fetch the file content
+            response = requests.get(api_url, headers=headers)
+            # Check if the request was successful
+           if response.status_code == 200:
+               # Decode the content from base64
+               content = response.json()["content"]
+               content = content.encode("utf-8")
+               content = content.decode("base64")
+               # Print the content (the YAML file)
+               print(content)
+           else:
+               print(f"Failed to fetch the file. Status code: {response.status_code}")
+               #trigger_model(first_item)
+           subprocess.run(['gh', 'workflow', 'run', file_path])
 
-# GitHub Personal Access Token (PAT)
-token = "your-personal-access-token"
 
-# Create the API URL for fetching the file content
-api_url = f"https://api.github.com/repos/{repository_owner}/{repository_name}/contents/{file_path}"
 
-# Create the headers with the Authorization token
-headers = {
-    "Authorization": f"token {token}"
-}
 
-# Send a GET request to the GitHub API to fetch the file content
-response = requests.get(api_url, headers=headers)
-
-# Check if the request was successful
-if response.status_code == 200:
-    # Decode the content from base64
-    content = response.json()["content"]
-    content = content.encode("utf-8")
-    content = content.decode("base64")
-    
-    # Print the content (the YAML file)
-    print(content)
-else:
-    print(f"Failed to fetch the file. Status code: {response.status_code}")
     
     
 
