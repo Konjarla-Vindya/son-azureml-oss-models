95a1c2465ab9ac3a0194fb7672dce14e71958b2d
priyaray21 <139554535+priyaray21@users.noreply.github.com>
2023-10-23 11:42:36 +0530

    Update registration_models_ws.py

--- tests/src/automation_for_constant_library/registration_models_ws.py
+++ tests/src/automation_for_constant_library/registration_models_ws.py
@@ -11,40 +11,25 @@ with open("config_registration.json", "r") as config_file:
     config = json.load(config_file)
 
 # Function to register a model in a workspace
-def clean_model_name(model_name):
-    # Remove any characters that are not letters, numbers, dashes, periods, or underscores
-    cleaned_model_name = re.sub(r'[^a-zA-Z0-9-._/]', '_', model_name)
-    # Ensure the model name starts with a letter or number
-    if not cleaned_model_name[0].isalnum():
-        cleaned_model_name = 'model_' + cleaned_model_name
-    # Limit the model name to 255 characters
-    return cleaned_model_name[:255]
-
-def register_model(workspace_name, model_name, model_path):
+def register_model(workspace, model_name, model_path):
     try:
-        cleaned_model_name = clean_model_name(model_name)
-        workspace = Workspace.get(name=workspace_name, subscription_id=config["subscription_id"], resource_group=config["resource_group"])
-        model = Model.register(workspace, cleaned_model_name, model_path)
-        print(f"Registered {model.name} in {workspace_name}")
+        model = Model.register(workspace, model_name, model_path)
+        print(f"Registered {model.name} in {workspace.name}")
     except Exception as e:
-        print(f"An error occurred while working with {workspace_name}: {str(e)}")
-
-# def register_model(workspace_name, model_name, model_path):
-#     try:
-#         workspace = Workspace.get(name=workspace_name, subscription_id=config["subscription_id"], resource_group=config["resource_group"])
-#         model = Model.register(workspace, model_name, model_path)
-#         print(f"Registered {model.name} in {workspace_name}")
-#     except Exception as e:
-#         print(f"An error occurred while working with {workspace_name}: {str(e)}")
+        print(f"An error occurred while working with {workspace.name}: {str(e)}")
 
 # Create and start threads for registration
 threads = []
-for workspace_name in config["workspace_names"]:
-    for model_name in config["model_names"]:
-        model_path = os.path.join(config["models_directory"], model_name)
-        thread = threading.Thread(target=register_model, args=(workspace_name, model_name, model_path))
-        thread.start()
-        threads.append(thread)
+workspace_names = config["workspace_names"]
+workspace_count = len(workspace_names)
+model_names = config["model_names"]
+for i, model_name in enumerate(model_names):
+    workspace_name = workspace_names[i % workspace_count]
+    workspace = Workspace.get(name=workspace_name, subscription_id=config["subscription_id"], resource_group=config["resource_group"])
+    model_path = os.path.join(config["models_directory"], model_name)
+    thread = threading.Thread(target=register_model, args=(workspace, model_name, model_path))
+    thread.start()
+    threads.append(thread)
 
 # Wait for all threads to complete
 for thread in threads:
