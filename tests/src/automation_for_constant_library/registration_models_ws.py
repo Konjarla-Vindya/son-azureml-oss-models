6bae39e90572c053795d922a6988fd49f6f95bdf
priyaray21 <139554535+priyaray21@users.noreply.github.com>
2023-10-23 12:20:00 +0530

    Update registration_models_ws.py

--- tests/src/automation_for_constant_library/registration_models_ws.py
+++ tests/src/automation_for_constant_library/registration_models_ws.py
@@ -10,26 +10,35 @@ import re
 with open("config_registration.json", "r") as config_file:
     config = json.load(config_file)
 
+# Dictionary to keep track of which model is registered in which workspace
+registered_models = {}
+
+# Lock for thread synchronization
+lock = threading.Lock()
+
 # Function to register a model in a workspace
-def register_model(workspace, model_name, model_path):
+def register_model(workspace_name, model_name, model_path):
     try:
-        model = Model.register(workspace, model_name, model_path)
-        print(f"Registered {model.name} in {workspace.name}")
+        workspace = Workspace.get(name=workspace_name, subscription_id=config["subscription_id"], resource_group=config["resource_group"])
+        # Check if the model has already been registered
+        with lock:
+            if model_name in registered_models:
+                print(f"Model {model_name} is already registered in {registered_models[model_name]}. Skipping registration in {workspace_name}.")
+                return
+            model = Model.register(workspace, model_name, model_path)
+            registered_models[model_name] = workspace_name
+            print(f"Registered {model.name} in {workspace_name}")
     except Exception as e:
-        print(f"An error occurred while working with {workspace.name}: {str(e)}")
+        print(f"An error occurred while working with {workspace_name}: {str(e)}")
 
 # Create and start threads for registration
 threads = []
-workspace_names = config["workspace_names"]
-workspace_count = len(workspace_names)
-model_names = config["model_names"]
-for i, model_name in enumerate(model_names):
-    workspace_name = workspace_names[i % workspace_count]
-    workspace = Workspace.get(name=workspace_name, subscription_id=config["subscription_id"], resource_group=config["resource_group"])
-    model_path = os.path.join(config["models_directory"], model_name)
-    thread = threading.Thread(target=register_model, args=(workspace, model_name, model_path))
-    thread.start()
-    threads.append(thread)
+for workspace_name in config["workspace_names"]:
+    for model_name in config["model_names"]:
+        model_path = os.path.join(config["models_directory"], model_name)
+        thread = threading.Thread(target=register_model, args=(workspace_name, model_name, model_path))
+        thread.start()
+        threads.append(thread)
 
 # Wait for all threads to complete
 for thread in threads:
