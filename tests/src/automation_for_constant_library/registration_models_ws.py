5ca015e9f7d5e5f56cef53d35650849222edd939
priyaray21 <139554535+priyaray21@users.noreply.github.com>
2023-10-24 09:22:50 +0530

    Update registration_models_ws.py

--- tests/src/automation_for_constant_library/registration_models_ws.py
+++ tests/src/automation_for_constant_library/registration_models_ws.py
@@ -12,26 +12,58 @@ import threading
 from box import ConfigBox
 from mlflow.tracking.client import MlflowClient
 
+
+
+from azureml.core import Workspace, Environment, ComputeTarget
+import mlflow
+import os
+
 # Load configuration from the JSON file
 with open("config_registration.json", "r") as config_file:
     config = json.load(config_file)
 
+# Connect to the Azure ML workspace
+subscription_id = config["subscription_id"]
+resource_group = config["resource_group"]
+workspace_name = config["workspace_name"]
+workspace = Workspace.get(subscription_id=subscription_id, resource_group=resource_group, name=workspace_name)
+
+# Define an environment for your model
+myenv = Environment(name="myenv")
+myenv.python.conda_dependencies = CondaDependencies.create(conda_packages=["scikit-learn"])
+
+# Create or get a compute target
+compute_name = "mycompute"
+try:
+    compute_target = ComputeTarget(workspace=workspace, name=compute_name)
+except Exception as e:
+    compute_config = AmlCompute.provisioning_configuration(vm_size="STANDARD_D2_V2", max_nodes=4)
+    compute_target = ComputeTarget.create(workspace, compute_name, compute_config)
+    compute_target.wait_for_completion(show_output=True)
+
 # Function to register a model in a workspace
 def register_model(subscription_id, resource_group, workspace_name, model_name, model_path):
     try:
-        # Acquire Azure credentials
-        try:
-            # Try to use DefaultAzureCredential for authentication
-            credential = DefaultAzureCredential()
-            credential.get_token("https://management.azure.com/.default")
-        except Exception as ex:
-            # Fall back to InteractiveBrowserCredential in case DefaultAzureCredential doesn't work
-            credential = InteractiveBrowserCredential()
+        # Connect to the Azure ML workspace
+        workspace = Workspace.get(subscription_id=subscription_id, resource_group=resource_group, name=workspace_name)
 
-        workspace = Workspace.get(name=workspace_name, subscription_id=subscription_id, resource_group=resource_group, auth=credential)
+        # Set the environment for the model
+        environment_name = myenv.name
+        registered_environment = myenv.register(workspace)
 
-        registered_model = mlflow.register_model(model_path, model_name, workspace_name)
-        print(f"Registered {model_name} in {workspace_name}")
+        # Set the compute target for model registration
+        compute_target_name = compute_target.name
+
+        # Register the model with MLflow
+        mlflow.register_model(
+            model_uri=model_path,
+            name=model_name,
+            workspace=workspace,
+            registered_env=registered_environment,
+            compute_target=compute_target_name
+        )
+
+        print(f"Registered {model_name} in {workspace.name}")
     except Exception as e:
         print(f"An error occurred while working with {workspace_name}: {str(e)}")
 
@@ -40,9 +72,6 @@ threads = []
 workspace_names = config["workspace_names"]
 workspace_count = len(workspace_names)
 model_names = config["model_names"]
-
-# Initialize a set to keep track of registered models
-registered_models = set()
 
 for i, model_name in enumerate(model_names):
     workspace_name = workspace_names[i % workspace_count]
