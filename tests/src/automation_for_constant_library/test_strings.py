ecde7ba8348cdbd91f963354e651594324989aed
priyaray21 <139554535+priyaray21@users.noreply.github.com>
2023-10-25 12:48:52 +0530

    Update test_strings.py

--- tests/src/automation_for_constant_library/test_strings.py
+++ tests/src/automation_for_constant_library/test_strings.py
@@ -10,25 +10,14 @@ json_files = [f for f in os.listdir(json_directory) if f.endswith('.json')]
 # Sort the list of JSON files to process them in a specific order
 json_files.sort()
 
-if len(json_files) < 2:
-    print("There are not enough JSON files to proceed.")
-else:
-    # Open the first JSON file
-    with open(os.path.join(json_directory, json_files[0]), 'r') as file1:
-        data1 = json.load(file1)
-        strings1 = data1.get("strings", [])
+# Function to trigger the model on JSON data
+def trigger_model(data):
+    # Replace this with code to trigger your model
+    print("Model triggered with data:", data)
 
-    # Open the second JSON file
-    with open(os.path.join(json_directory, json_files[1]), 'r') as file2:
-        data2 = json.load(file2)
-        strings2 = data2.get("strings", [])
-
-    # Determine the maximum number of strings in both files
-    max_strings = max(len(strings1), len(strings2))
-
-    for i in range(max_strings):
-        if i < len(strings1):
-            print(f"From JSON 1: {strings1[i]}")
-
-        if i < len(strings2):
-            print(f"From JSON 2: {strings2[i]}")
+# Iterate through JSON files
+for json_file in json_files[:2]:
+    with open(os.path.join(json_directory, json_file), 'r') as file:
+        data = json.load(file)
+        data1=data["models"][0]
+        print(data["models"][0])
