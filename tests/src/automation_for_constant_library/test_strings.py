4cc4b18f765e13ffcc146de9f86ae22f33e87bc0
priyaray21 <139554535+priyaray21@users.noreply.github.com>
2023-10-25 13:13:20 +0530

    Update test_strings.py

--- tests/src/automation_for_constant_library/test_strings.py
+++ tests/src/automation_for_constant_library/test_strings.py
@@ -15,9 +15,30 @@ def trigger_model(data):
     # Replace this with code to trigger your model
     print("Model triggered with data:", data)
 
+repository_owner = "Konjarla-Vindya"
+repository_name = "son-azureml-oss-models"
+github_token = "WORKFLOW_TOKEN"
+file_path = "data1.yml"  # Replace with the desired YAML file path
+api_url = "https://github.com/Konjarla-Vindya/son-azureml-oss-models/blob/main/.github/workflows/HELLO_WORLD.yml" 
+
 # Iterate through JSON files
 for json_file in json_files[:2]:
     with open(os.path.join(json_directory, json_file), 'r') as file:
         data = json.load(file)
         data1=data["models"][0]
-        print(data["models"][0])
+        print(data1)
+        # Trigger the GitHub Action workflow
+        api_url = f"https://api.github.com/repos/{repository_owner}/{repository_name}/actions/workflows/{workflow_file_path}/dispatches"
+        headers = {
+            "Authorization": f"token {github_token}",
+            "Accept": "application/vnd.github.v3+json"
+        }
+        payload = {
+            "ref": "main"  # Replace with the branch where your workflow is located
+        }
+        response = requests.post(api_url, headers=headers, json=payload)
+        
+        if response.status_code == 204:
+            print(f"Workflow '{workflow_file_path}' has been triggered successfully.")
+        else:
+            print(f"Failed to trigger the workflow. Status code: {response.status_code}")
