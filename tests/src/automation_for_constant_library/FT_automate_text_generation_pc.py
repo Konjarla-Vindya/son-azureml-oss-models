49eb6692bc5ed82eb0b13b1f423f2f29ecafffd6
SwathiPriyaRV <66841534+Swathi23345@users.noreply.github.com>
2023-11-08 15:51:17 +0530

    Update FT_automate_text_generation_pc.py

--- tests/src/automation_for_constant_library/FT_automate_text_generation_pc.py
+++ tests/src/automation_for_constant_library/FT_automate_text_generation_pc.py
@@ -212,7 +212,7 @@ def create_or_get_aml_compute(workspace_ml_client, compute_cluster, compute_clus
 
 def download_and_process_dataset():
     # Download the dataset using the helper script.
-    exit_status = os.system("python ./download-datasetn.py --dataset_subset generation --dataset truthful_qa --download_dir truthful_qa-dataset")
+    exit_status = os.system("python ./download_datasetn.py --dataset_subset generation --dataset truthful_qa --download_dir truthful_qa-dataset")
     if exit_status != 0:
         raise Exception("Error downloading dataset")
 
