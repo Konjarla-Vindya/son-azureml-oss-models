3b8623fddd5780e14389144f338c6901da6575aa
PoojaRam12 <139552501+PoojaRam12@users.noreply.github.com>
2023-11-15 14:32:50 +0530

    Update FT_P_All.py

--- tests/src/pipeline_component/FT_P_All.py
+++ tests/src/pipeline_component/FT_P_All.py
@@ -36,6 +36,32 @@ def get_sku_override():
         print(f"::warning:: Could not find sku-override file: \n{e}")
         return None
 
+def set_next_trigger_model(queue):
+    print("In set_next_trigger_model...")
+# file the index of test_model_name in models list queue dictionary
+    model_list = list(queue.models)
+    #model_name_without_slash = test_model_name.replace('/', '-')
+    check_mlflow_model = "MLFlow-Batch-"+test_model_name
+    index = model_list.index(check_mlflow_model)
+    #index = model_list.index(test_model_name)
+    #index = model_list.index(test_model_name)
+    print(f"index of {test_model_name} in queue: {index}")
+# if index is not the last element in the list, get the next element in the list
+    if index < len(model_list) - 1:
+        next_model = model_list[index + 1]
+    else:
+        if (test_keep_looping == "true"):
+            next_model = queue[0]
+        else:
+            print("::warning:: finishing the queue")
+            next_model = ""
+
+    
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        print(f'NEXT_MODEL={next_model}')
+        print(f'NEXT_MODEL={next_model}', file=fh)
+        
+
 def run_fine_tuning(primary_task):
     # Import necessary modules
     import subprocess
