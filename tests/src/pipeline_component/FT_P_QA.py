07809dfeaf72869e45f8c1dfdc7ce9f09ced7232
PoojaRam12 <139552501+PoojaRam12@users.noreply.github.com>
2023-11-15 16:03:49 +0530

    Update FT_P_QA.py

--- tests/src/pipeline_component/FT_P_QA.py
+++ tests/src/pipeline_component/FT_P_QA.py
@@ -366,6 +366,7 @@ if __name__ == "__main__":
     if test_trigger_next_model == "true":
         set_next_trigger_model(queue)
     # print values of all above variables
+    print("Running for QA")
     print (f"test_subscription_id: {queue['subscription']}")
     print (f"test_resource_group: {queue['resource_group']}")
     print (f"test_workspace_name: {queue['workspace']}")
