6669e27faf031f36b00f5ef55cd106597cdec590
praveen0514 <83938401+praveen0514@users.noreply.github.com>
2023-12-22 09:43:13 +0530

    Update automate_create_env.py

--- automate_create_env.py
+++ automate_create_env.py
@@ -19,11 +19,11 @@ channels = ["conda-forge", "defaults"]
 conda_dependencies = ["python=3.10", "numpy", "pandas", "pip"]
 pip_dependencies = [
    "azureml-mlflow","azureml-core","ipython","datasets","accelerate==0.21.0",
-   "ipykernel","evaluate","azure-ai-ml","numpy","tensorflow==2.9","mlflow==2.4",
+   "ipykernel","evaluate","azure-ai-ml","numpy","tensorflow==2.9","mlflow==2.8.0",
    "cffi==1.15.1","dill==0.3.6","google-api-core==2.11.0","ipython==8.8.0",
    "numpy==1.23.5","packaging==21.3","protobuf==3.20.3","pyyaml==6.0",
    "requests==2.28.2","safetensors==0.3.1","scikit-learn==1.2.2","scipy==1.10.1",
-   "torch==2.0.1","torchvision==0.15.2","transformers==4.29.0",
+   "torch==2.0.1","torchvision==0.15.2","transformers==4.34.0",
    "xformers==0.0.20","azureml-mlflow==1.53.0","azure-core==1.27.1","torchvision",
    "sacremoses","python-box","sentencepiece","fugashi[unidic-lite]", "einops",
     "tiktoken","transformers_stream_generator","cpm_kernels","ipadic","mecab-python3"
